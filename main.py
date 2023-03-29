52cd9c9686b72419c3b732832dbb4a890367edb3
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-03-29 21:20:11 +0530

    reformat file.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from search_insert_position import Solution
 
 if __name__ == '__main__':
-    nums = [1,2,4,6,7]
+    nums = [1, 2, 4, 6, 7]
     target = 3
     sol = Solution()
     print(sol.searchInsert(nums=nums, target=target))
