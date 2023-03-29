33e0298ab51fa41fb0104e52c80a851e660ed173
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-03-29 20:26:25 +0530

    update main to solve remove-element solution.

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
+from remove_element import Solution
 if __name__ == '__main__':
+    nums = [3, 2, 2, 3]
+    val = 3
+    sol = Solution()
+    print("m" , nums)
+    print(sol.removeElement(nums, val))
+    print("m", nums)
     pass
