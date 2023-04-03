8d3e78aab2d5d31db5f9218d23d0e84d5fd9892a
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-04-03 19:50:45 +0530

    add the solution.

--- merge_sorted_array.py
+++ merge_sorted_array.py
@@ -4,7 +4,7 @@ from typing import List
 
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        """
-        Do not return anything, modify nums1 in-place instead.
-        """
-        pass
+        if m != len(nums1):
+            for i in nums2:
+                nums1.remove(0)
+        nums1[:] = sorted(nums1 + nums2)
