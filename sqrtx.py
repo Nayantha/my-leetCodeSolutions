45325dc65a1468a246829daef522a166d7edf5ef
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-04-03 18:21:58 +0530

    add the solution using newton's method.

--- sqrtx.py
+++ sqrtx.py
@@ -1,4 +1,10 @@
 # https://leetcode.com/problems/sqrtx/
 class Solution:
     def mySqrt(self, x: int) -> int:
-        pass
+        r = x
+        while r * r > x:
+            r = (r + x / r) / 2
+        return r
+
+sol = Solution()
+print(sol.mySqrt(8))
