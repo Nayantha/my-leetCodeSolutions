d4190574fa426a8c8d02c96e5a6882dae282ef0d
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-04-03 17:26:51 +0530

    remove all code testing part`.

--- main.py
+++ main.py
@@ -1,8 +1,2 @@
-from search_insert_position import Solution
-
 if __name__ == '__main__':
-    nums = [1, 2, 4, 6, 7]
-    target = 3
-    sol = Solution()
-    print(sol.searchInsert(nums=nums, target=target))
     pass
