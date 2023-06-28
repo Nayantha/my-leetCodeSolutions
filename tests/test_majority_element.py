85f1b703cb793a303dbabbae1eaacf7b0358ec0d
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-06-28 16:26:41 +0530

    add a test to test outcome.

--- tests/test_majority_element.py
+++ tests/test_majority_element.py
@@ -1,2 +1,13 @@
-def test_majority_element():
-    assert False
+from typing import List
+from majority_element import Solution
+import pytest
+
+
+class TestSolution:
+    @pytest.mark.parametrize("nums, expected", [
+        ([3, 2, 3], 3),
+        ([2, 2, 1, 1, 1, 2, 2], 2)
+    ])
+    def test_majority_element(self, nums: List[int], expected: int):
+        sol = Solution()
+        assert sol.majority_element(nums) == expected
