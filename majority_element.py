abf45c431759a78e30e0211981c1530854fe269e
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-06-28 16:26:11 +0530

    add a solution.

--- majority_element.py
+++ majority_element.py
@@ -1,1 +1,15 @@
 # https://leetcode.com/problems/majority-element/?envType=study-plan-v2&envId=top-interview-150
+from typing import List
+
+
+class Solution:
+    def majority_element(self, nums: List[int]) -> int:
+        unique_number_set = set(sorted(nums))
+        max_occurrences = 0
+        max_occurred_num = 0
+        for num in unique_number_set:
+            num_count = nums.count(num)
+            if max_occurrences < num_count:
+                max_occurrences = num_count
+                max_occurred_num = num
+        return max_occurred_num
