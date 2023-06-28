1a0aa2c089d527f66248798c6881aa89f5e71bc2
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-06-28 16:50:41 +0530

    add testing code and parameters with test data.

---
+++ tests/test_best_time_to_buy_and_sell_stock.py
@@ -0,0 +1,14 @@
+from typing import List
+
+import pytest
+
+from best_time_to_buy_and_sell_stock import max_profit
+
+
+@pytest.mark.parametrize("prices, expected", [
+    ([7, 1, 5, 3, 6, 4], 5),
+    ([7, 6, 4, 3, 1], 0)
+])
+def test_max_profit(prices: List[int], expected: int):
+    assert max_profit(prices) == expected
+
