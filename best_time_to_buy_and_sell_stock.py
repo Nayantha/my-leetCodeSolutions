2dd12b5d5952df0918e4f4ce0f85d6fae09cc1c7
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-06-28 16:51:16 +0530

    completed not submitted solution.

--- best_time_to_buy_and_sell_stock.py
+++ best_time_to_buy_and_sell_stock.py
@@ -1,2 +1,10 @@
 # https://leetcode.com/problems/best-time-to-buy-and-sell-stock/?envType=study-plan-v2&envId=top-interview-150
 
+from typing import List
+def max_profit(prices: List[int]) -> int:
+    min_sell_value = min(prices)
+    sellable_price_list = prices[prices.index(min_sell_value) + 1:]
+    if not sellable_price_list:
+        return 0
+    return max(sellable_price_list) - min_sell_value
+
