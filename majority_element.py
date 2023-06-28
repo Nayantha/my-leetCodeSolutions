b2944a1c433bc81cfd657cc91c1f9d819659effd
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-06-28 16:32:38 +0530

    add another solution.

--- majority_element.py
+++ majority_element.py
@@ -3,13 +3,23 @@ from typing import List
 
 
 class Solution:
+    # def majority_element(self, nums: List[int]) -> int:
+    #     unique_number_set = set(sorted(nums))
+    #     max_occurrences = 0
+    #     max_occurred_num = 0
+    #     for num in unique_number_set:
+    #         num_count = nums.count(num)
+    #         if max_occurrences < num_count:
+    #             max_occurrences = num_count
+    #             max_occurred_num = num
+    #     return max_occurred_num
+
     def majority_element(self, nums: List[int]) -> int:
-        unique_number_set = set(sorted(nums))
-        max_occurrences = 0
-        max_occurred_num = 0
-        for num in unique_number_set:
-            num_count = nums.count(num)
-            if max_occurrences < num_count:
-                max_occurrences = num_count
-                max_occurred_num = num
-        return max_occurred_num
+        numbers_and_count_dict : dict = dict()
+        for num in set(sorted(nums)):
+            numbers_and_count_dict[num] = nums.count(num)
+        for key, value in numbers_and_count_dict.items():
+            if value > len(nums) / 2:
+                return key
+        return 0
+
