c7ef6c86bb41116621a2007245f180fcae88749b
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-06-30 11:48:08 +0530

    minimize the code.

--- valid_palindrome.py
+++ valid_palindrome.py
@@ -1,5 +1,6 @@
 # https://leetcode.com/problems/valid-palindrome/?envType=study-plan-v2&envId=top-interview-150
 
 def is_palindrome(s: str) -> bool:
-    processed_str = "".join([letter.lower() for letter in s if letter.isalnum()])
+    # processed_str = "".join([letter.lower() for letter in s if letter.isalnum()])
+    processed_str = [letter.lower() for letter in s if letter.isalnum()]
     return processed_str == processed_str[::-1]
