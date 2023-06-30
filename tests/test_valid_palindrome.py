a39ff97a974e8362429b502a7ba7964a4cdae6ed
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-06-30 11:22:58 +0530

    add test data.

--- tests/test_valid_palindrome.py
+++ tests/test_valid_palindrome.py
@@ -1,2 +1,10 @@
-def test_is_palindrome():
+import pytest
+
+
+@pytest.mark.parametrize("s, expected", [
+    ("A man, a plan, a canal: Panama", True),
+    ("race a car", False),
+    (" ", True)
+])
+def test_is_palindrome(s: str, expected: str):
     assert False
