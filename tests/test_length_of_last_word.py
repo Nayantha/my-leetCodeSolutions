1131e34474ba4f0b1047bf43a16465f37d89a056
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-06-30 11:10:32 +0530

    add test data.

--- tests/test_length_of_last_word.py
+++ tests/test_length_of_last_word.py
@@ -1,2 +1,8 @@
-def test_length_of_last_word():
+import pytest
+@pytest.mark.parametrize("s, expected", [
+    ("Hello World", 5),
+    ("   fly me   to   the moon  ",4),
+    ("luffy is still joyboy", 6)
+])
+def test_length_of_last_word(s: str, expected: int):
     assert False
