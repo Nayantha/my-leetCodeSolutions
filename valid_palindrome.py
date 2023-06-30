eb3c5f9d0003358ce49342fe2f7e341b51aa33a2
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-06-30 11:32:30 +0530

    add a solution.

--- valid_palindrome.py
+++ valid_palindrome.py
@@ -1,4 +1,5 @@
 # https://leetcode.com/problems/valid-palindrome/?envType=study-plan-v2&envId=top-interview-150
 
 def is_palindrome(s: str) -> bool:
-    ...
+    processed_str = "".join([letter.lower() for letter in s if letter.isalpha()])
+    return processed_str == processed_str[::-1]
