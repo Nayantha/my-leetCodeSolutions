83c237f2e5ead576b12f7b2cb9a5bde3245140c7
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-06-30 11:11:43 +0530

    add test logic.

--- tests/test_length_of_last_word.py
+++ tests/test_length_of_last_word.py
@@ -1,8 +1,12 @@
 import pytest
+
+from length_of_last_word import length_of_last_word
+
+
 @pytest.mark.parametrize("s, expected", [
     ("Hello World", 5),
     ("   fly me   to   the moon  ",4),
     ("luffy is still joyboy", 6)
 ])
 def test_length_of_last_word(s: str, expected: int):
-    assert False
+    assert length_of_last_word(s) == expected
