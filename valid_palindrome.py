53607ba5d60e2ceb93fd1254c2658559d5429aaf
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-06-30 11:48:43 +0530

    add two pointer solution.

--- valid_palindrome.py
+++ valid_palindrome.py
@@ -4,3 +4,15 @@ def is_palindrome(s: str) -> bool:
     # processed_str = "".join([letter.lower() for letter in s if letter.isalnum()])
     processed_str = [letter.lower() for letter in s if letter.isalnum()]
     return processed_str == processed_str[::-1]
+
+
+def is_palindrome_two_pointers(s: str) -> bool:
+    processed_str = [letter.lower() for letter in s if letter.isalnum()]
+    left = 0
+    right = len(processed_str) - 1
+    while left <= right:
+        if processed_str[left] != processed_str[right]:
+            return False
+        left += 1
+        right -= 1
+    return True
