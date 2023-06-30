bb8c09985f460563f0328a1f383f08676ff46438
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-06-30 11:25:34 +0530

    add test logic.

--- tests/test_valid_palindrome.py
+++ tests/test_valid_palindrome.py
@@ -1,10 +1,12 @@
 import pytest
 
+from valid_palindrome import is_palindrome
+
 
 @pytest.mark.parametrize("s, expected", [
     ("A man, a plan, a canal: Panama", True),
     ("race a car", False),
     (" ", True)
 ])
-def test_is_palindrome(s: str, expected: str):
-    assert False
+def test_is_palindrome(s: str, expected: bool):
+    assert is_palindrome(s) == expected
