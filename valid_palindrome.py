dfac78f1c1e2e836b5bac5684bc21ceb5e3c0ce3
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-06-30 11:36:25 +0530

    modify the solution to fit numbers in string.

--- valid_palindrome.py
+++ valid_palindrome.py
@@ -1,5 +1,5 @@
 # https://leetcode.com/problems/valid-palindrome/?envType=study-plan-v2&envId=top-interview-150
 
 def is_palindrome(s: str) -> bool:
-    processed_str = "".join([letter.lower() for letter in s if letter.isalpha()])
+    processed_str = "".join([letter.lower() for letter in s if letter.isalnum()])
     return processed_str == processed_str[::-1]
