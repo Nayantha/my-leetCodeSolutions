7d67bc1ca3104403ffa7058246357ea62f44341a
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-06-30 11:13:55 +0530

    add a solution.

--- length_of_last_word.py
+++ length_of_last_word.py
@@ -1,4 +1,4 @@
 # https://leetcode.com/problems/length-of-last-word/?envType=study-plan-v2&envId=top-interview-150
 
 def length_of_last_word(s: str) -> int:
-    ...
+    return len(s.strip().split(" ")[-1])
