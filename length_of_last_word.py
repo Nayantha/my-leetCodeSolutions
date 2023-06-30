e4812d0cf2035ed2e6299e41f499ecd8b18f8500
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-06-30 11:04:44 +0530

    remove unwanted class.

--- length_of_last_word.py
+++ length_of_last_word.py
@@ -1,5 +1,4 @@
 # https://leetcode.com/problems/length-of-last-word/?envType=study-plan-v2&envId=top-interview-150
 
-class Solution:
-    def length_of_last_word(self, s: str) -> int:
+def length_of_last_word(self, s: str) -> int:
         ...
