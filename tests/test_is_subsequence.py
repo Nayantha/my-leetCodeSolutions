0f5b7708ca911ab75bfdc94502a6021333579c7d
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-02 10:29:28 +0530

    add test data.

--- tests/test_is_subsequence.py
+++ tests/test_is_subsequence.py
@@ -1,2 +1,7 @@
-def test_is_subsequence():
-    assert False
+import pytest
+@pytest.mark.parametrize("s, t, expected", [
+    ("abc", "ahbgdc", True),
+    ("axc", "ahbgdc", False)
+])
+def test_is_subsequence(s: str, t: str, expected: bool):
+    ...
