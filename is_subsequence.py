9d5fbc5974b46ac020556a0b6843a1a5adf18d83
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-02 10:30:49 +0530

    add a solution.

--- is_subsequence.py
+++ is_subsequence.py
@@ -1,4 +1,7 @@
 # https://leetcode.com/problems/is-subsequence/?envType=study-plan-v2&envId=top-interview-150
 
 def is_subsequence(self, s: str, t: str) -> bool:
-    ...
+    for letter in s:
+        if letter not in t:
+            return False
+    return True
