9fc245403c98c22471b41afa0587eedd3850d990
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-02 10:32:58 +0530

    remove class inherit parameter.

--- is_subsequence.py
+++ is_subsequence.py
@@ -1,6 +1,6 @@
 # https://leetcode.com/problems/is-subsequence/?envType=study-plan-v2&envId=top-interview-150
 
-def is_subsequence(self, s: str, t: str) -> bool:
+def is_subsequence(s: str, t: str) -> bool:
     for letter in s:
         if letter not in t:
             return False
