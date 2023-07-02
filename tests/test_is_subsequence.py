f18aa18ad11a5b8c14e9a6996b1a3f535da37276
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-02 10:31:33 +0530

    add test logic.

--- tests/test_is_subsequence.py
+++ tests/test_is_subsequence.py
@@ -1,7 +1,11 @@
 import pytest
+
+from is_subsequence import is_subsequence
+
+
 @pytest.mark.parametrize("s, t, expected", [
     ("abc", "ahbgdc", True),
     ("axc", "ahbgdc", False)
 ])
 def test_is_subsequence(s: str, t: str, expected: bool):
-    ...
+    assert is_subsequence(s, t) == expected
