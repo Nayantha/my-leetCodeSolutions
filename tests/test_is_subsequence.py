804915869c2511c103bbc4b648bbc05e1aa4cc03
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-02 10:52:23 +0530

    add another test case.

--- tests/test_is_subsequence.py
+++ tests/test_is_subsequence.py
@@ -6,7 +6,9 @@ from is_subsequence import is_subsequence
 @pytest.mark.parametrize("s, t, expected", [
     ("abc", "ahbgdc", True),
     ("axc", "ahbgdc", False),
-    ("acb", "ahbgdc", False)
+    ("acb", "ahbgdc", False),
+    ("b", "abc", True),
+    ("", "abc", True)
 ])
 def test_is_subsequence(s: str, t: str, expected: bool):
     assert is_subsequence(s, t) == expected
