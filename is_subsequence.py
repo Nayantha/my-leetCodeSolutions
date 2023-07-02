41493e5e89531dc42d72c879d5110898b37d46a4
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-02 12:10:48 +0530

    complete accepted solution.

--- is_subsequence.py
+++ is_subsequence.py
@@ -1,5 +1,11 @@
 # https://leetcode.com/problems/is-subsequence/?envType=study-plan-v2&envId=top-interview-150
 
 def is_subsequence(s: str, t: str) -> bool:
-    sub_string = "".join([letter for letter in t if letter in s])
-    return s == sub_string or s in sub_string
+    if s in t:
+        return True
+    for letter in s:
+        if letter not in t:
+            return False
+        else:
+            t = t[t.index(letter)+1:]
+    return True
