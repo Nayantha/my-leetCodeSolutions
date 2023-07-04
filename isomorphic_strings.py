86e6048ec6eb0f49af765d3e36a4c585ba0e0679
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-04 12:47:32 +0530

    add a solution.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- isomorphic_strings.py
+++ isomorphic_strings.py
@@ -1,3 +1,12 @@
 # https://leetcode.com/problems/isomorphic-strings/?envType=study-plan-v2&envId=top-interview-150
 def is_isomorphic(s: str, t: str) -> bool:
-    ...
+    if len(set(s)) != len(set(t)):
+        return False
+    hash_map = dict()
+    for letter_in_s, letter_in_t in zip(s, t):
+        dict_value = hash_map.get(letter_in_s, None)
+        if not dict_value:
+            hash_map[letter_in_s] = letter_in_t
+        elif dict_value != letter_in_t:
+            return False
+    return True
