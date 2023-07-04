9e9294760ccaa594ffc661961beaa8fe548fef6d
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-04 11:52:57 +0530

    add test logic.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_ransom_note.py
+++ tests/test_ransom_note.py
@@ -1,5 +1,7 @@
 import pytest
 
+from ransom_note import can_construct
+
 
 @pytest.mark.parametrize("ransom_note, magazine, expected", [
     ("a", "b", False),
@@ -7,4 +9,4 @@ import pytest
     ("aa", "aab", True)
 ])
 def test_can_construct(ransom_note: str, magazine: str, expected: bool):
-    ...
+    assert can_construct(ransom_note, magazine) == expected
