43b162f84e4b5f4fe3e608ffa80a94d2022611e3
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-04 12:16:05 +0530

    add test data.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_isomorphic_strings.py
+++ tests/test_isomorphic_strings.py
@@ -1,2 +1,10 @@
-def test_is_isomorphic():
+import pytest
+
+
+@pytest.mark.parametrize("s, t, expected", [
+    ("egg", "add", True),
+    ("paper", "title", True),
+    ("foo", "bar", False)
+])
+def test_is_isomorphic(s: str, t: str, expected: bool):
     assert False
