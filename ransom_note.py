4d18164ac1a6b15182106708537f58c110163f94
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-04 11:52:01 +0530

    add a solution.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- ransom_note.py
+++ ransom_note.py
@@ -1,3 +1,8 @@
 # https://leetcode.com/problems/ransom-note/?envType=study-plan-v2&envId=top-interview-150
 def can_construct(ransom_note: str, magazine: str) -> bool:
-    ...
+    for letter in ransom_note:
+        if letter not in magazine:
+            return False
+        else:
+            magazine = magazine.replace(letter, "")
+    return True
