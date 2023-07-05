0754c03b229aeb414d79d716f881f60feef76d17
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-05 11:17:40 +0530

    add a solution.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- happy_number.py
+++ happy_number.py
@@ -1,3 +1,7 @@
 # https://leetcode.com/problems/happy-number/?envType=study-plan-v2&envId=top-interview-150
 def is_happy(n: int) -> bool:
-    ...
+    if n == 1:
+        return True
+    if len(str(n)) == 1:
+        return False
+    return is_happy(sum([pow(int(num), 2) for num in list(str(n))]))
