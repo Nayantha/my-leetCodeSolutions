2bd74732a9f8b334e901cdafaa3fefdece18d118
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-05 11:17:52 +0530

    add test logic.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_happy_number.py
+++ tests/test_happy_number.py
@@ -1,9 +1,11 @@
 import pytest
 
+from happy_number import is_happy
+
 
 @pytest.mark.parametrize("n, expected", [
     (19, True),
     (2, False)
 ])
 def test_is_happy(n: int, expected: bool):
-    ...
+    assert is_happy(n) == expected
