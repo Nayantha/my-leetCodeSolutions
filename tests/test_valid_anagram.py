126aa4bb34fdac834e719ef398bc1b3b3189b869
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-05 10:39:07 +0530

    add test logic.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_valid_anagram.py
+++ tests/test_valid_anagram.py
@@ -1,9 +1,11 @@
 import pytest
 
+from valid_anagram import is_anagram
+
 
 @pytest.mark.parametrize("s, t, expected", [
     ("anagram", "nagaram", True),
     ("rat", "car", False)
 ])
 def test_is_anagram(s: str, t: str, expected: bool):
-    ...
+    assert is_anagram(s, t) == expected
