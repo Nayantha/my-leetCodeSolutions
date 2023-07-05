067ccc80b8f3caac75fa0e9f4d0c26cc1aafb75a
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-05 10:43:23 +0530

    edit the solution.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- valid_anagram.py
+++ valid_anagram.py
@@ -1,4 +1,4 @@
 # https://leetcode.com/problems/valid-anagram/?envType=study-plan-v2&envId=top-interview-150
 
 def is_anagram(s: str, t: str) -> bool:
-    return list(s) == list(t)
+    return sorted(list(s)) == sorted(list(t))
