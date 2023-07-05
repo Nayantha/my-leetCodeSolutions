0ee8c10ed499d5bf575c2b7d97d82a6e70c04aa6
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-05 10:59:07 +0530

    add test data.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_happy_number.py
+++ tests/test_happy_number.py
@@ -1,2 +1,9 @@
-def test_is_happy():
-    assert False
+import pytest
+
+
+@pytest.mark.parametrize("n, expected", [
+    (19, True),
+    (2, False)
+])
+def test_is_happy(n: int, expected: bool):
+    ...
