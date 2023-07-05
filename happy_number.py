a1528643e79e0912dda19c590495cfef216166b5
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-05 11:36:52 +0530

    edit solution.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- happy_number.py
+++ happy_number.py
@@ -1,7 +1,13 @@
 # https://leetcode.com/problems/happy-number/?envType=study-plan-v2&envId=top-interview-150
 def is_happy(n: int) -> bool:
-    if n == 1:
-        return True
-    if len(str(n)) == 1:
-        return False
-    return is_happy(sum([pow(int(num), 2) for num in list(str(n))]))
+    sum_of_square_seq = set()
+    while n != 1:
+        n = square_and_sum(n)
+        if n in sum_of_square_seq:
+            return False
+        sum_of_square_seq.add(n)
+    return True
+
+
+def square_and_sum(n):
+    return sum([pow(int(num), 2) for num in list(str(n))])
