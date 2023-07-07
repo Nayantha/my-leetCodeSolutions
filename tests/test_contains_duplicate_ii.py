821849f7e9b9f3b8795b029a60b0a35bf045e76b
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-07 10:16:38 +0530

    add test data.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_contains_duplicate_ii.py
+++ tests/test_contains_duplicate_ii.py
@@ -1,2 +1,12 @@
-def test_contains_nearby_duplicate():
-    assert False
+from typing import List
+
+import pytest
+
+
+@pytest.mark.parametrize("nums, k, expected", [
+    ([1, 2, 3, 1], 3, True),
+    ([1, 0, 1, 1], 1, True),
+    ([1, 2, 3, 1, 2, 3], 2, False)
+])
+def test_contains_nearby_duplicate(nums: List[int], k: int, expected: bool):
+    ...
