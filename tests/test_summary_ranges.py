0f422ba39948e74c2698c4a8e38a8cd1b07f2f4e
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-07 11:03:16 +0530

    add test data.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_summary_ranges.py
+++ tests/test_summary_ranges.py
@@ -1,2 +1,11 @@
-def test_summary_ranges():
+from typing import List
+
+import pytest
+
+
+@pytest.mark.parametrize("nums, expected", [
+    ([0, 1, 2, 4, 5, 7], ["0->2", "4->5", "7"]),
+    ([0, 2, 3, 4, 6, 8, 9], ["0", "2->4", "6", "8->9"])
+])
+def test_summary_ranges(nums: List[int], expected: List[str]):
     assert False
