96ee2ac49f7fd3dade77a96d7cf30dcd77745022
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-07 11:41:24 +0530

    add test logic.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_summary_ranges.py
+++ tests/test_summary_ranges.py
@@ -2,10 +2,12 @@ from typing import List
 
 import pytest
 
+from summary_ranges import summary_ranges
+
 
 @pytest.mark.parametrize("nums, expected", [
     ([0, 1, 2, 4, 5, 7], ["0->2", "4->5", "7"]),
     ([0, 2, 3, 4, 6, 8, 9], ["0", "2->4", "6", "8->9"])
 ])
 def test_summary_ranges(nums: List[int], expected: List[str]):
-    assert False
+    assert summary_ranges(nums) == expected
