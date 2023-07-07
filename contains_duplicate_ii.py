e7a04d5fca4910618d0037c87eabad8d8caeb365
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-07 10:54:10 +0530

    add a solution.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- contains_duplicate_ii.py
+++ contains_duplicate_ii.py
@@ -2,5 +2,10 @@
 from typing import List
 
 
-def contains_nearby_duplicate(self, nums: List[int], k: int) -> bool:
-    ...
+def contains_nearby_duplicate(nums: List[int], k: int) -> bool:
+    index_map = dict()
+    for i, num in enumerate(nums):
+        if num in index_map and i - index_map[num] <= k:
+            return True
+        index_map[num] = i
+    return False
