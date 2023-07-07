e0730be6bdb67598e000a8ba0d1d98b1e707cb78
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-07 11:00:02 +0530

    copy skeleton code form site.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- summary_ranges.py
+++ summary_ranges.py
@@ -1,1 +1,6 @@
 # https://leetcode.com/problems/summary-ranges/?envType=study-plan-v2&envId=top-interview-150
+from typing import List
+
+
+def summary_ranges(nums: List[int]) -> List[str]:
+    ...
