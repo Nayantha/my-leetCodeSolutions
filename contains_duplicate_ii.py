e117442a78c069f5b22c87cbbd90979fefca9d46
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-07 10:07:44 +0530

    copy skeleton code form site.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- contains_duplicate_ii.py
+++ contains_duplicate_ii.py
@@ -1,1 +1,6 @@
 # https://leetcode.com/problems/contains-duplicate-ii/?envType=study-plan-v2&envId=top-interview-150
+from typing import List
+
+
+def contains_nearby_duplicate(self, nums: List[int], k: int) -> bool:
+    ...
