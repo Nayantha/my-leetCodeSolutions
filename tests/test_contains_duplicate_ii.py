aeab4448dc2846606f84560d9a11f4953510bc7e
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-07 10:54:36 +0530

    add test logic.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_contains_duplicate_ii.py
+++ tests/test_contains_duplicate_ii.py
@@ -2,6 +2,8 @@ from typing import List
 
 import pytest
 
+from contains_duplicate_ii import contains_nearby_duplicate
+
 
 @pytest.mark.parametrize("nums, k, expected", [
     ([1, 2, 3, 1], 3, True),
@@ -9,4 +11,4 @@ import pytest
     ([1, 2, 3, 1, 2, 3], 2, False)
 ])
 def test_contains_nearby_duplicate(nums: List[int], k: int, expected: bool):
-    ...
+    assert contains_nearby_duplicate(nums, k) == expected
