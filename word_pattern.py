da5ed1c4fd41ece336c7860cca3b4535b3ce287f
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-08 12:27:49 +0530

    add new variable.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- word_pattern.py
+++ word_pattern.py
@@ -3,10 +3,11 @@ def word_pattern(pattern: str, s: str) -> bool:
     letter_to_word_map = dict()
     words_in_s = s.split()
     for i, letter in enumerate(pattern):
+        word = words_in_s[i]
         mapped_word_to_letter = letter_to_word_map.get(letter)
         if not mapped_word_to_letter:
-            letter_to_word_map[letter] = words_in_s[i]
+            letter_to_word_map[letter] = word
             continue
-        if mapped_word_to_letter != words_in_s[i]:
+        if mapped_word_to_letter != word:
             return False
     return True
