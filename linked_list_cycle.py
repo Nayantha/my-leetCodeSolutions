f8ece616e9d76a5b31f41927f1ef74a8290f7425
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-08 13:28:22 +0530

    copy skeleton code form site.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- linked_list_cycle.py
+++ linked_list_cycle.py
@@ -1,1 +1,12 @@
 # https://leetcode.com/problems/linked-list-cycle/?envType=study-plan-v2&envId=top-interview-150
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+def has_cycle(head: Optional[ListNode]) -> bool:
+    ...
