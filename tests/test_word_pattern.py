092370f093ab59e06ea253518e42dd88c00a276c
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-08 11:51:46 +0530

    add another test case.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_word_pattern.py
+++ tests/test_word_pattern.py
@@ -6,7 +6,8 @@ from word_pattern import word_pattern
 @pytest.mark.parametrize("pattern, s, expected", [
     ("abba", "dog cat cat dog", True),
     ("abba", "dog cat cat fish", False),
-    ("aaaa", "dog cat cat dog", False)
+    ("aaaa", "dog cat cat dog", False),
+    ("abba", "dog dog dog dog", False)
 ])
 def test_word_pattern(pattern: str, s: str, expected: bool):
     assert word_pattern(pattern, s) == expected
