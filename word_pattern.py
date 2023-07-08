224f5fa5fff3b9e0180b9a0e208bc449d84bd45c
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-08 12:43:26 +0530

    add suitable solution.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- word_pattern.py
+++ word_pattern.py
@@ -10,4 +10,9 @@ def word_pattern(pattern: str, s: str) -> bool:
             continue
         if mapped_word_to_letter != word:
             return False
+        flipped_dict = {v: k for k, v in letter_to_word_map.items()}
+        print(flipped_dict, letter_to_word_map)
+        print({v: k for k, v in flipped_dict.items()}, letter_to_word_map)
+        if {v: k for k, v in flipped_dict.items()} != letter_to_word_map:
+            return False
     return True
