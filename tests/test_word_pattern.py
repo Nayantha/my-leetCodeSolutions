8418091f6fb0d7e1a3d0c0bea5f8e0c25f09700b
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-08 12:43:04 +0530

    add another test case.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_word_pattern.py
+++ tests/test_word_pattern.py
@@ -7,7 +7,8 @@ from word_pattern import word_pattern
     ("abba", "dog cat cat dog", True),
     ("abba", "dog cat cat fish", False),
     ("aaaa", "dog cat cat dog", False),
-    ("abba", "dog dog dog dog", False)
+    ("abba", "dog dog dog dog", False),
+    ("abc", "dog cat dog", False)
 ])
 def test_word_pattern(pattern: str, s: str, expected: bool):
     assert word_pattern(pattern, s) == expected
