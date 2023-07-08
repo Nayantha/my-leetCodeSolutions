d885b1d3d04dd7015039f9eb1abb908eb7924709
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-08 11:34:12 +0530

    add start testing code.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_word_pattern.py
+++ tests/test_word_pattern.py
@@ -1,5 +1,7 @@
 import pytest
 
+from word_pattern import word_pattern
+
 
 @pytest.mark.parametrize("pattern, s, expected", [
     ("abba", "dog cat cat dog", True),
@@ -7,4 +9,4 @@ import pytest
     ("aaaa", "dog cat cat dog", False)
 ])
 def test_word_pattern(pattern: str, s: str, expected: bool):
-    assert False
+    word_pattern(pattern, s)
