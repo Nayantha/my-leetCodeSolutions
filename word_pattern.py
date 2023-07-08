0a8756b21ac72545b8da1609fd180522d6ded7f5
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-08 13:25:57 +0530

    add the final solution.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- word_pattern.py
+++ word_pattern.py
@@ -2,17 +2,19 @@
 def word_pattern(pattern: str, s: str) -> bool:
     letter_to_word_map = dict()
     words_in_s = s.split()
+
+    if len(pattern) != len(words_in_s):
+        return False
+
+    if len(set(pattern)) != len(set(words_in_s)):
+        return False
+
     for i, letter in enumerate(pattern):
         word = words_in_s[i]
         mapped_word_to_letter = letter_to_word_map.get(letter)
-        if not mapped_word_to_letter:
+        if letter not in letter_to_word_map and word not in letter_to_word_map.values():
             letter_to_word_map[letter] = word
             continue
         if mapped_word_to_letter != word:
-            return False
-        flipped_dict = {v: k for k, v in letter_to_word_map.items()}
-        print(flipped_dict, letter_to_word_map)
-        print({v: k for k, v in flipped_dict.items()}, letter_to_word_map)
-        if {v: k for k, v in flipped_dict.items()} != letter_to_word_map:
             return False
     return True
