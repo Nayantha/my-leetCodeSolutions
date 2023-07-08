98ab6ff8a804ab498eda2f3e5f76ec65cea8b2b0
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-08 11:28:35 +0530

    add codes to create a new file and new test file for that file.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- question_title_to_name.py
+++ question_title_to_name.py
@@ -2,6 +2,10 @@ def main():
     name = input("Enter the question title:")
     file_name = name.strip().replace(" ", "_").lower()
     print(file_name)
+    with open(f"{file_name}.py", "w") as new_solution_file:
+        ...
+    with open(f"./tests/test_{file_name}.py", "w") as new_test_solution_file:
+        ...
     return file_name
 
 
