bf3b01fc8e8d17bba0d2f68fb5405825b90066d6
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-08 14:08:38 +0530

    add test data.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_linked_list_cycle.py
+++ tests/test_linked_list_cycle.py
@@ -1,12 +1,13 @@
+
 from typing import List
 
 import pytest
 
 
-@pytest.mark.parametrize("data, next, expected", [
-    ([3, 2, 0, -4], [1, 2, 3, 1], 1),
-    ([1, 2], [1, 0], 0),
-    ([1], [None], -1)
+@pytest.mark.parametrize("values, next_node_indexes, expected", [
+    ([3, 2, 0, -4], [1, 2, 3, 1], True),
+    ([1, 2], [1, 0], True),
+    ([1], [None], False)
 ])
-def test_has_cycle(data: List[int], next: List[int], expected: int):
-    assert False
+def test_has_cycle(values: List[int], next_node_indexes: List[int], expected: bool):
+    ...
