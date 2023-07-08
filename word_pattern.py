21b56acffe870cb6f7c3e154245244d3974400a3
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-08 11:45:17 +0530

    add a solution.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- word_pattern.py
+++ word_pattern.py
@@ -1,3 +1,12 @@
 # https://leetcode.com/problems/word-pattern/?envType=study-plan-v2&envId=top-interview-150
 def word_pattern(pattern: str, s: str) -> bool:
-    ...
+    letter_to_word_map = dict()
+    words_in_s = s.split()
+    for i, letter in enumerate(pattern):
+        mapped_word_to_letter = letter_to_word_map.get(letter)
+        if not mapped_word_to_letter:
+            letter_to_word_map[letter] = words_in_s[i]
+            continue
+        if mapped_word_to_letter != words_in_s[i]:
+            return False
+    return True
