4a5fbf27123c18e7605a315824f33c3a5c889553
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-09 13:03:05 +0530

    change while loop condition.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- merge_two_sorted_lists.py
+++ merge_two_sorted_lists.py
@@ -15,7 +15,7 @@ def merge_two_lists(list1: Optional[ListNode], list2: Optional[ListNode]) -> Opt
     if not list1:
         return list2
     ref = merged_list = ListNode()
-    while list1 and list2:
+    while list1 or list2:
         if list1.val == list2.val:
             ref.next = list1
             list1 = list1.next
