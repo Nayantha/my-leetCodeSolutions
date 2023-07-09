6c49c81787395ac42ea0cf42a631089ce0582d8a
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-09 13:01:36 +0530

    add a solution.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- merge_two_sorted_lists.py
+++ merge_two_sorted_lists.py
@@ -8,4 +8,33 @@ class ListNode:
 
 
 def merge_two_lists(list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-    ...
+    if not list1 and not list2:
+        return ListNode()
+    if not list2:
+        return list1
+    if not list1:
+        return list2
+    ref = merged_list = ListNode()
+    while list1 and list2:
+        if list1.val == list2.val:
+            ref.next = list1
+            list1 = list1.next
+            ref = ref.next
+
+            ref.next = list2
+            list2 = list2.next
+
+        if list1.val < list2.val:
+            ref = list1
+            list1 = list1.next
+        else:
+            ref = list2
+            list2 = list2.next
+        ref = ref.next
+
+    if not list1:
+        merged_list.next = list2
+    if not list2:
+        merged_list.next = list1
+
+    return merged_list.next
