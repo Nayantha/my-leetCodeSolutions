4fbbe931979fbd7bebd95f379db71bfc674ea486
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-09 13:43:27 +0530

    copy skeleton code form site.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- maximum_depth_of_binary_tree.py
+++ maximum_depth_of_binary_tree.py
@@ -1,1 +1,14 @@
 # https://leetcode.com/problems/maximum-depth-of-binary-tree/?envType=study-plan-v2&envId=top-interview-150
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def max_depth(self, root: Optional[TreeNode]) -> int:
+        ...
