0f1c8ea8a6af53e45951756cbdade95a5a6ae37f
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-09 12:46:20 +0530

    remove code form previous attempt.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- merge_two_sorted_lists.py
+++ merge_two_sorted_lists.py
@@ -1,44 +1,11 @@
 from typing import Optional
 
-#https://leetcode.com/problems/merge-two-sorted-lists/
+# https://leetcode.com/problems/merge-two-sorted-lists/
 class ListNode:
-    def __init__(self, val=0, next=None):
+    def __init__(self, val=0, next_node=None):
         self.val = val
         self.next = next
 
 
-class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        resultList = ListNode()
-        ref = resultList
-        if list1.next is None or list2.next is None:
-            return []
-        if list1.next is None:
-            return list2
-        if list2.next is None:
-            return list1
-        
-        if list1.val <= list2.val:
-            resultList = list1
-            list1 = list1.next
-        else:
-            resultList = list2
-            list2 = list2.next
-        
-        ref = resultList
-        while list1 is not None and list2 is not None:
-            if list1.val <= list2.val:
-                ref.next = list1
-                list1 = list1.next
-            else:
-                ref.next = list2
-                list2 = list2.next
-            ref = ref.next
-        if list1 is None:
-            ref.next = list2
-        
-        if list2 is None:
-            ref.next = list1
-        
-        return resultList
-        
+def merge_two_lists(list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+    ...
