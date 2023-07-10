df378d8cbc1a2fb1267bb221a9a9b1d08432bd06
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-10 14:50:06 +0530

    add file to write solution "https://leetcode.com/problems/same-tree/?envType=study-plan-v2&envId=top-interview-150".
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- same_tree.py
+++ same_tree.py
@@ -1,16 +1,1 @@
-# https://leetcode.com/problems/same-tree/
-from typing import Optional
-
-
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
-
-class Solution:
-    def is_same_tree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        if p is None or q is None:
-            return p == q
-        return p.val == q.val and self.is_same_tree(p.left, q.left) and self.is_same_tree(p.right, q.right)
+# https://leetcode.com/problems/same-tree/?envType=study-plan-v2&envId=top-interview-150
