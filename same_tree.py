d0dcc4e4e7fe718c98a88515559ae3aaf6ac8398
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-10 14:56:48 +0530

    add the final solution.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- same_tree.py
+++ same_tree.py
@@ -1,3 +1,9 @@
 # https://leetcode.com/problems/same-tree/?envType=study-plan-v2&envId=top-interview-150
 def is_same_tree(p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-    ...
+    if not p and not q:
+        return True
+    if not p and q or p and not q:
+        return False
+    if p.val != q.val:
+        return False
+    return is_same_tree(p.left, q.left) and is_same_tree(p.right, q.right)
