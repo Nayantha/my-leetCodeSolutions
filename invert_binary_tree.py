161119e5521283e1a7d0943def8372fcea230270
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-10 15:05:43 +0530

    copy skeleton code form site.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- invert_binary_tree.py
+++ invert_binary_tree.py
@@ -1,1 +1,4 @@
 # https://leetcode.com/problems/invert-binary-tree/?envType=study-plan-v2&envId=top-interview-150
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        ...
