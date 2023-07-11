44a0baf7411b6a80bd9496de5a18a76442755219
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-11 16:39:40 +0530

    add the final solution.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- path_sum.py
+++ path_sum.py
@@ -10,4 +10,9 @@ class TreeNode:
 
 
 def has_path_sum(root: Optional[TreeNode], target_sum: int) -> bool:
-    ...
+    if not root:
+        return False
+    if not root.left and root.right and root.val == target_sum:
+        return True
+    current_sum = target_sum - root.val
+    return has_path_sum(root.left, current_sum) or has_path_sum(root.right, current_sum)
