d19444741224287a5bd9e01012ec76f72d5b933a
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-11 16:22:14 +0530

    copy skeleton code form site.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- path_sum.py
+++ path_sum.py
@@ -1,1 +1,13 @@
 # https://leetcode.com/problems/path-sum/?envType=study-plan-v2&envId=top-interview-150
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def has_path_sum(root: Optional[TreeNode], target_sum: int) -> bool:
+    ...
