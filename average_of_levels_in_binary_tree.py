b52327c5cfcbd9e8b1329507b47d5b542d6296ac
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-14 18:37:57 +0530

    rename dict.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- average_of_levels_in_binary_tree.py
+++ average_of_levels_in_binary_tree.py
@@ -5,19 +5,19 @@ from binary_tree_inorder_traversal import TreeNode
 
 
 def average_of_levels(root: Optional[TreeNode]) -> List[float]:
-    d = {}
+    level_against_sum_dict = {}
 
     def dfs(node=root, level=0):
         if not node:
             return
         level += 1
-        d_v = d.get(level)
+        d_v = level_against_sum_dict.get(level)
         if not d_v:
-            d[level] = node.val
+            level_against_sum_dict[level] = node.val
         else:
-            d[level] = d_v + node.val
+            level_against_sum_dict[level] = d_v + node.val
         dfs(node.left, level + 1)
         dfs(node.right, level + 1)
 
     dfs()
-    return [v / k for k, v in d.items()]
+    return [v / k for k, v in level_against_sum_dict.items()]
