4d042f1e3e87f8af3df22f4e897e32182360a154
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-14 17:47:09 +0530

    copy skeleton code form site.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- average_of_levels_in_binary_tree.py
+++ average_of_levels_in_binary_tree.py
@@ -1,1 +1,8 @@
 # https://leetcode.com/problems/average-of-levels-in-binary-tree/solutions/?envType=study-plan-v2&envId=top-interview-150
+from typing import Optional, List
+
+from binary_tree_inorder_traversal import TreeNode
+
+
+def average_of_levels(self, root: Optional[TreeNode]) -> List[float]:
+    ...
