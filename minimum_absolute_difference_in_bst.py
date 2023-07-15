78df73996cec0c5bf26f9c6d2aebf68548b89bc9
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-15 18:25:50 +0530

    fix exceptions and import required entities.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- minimum_absolute_difference_in_bst.py
+++ minimum_absolute_difference_in_bst.py
@@ -1,2 +1,8 @@
 # https://leetcode.com/problems/minimum-absolute-difference-in-bst/?envType=study-plan-v2&envId=top-interview-150
-def getMinimumDifference(root: Optional[TreeNode]) -> int:
+from typing import Optional
+
+from binary_tree_inorder_traversal import TreeNode
+
+
+def get_minimum_difference(root: Optional[TreeNode]) -> int:
+    ...
