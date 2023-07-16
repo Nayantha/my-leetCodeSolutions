dfa7413aa1df21d041a0a0f4417021d62a1e2d56
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-16 18:46:49 +0530

    add imports.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- convert_sorted_array_to_binary_search_tree.py
+++ convert_sorted_array_to_binary_search_tree.py
@@ -1,3 +1,8 @@
 # https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/?envType=study-plan-v2&envId=top-interview-150
+from typing import List, Optional
+
+from binary_tree_inorder_traversal import TreeNode
+
+
 def sorted_array_to_bst(self, nums: List[int]) -> Optional[TreeNode]:
     ...
