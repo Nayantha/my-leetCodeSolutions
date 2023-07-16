b06f9a215fefea38b4796b87eaea747e3e72b107
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-16 18:44:25 +0530

    rename function according to python3 tradition.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- convert_sorted_array_to_binary_search_tree.py
+++ convert_sorted_array_to_binary_search_tree.py
@@ -1,3 +1,3 @@
 # https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/?envType=study-plan-v2&envId=top-interview-150
-def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+def sorted_array_to_bst(self, nums: List[int]) -> Optional[TreeNode]:
     ...
