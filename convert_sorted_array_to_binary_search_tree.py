04823714bc4da129af7f05121088a1166393ddc2
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-16 19:05:53 +0530

    add a possible solution.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- convert_sorted_array_to_binary_search_tree.py
+++ convert_sorted_array_to_binary_search_tree.py
@@ -5,4 +5,26 @@ from binary_tree_inorder_traversal import TreeNode
 
 
 def sorted_array_to_bst(self, nums: List[int]) -> Optional[TreeNode]:
-    ...
+    def initialize_tree_node(val: int) -> TreeNode:
+        return TreeNode(val=val)
+
+    def assign_node_to_parent(parent: TreeNode, new_node: TreeNode):
+        if new_node.val > parent.val:
+            if not parent.right:
+                parent.right = new_node
+            else:
+                assign_node_to_parent(parent.right, new_node)
+        else:
+            if not parent.left:
+                parent.left = new_node
+            else:
+                assign_node_to_parent(parent.left, new_node)
+
+    root = None
+    for num in nums:
+        node = initialize_tree_node(num)
+        if not node:
+            root = node
+            continue
+
+    return root
