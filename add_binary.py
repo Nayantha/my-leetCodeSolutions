e3cb6e5427b746d4342c8322469c5d332ea2da11
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-17 13:22:24 +0530

    remove unwanted parameter.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- add_binary.py
+++ add_binary.py
@@ -1,3 +1,3 @@
 # https://leetcode.com/problems/add-binary/?envType=study-plan-v2&envId=top-interview-150
-def add_binary(self, a: str, b: str) -> str:
+def add_binary(a: str, b: str) -> str:
     ...
