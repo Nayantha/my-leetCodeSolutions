cade76c42102116f15b98a540a3fe4eaf075af14
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-17 13:30:47 +0530

    add a solution.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- add_binary.py
+++ add_binary.py
@@ -1,3 +1,3 @@
 # https://leetcode.com/problems/add-binary/?envType=study-plan-v2&envId=top-interview-150
 def add_binary(a: str, b: str) -> str:
-    ...
+    return str(bin(int(a, 2) + int(b, 2))).removeprefix("0b")
