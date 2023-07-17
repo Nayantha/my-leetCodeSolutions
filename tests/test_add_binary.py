4081d8d0ac0e6db4e189790a9e4d31f0558afa2c
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-17 13:21:24 +0530

    add test data.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_add_binary.py
+++ tests/test_add_binary.py
@@ -1,2 +1,11 @@
-def test_add_binary():
-    assert False
+import pytest
+
+from add_binary import add_binary
+
+
+@pytest.mark.parametrize("a, b, expectation", [
+    ("11", "1", "100"),
+    ("1010", "1011", "10101")
+])
+def test_add_binary(a: str, b: str, expectation: str):
+    add_binary(a, b)
