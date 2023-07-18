431d610e68f78a977a888455b72959bada406b49
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-18 19:08:47 +0530

    add the solution.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- reverse_bits.py
+++ reverse_bits.py
@@ -1,3 +1,8 @@
 # https://leetcode.com/problems/reverse-bits/?envType=study-plan-v2&envId=top-interview-150
 def reverse_bits(n: int) -> int:
-    ...
+    res = 0
+    for _ in range(32):
+        res = res << 1
+        res += n % 2
+        n = n >> 1
+    return res
