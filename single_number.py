5940c3a9f55ea1c119817a53cb88236423837bab
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-20 09:38:42 +0530

    clean up the code and fix errors.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- single_number.py
+++ single_number.py
@@ -1,2 +1,6 @@
 # https://leetcode.com/problems/single-number/?envType=study-plan-v2&envId=top-interview-150
-def singleNumber(self, nums: List[int]) -> int:
+from typing import List
+
+
+def single_number(self, nums: List[int]) -> int:
+    ...
