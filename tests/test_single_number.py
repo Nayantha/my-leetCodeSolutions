1cb48674e6b5763588e2a5ed3e8b18a0a9383cc8
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-20 09:42:37 +0530

    add test data.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_single_number.py
+++ tests/test_single_number.py
@@ -1,2 +1,12 @@
-def test_single_number():
+from typing import List
+
+import pytest
+
+
+@pytest.mark.parametrize("nums, expected", [
+    ([2, 2, 1], 1),
+    ([4, 1, 2, 1, 2], 4),
+    ([1], 1)
+])
+def test_single_number(nums: List[int], expected: int):
     assert False
