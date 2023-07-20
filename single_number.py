4394368f5cd0b02bfd08f35d5ff0d5fa594467ad
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-20 09:44:12 +0530

    add counter class to get count of the list element.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- single_number.py
+++ single_number.py
@@ -1,6 +1,8 @@
 # https://leetcode.com/problems/single-number/?envType=study-plan-v2&envId=top-interview-150
+from collections import Counter
 from typing import List
 
 
 def single_number(nums: List[int]) -> int:
-    ...
+    num_and_count = Counter(nums)
+    print(num_and_count)
