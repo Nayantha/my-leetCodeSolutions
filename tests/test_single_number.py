6b00273aecb86c69f7d7425f56d2324de4dc25ff
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-20 09:44:52 +0530

    import testing function.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_single_number.py
+++ tests/test_single_number.py
@@ -2,6 +2,8 @@ from typing import List
 
 import pytest
 
+from single_number import single_number
+
 
 @pytest.mark.parametrize("nums, expected", [
     ([2, 2, 1], 1),
@@ -9,4 +11,4 @@ import pytest
     ([1], 1)
 ])
 def test_single_number(nums: List[int], expected: int):
-    assert False
+    single_number(nums)
