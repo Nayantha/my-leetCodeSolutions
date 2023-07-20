c3517f943c3343c4757982d68f98970fae7e6529
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-20 09:50:40 +0530

    simplify the solution.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- single_number.py
+++ single_number.py
@@ -1,10 +1,8 @@
 # https://leetcode.com/problems/single-number/?envType=study-plan-v2&envId=top-interview-150
-from collections import Counter
 from typing import List
 
 
 def single_number(nums: List[int]) -> int:
-    num_and_count = Counter(nums)
-    for num, count in num_and_count.items():
-        if count == 1:
+    for num in nums:
+        if nums.count(num) == 1:
             return num
