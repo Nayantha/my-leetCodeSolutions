45660c2f0a5a89c1f8221d5ffb22548aecdc46c4
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-21 18:37:29 +0530

    add test logic.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_remove_duplicates_from_sorted_array_ii.py
+++ tests/test_remove_duplicates_from_sorted_array_ii.py
@@ -1,2 +1,12 @@
-def test_remove_duplicates():
-    assert False
+from typing import List
+
+import pytest
+
+from remove_duplicates_from_sorted_array_ii import remove_duplicates
+
+
+@pytest.mark.parametrize("nums,expected_nums_list")
+def test_remove_duplicates(nums: List[int], expected_nums_list: List[int]):
+    assert remove_duplicates(nums) == len(expected_nums_list)
+    for i, j in zip(nums, expected_nums_list):
+        assert i == j
