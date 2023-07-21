af287a130edd0a43aefc10ee6c5f10135d588df4
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-21 18:39:15 +0530

    add a new parameter.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_remove_duplicates_from_sorted_array_ii.py
+++ tests/test_remove_duplicates_from_sorted_array_ii.py
@@ -5,8 +5,8 @@ import pytest
 from remove_duplicates_from_sorted_array_ii import remove_duplicates
 
 
-@pytest.mark.parametrize("nums,expected_nums_list")
-def test_remove_duplicates(nums: List[int], expected_nums_list: List[int]):
+@pytest.mark.parametrize("nums,expected_nums_list,expected_nums_list_len")
+def test_remove_duplicates(nums: List[int], expected_nums_list: List[int], expected_nums_list_len: int):
     assert remove_duplicates(nums) == len(expected_nums_list)
     for i, j in zip(nums, expected_nums_list):
         assert i == j
