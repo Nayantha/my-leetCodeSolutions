f6f3dcf219f6961f157112d5cb0b17d5164d04a3
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-21 18:41:45 +0530

    add test data.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_remove_duplicates_from_sorted_array_ii.py
+++ tests/test_remove_duplicates_from_sorted_array_ii.py
@@ -5,7 +5,10 @@ import pytest
 from remove_duplicates_from_sorted_array_ii import remove_duplicates
 
 
-@pytest.mark.parametrize("nums,expected_nums_list,expected_nums_list_len")
+@pytest.mark.parametrize("nums,expected_nums_list,expected_nums_list_len", [
+    ([1, 1, 1, 2, 2, 3], [1, 1, 2, 2, 3], 5),
+    ([0, 0, 1, 1, 1, 1, 2, 3, 3], [0, 0, 1, 1, 2, 3, 3], 7)
+])
 def test_remove_duplicates(nums: List[int], expected_nums_list: List[int], expected_nums_list_len: int):
     assert remove_duplicates(nums) == expected_nums_list_len
     for i, j in zip(nums, expected_nums_list):
