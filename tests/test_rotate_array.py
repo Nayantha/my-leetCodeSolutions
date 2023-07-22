ce20be3c7386ed53ebc0d70fefdb9e32460c6811
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-22 19:17:32 +0530

    import testing function.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_rotate_array.py
+++ tests/test_rotate_array.py
@@ -2,10 +2,12 @@ from typing import List
 
 import pytest
 
+from rotate_array import rotate
+
 
 @pytest.mark.parametrize("nums, k, expected", [
     ([1, 2, 3, 4, 5, 6, 7], 3, [5, 6, 7, 1, 2, 3, 4]),
     ([-1, -100, 3, 99], 2, [3, 99, -1, -100])
 ])
 def test_rotate(nums: List[int], k: int, expected: List[int]):
-    assert False
+    rotate(nums, k)
