4dd394ccddbdc6b089301ffd9b47bac12670318a
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-22 19:21:28 +0530

    add a solution.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- rotate_array.py
+++ rotate_array.py
@@ -3,4 +3,8 @@ from typing import List
 
 
 def rotate(nums: List[int], k: int) -> None:
-    ...
+    nums_len = len(nums)
+    k = k % nums_len
+    list_separation_index = nums_len - k
+    nums = nums[list_separation_index:] + nums[:list_separation_index]
+    print(nums)
