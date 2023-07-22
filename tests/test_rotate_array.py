74178b3588fcf0a2b5390e2d706fb28fbcf47667
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-22 19:10:23 +0530

    add a new parameter.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_rotate_array.py
+++ tests/test_rotate_array.py
@@ -3,6 +3,6 @@ from typing import List
 import pytest
 
 
-@pytest.mark.parametrize("nums, expected")
-def test_rotate(nums: List[int], expected: List[int]):
+@pytest.mark.parametrize("nums, k, expected")
+def test_rotate(nums: List[int], k: int, expected: List[int]):
     assert False
