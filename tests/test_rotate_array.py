88f2872b2669dc26e62f1fbc2ab777903024978c
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-22 19:12:09 +0530

    add test data.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_rotate_array.py
+++ tests/test_rotate_array.py
@@ -3,6 +3,9 @@ from typing import List
 import pytest
 
 
-@pytest.mark.parametrize("nums, k, expected")
+@pytest.mark.parametrize("nums, k, expected", [
+    ([1, 2, 3, 4, 5, 6, 7], 3, [5, 6, 7, 1, 2, 3, 4]),
+    ([-1, -100, 3, 99], 2, [3, 99, -1, -100])
+])
 def test_rotate(nums: List[int], k: int, expected: List[int]):
     assert False
