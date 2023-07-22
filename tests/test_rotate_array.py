ebbc2d88c58e05860429e2cd008ce25dcd3f1cf4
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-22 19:09:45 +0530

    add test framework.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_rotate_array.py
+++ tests/test_rotate_array.py
@@ -1,2 +1,8 @@
-def test_rotate():
+from typing import List
+
+import pytest
+
+
+@pytest.mark.parametrize("nums, expected")
+def test_rotate(nums: List[int], expected: List[int]):
     assert False
