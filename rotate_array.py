44a96881acd88787c78c38f393d71cd252bf67a5
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-22 19:05:06 +0530

    add file to write solution "https://leetcode.com/problems/rotate-array/?envType=study-plan-v2&envId=top-interview-150".
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- rotate_array.py
+++ rotate_array.py
@@ -1,18 +1,1 @@
-# https://leetcode.com/problems/rotate-array/
-
-from typing import List
-
-
-class Solution:
-    def rotate(self, nums: List[int], k: int) -> None:
-        new_array = nums.copy()
-        for i in range(len(nums)):
-            new_array[(k + i) % len(nums)] = nums[i]
-        nums.clear()
-        nums += new_array
-
-
-nums = [-1, -100, 3, 99]
-sol = Solution()
-sol.rotate(nums=nums, k=2)
-print(nums)
+# https://leetcode.com/problems/rotate-array/?envType=study-plan-v2&envId=top-interview-150
