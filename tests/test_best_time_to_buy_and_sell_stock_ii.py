96f01a813d0a248a62a025d75a67f394764fcc27
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-23 09:37:35 +0530

    add test data.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_best_time_to_buy_and_sell_stock_ii.py
+++ tests/test_best_time_to_buy_and_sell_stock_ii.py
@@ -3,6 +3,10 @@ from typing import List
 import pytest
 
 
-@pytest.mark.parametrize("prices, expected")
+@pytest.mark.parametrize("prices, expected", [
+    ([7, 1, 5, 3, 6, 4], 7),
+    ([1, 2, 3, 4, 5], 4),
+    ([7, 6, 4, 3, 1], 0)
+])
 def test_max_profit(prices: List[int], expected: int):
     assert False
