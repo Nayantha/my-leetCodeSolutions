da2270ef39dc0f01a7c866384930cdde66d5fc15
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-23 09:38:17 +0530

    add test logic.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_best_time_to_buy_and_sell_stock_ii.py
+++ tests/test_best_time_to_buy_and_sell_stock_ii.py
@@ -2,6 +2,8 @@ from typing import List
 
 import pytest
 
+from best_time_to_buy_and_sell_stock_ii import max_profit
+
 
 @pytest.mark.parametrize("prices, expected", [
     ([7, 1, 5, 3, 6, 4], 7),
@@ -9,4 +11,4 @@ import pytest
     ([7, 6, 4, 3, 1], 0)
 ])
 def test_max_profit(prices: List[int], expected: int):
-    assert False
+    assert max_profit(prices) == expected
