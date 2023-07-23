5f850015a746180f4196facd3066c9ae8cd3ed10
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-23 09:54:21 +0530

    add a solution.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- best_time_to_buy_and_sell_stock_ii.py
+++ best_time_to_buy_and_sell_stock_ii.py
@@ -3,4 +3,11 @@ from typing import List
 
 
 def max_profit(prices: List[int]) -> int:
-    ...
+    profit = 0
+    buy_day_price = prices[0]
+    for i in range(1, len(prices)):
+        sell_day_price = prices[i]
+        if sell_day_price > buy_day_price:
+            profit += sell_day_price - buy_day_price
+        buy_day_price = sell_day_price
+    return profit
