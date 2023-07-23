60a6b828392e90cd31cbca75f245209605fccc28
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-23 09:32:51 +0530

    clean up the code and fix errors.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- best_time_to_buy_and_sell_stock_ii.py
+++ best_time_to_buy_and_sell_stock_ii.py
@@ -1,2 +1,6 @@
 # https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/?envType=study-plan-v2&envId=top-interview-150
-def maxProfit(self, prices: List[int]) -> int:
+from typing import List
+
+
+def max_profit(self, prices: List[int]) -> int:
+    ...
