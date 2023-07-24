53261e3edb5847f278cde582c332fcd8e0684fed
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-24 15:51:04 +0530

    add second solution.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- jump_game.py
+++ jump_game.py
@@ -10,3 +10,11 @@ def can_jump(nums: List[int]) -> bool:
         current_jump -= 1
         current_jump = max(current_jump, nums[current_index])
     return True
+
+
+def can_jump_ii(nums: list[int]) -> bool:
+    goal_position = len(nums) - 1
+    for current_index in range(len(nums) - 1, -1, -1):
+        if current_index + nums[current_index] >= goal_position:
+            goal_position = current_index
+    return goal_position == 0
