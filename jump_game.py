7891df9d65239b26b59e84b16b989a3821b583d9
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-24 11:41:34 +0530

    add an if to check the jump is 0.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- jump_game.py
+++ jump_game.py
@@ -5,7 +5,7 @@ from typing import List
 def can_jump(nums: List[int]) -> bool:
     current_jump = nums[0]
     for current_index in range(1, len(nums)):
-        if nums[current_index] == 0:
+        if current_jump == 0:
             return False
         current_jump -= 1
         current_jump = max(current_jump, nums[current_index])
