cda5953c58e1d1760209bbbc8ef2b02444091750
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-24 11:22:54 +0530

    add test data.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_jump_game.py
+++ tests/test_jump_game.py
@@ -3,6 +3,9 @@ from typing import List
 import pytest
 
 
-@pytest.mark.parametrize("nums, expected")
+@pytest.mark.parametrize("nums, expected", [
+    ([2, 3, 1, 1, 4], True),
+    ([3, 2, 1, 0, 4], False)
+])
 def test_can_jump(nums: List[int], expected: bool):
     assert False
