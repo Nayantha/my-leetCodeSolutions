17bc136e2a47ae937259c781a1e8340d1b6b3a3a
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-24 11:24:00 +0530

    add test logic.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_jump_game.py
+++ tests/test_jump_game.py
@@ -2,10 +2,12 @@ from typing import List
 
 import pytest
 
+from jump_game import can_jump
+
 
 @pytest.mark.parametrize("nums, expected", [
     ([2, 3, 1, 1, 4], True),
     ([3, 2, 1, 0, 4], False)
 ])
 def test_can_jump(nums: List[int], expected: bool):
-    assert False
+    assert can_jump(nums) == expected
