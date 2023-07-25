21c9af1be4123a137282b28e9ea43c52ad0a645c
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-25 17:35:19 +0530

    add test data.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_jump_game_ii.py
+++ tests/test_jump_game_ii.py
@@ -3,6 +3,9 @@ import pytest
 from jump_game_ii import jump
 
 
-@pytest.mark.parametrize("nums, expected")
+@pytest.mark.parametrize("nums, expected", [
+    ([2, 3, 1, 1, 4], 2),
+    ([2, 3, 0, 1, 4], 2)
+])
 def test_jump(nums: list[int], expected: int):
     assert jump(nums) == int
