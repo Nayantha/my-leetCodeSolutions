adf9954a98ae83fe90c6d010c2e81caf58cfa484
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-25 17:29:02 +0530

    add test logic.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_jump_game_ii.py
+++ tests/test_jump_game_ii.py
@@ -1,6 +1,8 @@
 import pytest
 
+from jump_game_ii import jump
+
 
 @pytest.mark.parametrize("nums, expected")
 def test_jump(nums: list[int], expected: int):
-    assert False
+    assert jump(nums) == int
