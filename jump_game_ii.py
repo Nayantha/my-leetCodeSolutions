33db04289371082fa72a0dad3cb45b67fc6a27d0
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-25 17:35:38 +0530

    add a solution.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- jump_game_ii.py
+++ jump_game_ii.py
@@ -1,3 +1,11 @@
 # https://leetcode.com/problems/jump-game-ii/?envType=study-plan-v2&envId=top-interview-150
 def jump(nums: list[int]) -> int:
-    ...
+    jumps = l = r = 0
+    while r < len(nums) - 1:
+        furthest_jump = 0
+        for i in range(l, r + 1):
+            furthest_jump = max(furthest_jump, i + nums[i])
+        l = r + 1
+        r = furthest_jump
+        jumps += 1
+    return jumps
