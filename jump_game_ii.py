1eabd7a273253a5ff9f9baa9f140f7d507689ab5
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-25 17:40:38 +0530

    rename variables to make sense.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- jump_game_ii.py
+++ jump_game_ii.py
@@ -1,11 +1,11 @@
 # https://leetcode.com/problems/jump-game-ii/?envType=study-plan-v2&envId=top-interview-150
 def jump(nums: list[int]) -> int:
-    jumps = l = r = 0
-    while r < len(nums) - 1:
+    jumps = left_most_index_of_jump = right_most_index_of_jump = 0
+    while right_most_index_of_jump < len(nums) - 1:
         furthest_jump = 0
-        for i in range(l, r + 1):
+        for i in range(left_most_index_of_jump, right_most_index_of_jump + 1):
             furthest_jump = max(furthest_jump, i + nums[i])
-        l = r + 1
-        r = furthest_jump
+        left_most_index_of_jump = right_most_index_of_jump + 1
+        right_most_index_of_jump = furthest_jump
         jumps += 1
     return jumps
