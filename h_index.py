9c0ece94f423855bf18552fcd6fe5646367dc6bb
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-26 08:24:38 +0530

    add another solution.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- h_index.py
+++ h_index.py
@@ -1,3 +1,6 @@
 # https://leetcode.com/problems/h-index/?envType=study-plan-v2&envId=top-interview-150
 def h_index(citations: list[int]) -> int:
-    return sorted(citations)[len(citations) // 2]
+    citations = sorted(citations)
+    for index, citation in enumerate(citations):
+        if citation >= len(citations) - index:
+            return len(citations) - index
