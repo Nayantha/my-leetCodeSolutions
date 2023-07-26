471af9262ebaf789aa955966bf243609496ab6ff
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-26 07:56:10 +0530

    add comment saying problem link.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- h-index.py
+++ h-index.py
@@ -1,1 +1,1 @@
-https: // leetcode.com / problems / h - index /?envType = study - plan - v2 & envId = top - interview - 150
+# https://leetcode.com/problems/h-index/?envType=study-plan-v2&envId=top-interview-150
