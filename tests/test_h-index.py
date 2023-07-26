391cf9132d67bf7f53f82e910acc38bc1d712b8a
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-26 08:03:24 +0530

    add test logic.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_h-index.py
+++ tests/test_h-index.py
@@ -1,6 +1,8 @@
 import pytest
 
+from h_index import h_index
 
-@pytest.mark.parametrize("nums, expected")
-def test_h_index(nums: list[int], expected: int):
-    assert False
+
+@pytest.mark.parametrize("citations, expected")
+def test_h_index(citations: list[int], expected: int):
+    assert h_index(citations) == expected
