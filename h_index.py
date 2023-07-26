234b0c14e4d18c35ca88ee717dead080ae76b3a1
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-26 08:04:55 +0530

    add a solution.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- h_index.py
+++ h_index.py
@@ -1,3 +1,3 @@
 # https://leetcode.com/problems/h-index/?envType=study-plan-v2&envId=top-interview-150
 def h_index(citations: list[int]) -> int:
-    ...
+    return sorted(citations)[len(citations) // 2]
