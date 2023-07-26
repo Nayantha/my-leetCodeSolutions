48ba9265cffee1213fef5471027e22a33cc5c52a
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-26 08:05:28 +0530

    add test data.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_h-index.py
+++ tests/test_h-index.py
@@ -3,6 +3,9 @@ import pytest
 from h_index import h_index
 
 
-@pytest.mark.parametrize("citations, expected")
+@pytest.mark.parametrize("citations, expected", [
+    ([3, 0, 6, 1, 5], 3),
+    ([1, 3, 1], 1)
+])
 def test_h_index(citations: list[int], expected: int):
     assert h_index(citations) == expected
