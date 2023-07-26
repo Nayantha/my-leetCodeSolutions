212143ce3a97bf20aced218b3459fd17887b0413
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-26 08:46:32 +0530

    add binary search solution.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- h_index.py
+++ h_index.py
@@ -8,4 +8,16 @@ def h_index(citations: list[int]) -> int:
 
 
 def h_index_ii(citations: list[int]) -> int:
-    ...
+    total_num_of_citations = len(citations)
+    left_pointer = 0
+    right_pointer = total_num_of_citations
+    citations.sort()
+    while left_pointer < right_pointer:
+        mid_point = left_pointer + (right_pointer + left_pointer) // 2
+        if citations[mid_point] == total_num_of_citations - mid_point:
+            return citations[mid_point]
+        elif citations[mid_point] > total_num_of_citations - mid_point:
+            right_pointer = mid_point - 1
+        else:
+            left_pointer = mid_point + 1
+    return total_num_of_citations - left_pointer
