ffafafd7379e9c8507e1b959dbcf459e0aecaaa4
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-27 10:13:09 +0530

    copy skeleton code form site.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- insert_delete_get_random.py
+++ insert_delete_get_random.py
@@ -1,1 +1,10 @@
 # https://leetcode.com/problems/insert-delete-getrandom-o1/?envType=study-plan-v2&envId=top-interview-150
+class RandomizedSet:
+
+    def __init__(self):
+
+    def insert(self, val: int) -> bool:
+
+    def remove(self, val: int) -> bool:
+
+    def getRandom(self) -> int:
