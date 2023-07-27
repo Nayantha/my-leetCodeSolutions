b9fd69ee91cd7ebbc362973df78901b32bb843d4
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-27 11:22:19 +0530

    fix the remove logic.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- insert_delete_get_random.py
+++ insert_delete_get_random.py
@@ -39,13 +39,13 @@ class RandomizedSet2:
 
     def remove(self, val: int) -> bool:
         if val in self.num_map:
-            self.num_list[len(self.num_list)], self.num_list[self.num_map[val]] \
-                = self.num_list[self.num_map[val]], self.num_list[
-                len(self.num_list)]
+            index_of_value_to_be_removed = self.num_map[val]
+            last_value_of_list = self.num_list[-1]
+            self.num_list[index_of_value_to_be_removed] = last_value_of_list
+            # replace the last value with val if the set need to catch that value in future
+            # self.num_list[-1] = val
             self.num_list.pop()
-            self.num_map[
-                self.num_list[self.num_map[val]]] = self.num_list.index(
-                self.num_map[val])
-            # self.num_map.
+            self.num_map[last_value_of_list] = index_of_value_to_be_removed
+            del self.num_map[val]
             return True
         return False
