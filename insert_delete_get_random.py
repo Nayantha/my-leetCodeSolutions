f34f7d35e29aa4e443d34d058de7d9497cebb765
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-27 10:13:48 +0530

    clean up the code and fix errors.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- insert_delete_get_random.py
+++ insert_delete_get_random.py
@@ -2,9 +2,13 @@
 class RandomizedSet:
 
     def __init__(self):
+        ...
 
     def insert(self, val: int) -> bool:
+        ...
 
     def remove(self, val: int) -> bool:
+        ...
 
-    def getRandom(self) -> int:
+    def get_random(self) -> int:
+        ...
