87c6f089e748dfb3631035bdd9e77d14a4979cc4
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-28 18:58:59 +0530

    add test data.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_product_of_array_except_self.py
+++ tests/test_product_of_array_except_self.py
@@ -1,6 +1,9 @@
 import pytest
 
 
-@pytest.mark.parametrize("nums, expected")
+@pytest.mark.parametrize("nums, expected", [
+    ([1, 2, 3, 4], [24, 12, 8, 6]),
+    ([-1, 1, 0, -3, 3], [0, 0, 9, 0, 0])
+])
 def test_product_except_self(nums: list[int], expected: list[int]):
     assert False
