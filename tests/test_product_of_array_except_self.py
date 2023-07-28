9c7c3eb3eee8fde33b20d91c058c67a1e138ccea
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-28 19:09:54 +0530

    add test logic.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_product_of_array_except_self.py
+++ tests/test_product_of_array_except_self.py
@@ -1,9 +1,11 @@
 import pytest
 
+from product_of_array_except_self import product_except_self
+
 
 @pytest.mark.parametrize("nums, expected", [
     ([1, 2, 3, 4], [24, 12, 8, 6]),
     ([-1, 1, 0, -3, 3], [0, 0, 9, 0, 0])
 ])
 def test_product_except_self(nums: list[int], expected: list[int]):
-    assert False
+    assert product_except_self(nums) == expected
