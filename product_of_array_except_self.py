52fddb6a89e081b08b4c718a2ed584e34b0a83b7
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-28 19:08:37 +0530

    add a solution logic.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- product_of_array_except_self.py
+++ product_of_array_except_self.py
@@ -1,3 +1,15 @@
 # https://leetcode.com/problems/product-of-array-except-self/
+import numpy as np
+
+
 def product_except_self(nums: list[int]) -> list[int]:
-    ...
+    product_list = []
+    for index, current_num in enumerate(nums):
+        product_list.append(
+            np.product(
+                np.array(
+                    [num for num in nums if nums.index(num) != index and num != current_num]
+                )
+            )
+        )
+    return product_list
