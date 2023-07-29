fd574e6c24b4d76caca2c9ae64f7f5967f8caf89
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-29 21:33:13 +0530

    add a solution.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- gas_station.py
+++ gas_station.py
@@ -1,3 +1,9 @@
 # https://leetcode.com/problems/gas-station/?envType=study-plan-v2&envId=top-interview-150
 def can_complete_circuit(gas: list[int], cost: list[int]) -> int:
-    ...
+    # remain [i] = gas[i] - cost[i]
+    remaining_gas = [gas_of_i_th - cost_of_i_th for gas_of_i_th, cost_of_i_th in zip(gas, cost)]
+    if sum(remaining_gas) >= 0:
+        for i in range(len(remaining_gas)):
+            if remaining_gas[i] > 0:
+                return i
+    return -1
