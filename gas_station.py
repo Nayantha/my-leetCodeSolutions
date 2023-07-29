15c051859cde87e1cf0a190a0ea6f600022ca621
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-29 21:48:00 +0530

    complete the final accepted solution.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- gas_station.py
+++ gas_station.py
@@ -3,7 +3,13 @@ def can_complete_circuit(gas: list[int], cost: list[int]) -> int:
     # remain [i] = gas[i] - cost[i]
     remaining_gas = [gas_of_i_th - cost_of_i_th for gas_of_i_th, cost_of_i_th in zip(gas, cost)]
     if sum(remaining_gas) >= 0:
+        total = 0
+        start_pos = 0
         for i in range(len(remaining_gas)):
-            if remaining_gas[i] > 0:
-                return i
+            total += remaining_gas[i]
+            if total < 0:
+                total = 0
+                start_pos = i + 1
+                continue
+        return start_pos
     return -1
