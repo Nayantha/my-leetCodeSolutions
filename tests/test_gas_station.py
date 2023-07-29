271addd533c6f003755a61f734c63d62d475a857
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-29 21:16:04 +0530

    add pytest test framework.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_gas_station.py
+++ tests/test_gas_station.py
@@ -1,2 +1,6 @@
-def test_can_complete_circuit():
+import pytest
+
+
+@pytest.mark.parametrize("gas, cost, expectation")
+def test_can_complete_circuit(gas: list[int], cost: list[int], expectation: int):
     assert False
