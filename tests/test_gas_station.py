c18cd40ec4692424712230ed1f3cd205363c4ec1
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-29 21:18:29 +0530

    add test logic.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_gas_station.py
+++ tests/test_gas_station.py
@@ -1,9 +1,11 @@
 import pytest
 
+from gas_station import can_complete_circuit
+
 
 @pytest.mark.parametrize("gas, cost, expectation", [
     ([1, 2, 3, 4, 5], [3, 4, 5, 1, 2], 3),
     ([2, 3, 4], [3, 4, 3], 1)
 ])
 def test_can_complete_circuit(gas: list[int], cost: list[int], expectation: int):
-    assert False
+    assert can_complete_circuit(gas, cost) == expectation
