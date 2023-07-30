706df2c660787a1a3351710ee0df85f0f8a3309f
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-30 11:19:43 +0530

    add a new test case data.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_candy.py
+++ tests/test_candy.py
@@ -5,7 +5,8 @@ from candy import candy
 
 @pytest.mark.parametrize("ratings, expected", [
     ([1, 0, 2], 5),
-    ([1, 2, 2], 4)
+    ([1, 2, 2], 4),
+    ([1, 3, 2, 2, 1], 7)
 ])
 def test_candy(ratings: list[int], expected: int):
     assert candy(ratings) == expected
