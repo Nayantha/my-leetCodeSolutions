057028ef30188693cfeec73639ece2037939c7bf
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-30 11:07:08 +0530

    add test data.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_candy.py
+++ tests/test_candy.py
@@ -1,6 +1,9 @@
 import pytest
 
 
-@pytest.mark.parametrize()
-def test_candy():
+@pytest.mark.parametrize("ratings, expected", [
+    ([1, 0, 2], 5),
+    ([1, 2, 2], 4)
+])
+def test_candy(ratings: list[int], expected: int):
     assert False
