611b8f6e6cf5991df9b1e5175d9614a451c0660c
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-30 11:47:19 +0530

    edit the solution.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- candy.py
+++ candy.py
@@ -2,8 +2,13 @@
 def candy(ratings: list[int]) -> int:
     candy_dist = [1] * (len(ratings))
     for index in range(len(ratings) - 1):
-        if ratings[index] < ratings[index + 1] and candy_dist[index] <= candy_dist[index + 1]:
-            candy_dist[index + 1] += 1
+        if ratings[index] < ratings[index + 1] and candy_dist[index] >= candy_dist[index + 1]:
+            candy_dist[index + 1] += candy_dist[index]
         elif ratings[index] > ratings[index + 1] and candy_dist[index] <= candy_dist[index + 1]:
-            candy_dist[index] += 1
+            candy_dist[index] += candy_dist[index]
+    for index in range(len(ratings) - 1, 0, -1):
+        if ratings[index] < ratings[index - 1] and candy_dist[index] > candy_dist[index - 1]:
+            candy_dist[index - 1] += candy_dist[index]
+        elif ratings[index] > ratings[index - 1] and candy_dist[index] <= candy_dist[index - 1]:
+            candy_dist[index] += candy_dist[index]
     return sum(candy_dist)
