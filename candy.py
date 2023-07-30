62e58f9a20be894de72188c6806656c8bb8887b2
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-30 12:28:19 +0530

    add the final solution.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- candy.py
+++ candy.py
@@ -1,16 +1,13 @@
 # https://leetcode.com/problems/candy/?envType=study-plan-v2&envId=top-interview-150
 def candy(ratings: list[int]) -> int:
-    candy_dist = [1] * (len(ratings))
-    for index in range(1, len(ratings) - 1):
-        # prev = ratings[index - 1]
-        # curr = ratings[index]
-        # next = ratings[index + 1]
-        if ratings[index - 1] < ratings[index] and candy_dist[index - 1] >= candy_dist[index]:
-            candy_dist[index] += candy_dist[index - 1]
-        elif ratings[index - 1] > ratings[index] and candy_dist[index - 1] <= candy_dist[index]:
-            candy_dist[index - 1] += candy_dist[index]
-        if ratings[index] < ratings[index + 1] and candy_dist[index - 1] <= candy_dist[index]:
-            candy_dist[index + 1] += candy_dist[index]
-        elif ratings[index] > ratings[index + 1] and candy_dist[index - 1] >= candy_dist[index]:
-            candy_dist[index] += candy_dist[index + 1]
+    total_num_of_ratings = len(ratings)
+    candy_dist = [1] * total_num_of_ratings
+
+    for i in range(1, total_num_of_ratings):
+        if ratings[i] > ratings[i - 1]:
+            candy_dist[i] = candy_dist[i - 1] + 1
+
+    for i in range(total_num_of_ratings - 2, -1, -1):
+        if ratings[i] > ratings[i + 1]:
+            candy_dist[i] = max(candy_dist[i + 1] + 1, candy_dist[i])
     return sum(candy_dist)
