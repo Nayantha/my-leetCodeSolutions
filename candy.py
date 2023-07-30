30a92cea93ba2f3850a5e6d3af0e395ac1e705d1
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-30 11:15:23 +0530

    add a solution.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- candy.py
+++ candy.py
@@ -1,3 +1,9 @@
 # https://leetcode.com/problems/candy/?envType=study-plan-v2&envId=top-interview-150
 def candy(ratings: list[int]) -> int:
-    ...
+    candy_dist = [1] * (len(ratings))
+    for index in range(len(ratings) - 1):
+        if ratings[index] < ratings[index + 1]:
+            candy_dist[index + 1] += 1
+        elif ratings[index] > ratings[index + 1]:
+            candy_dist[index] += 1
+    return sum(candy_dist)
