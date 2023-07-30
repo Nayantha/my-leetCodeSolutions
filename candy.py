7e97d006e427210c3a9efa4c2f12df155edc0d85
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-30 12:03:40 +0530

    add another solution.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- candy.py
+++ candy.py
@@ -1,14 +1,16 @@
 # https://leetcode.com/problems/candy/?envType=study-plan-v2&envId=top-interview-150
 def candy(ratings: list[int]) -> int:
     candy_dist = [1] * (len(ratings))
-    for index in range(len(ratings) - 1):
-        if ratings[index] < ratings[index + 1] and candy_dist[index] >= candy_dist[index + 1]:
-            candy_dist[index + 1] += candy_dist[index]
-        elif ratings[index] > ratings[index + 1] and candy_dist[index] <= candy_dist[index + 1]:
-            candy_dist[index] += candy_dist[index]
-    for index in range(len(ratings) - 1, 0, -1):
-        if ratings[index] < ratings[index - 1] and candy_dist[index] > candy_dist[index - 1]:
+    for index in range(1, len(ratings) - 1):
+        # prev = ratings[index - 1]
+        # curr = ratings[index]
+        # next = ratings[index + 1]
+        if ratings[index - 1] < ratings[index] and candy_dist[index - 1] >= candy_dist[index]:
+            candy_dist[index] += candy_dist[index - 1]
+        elif ratings[index - 1] > ratings[index] and candy_dist[index - 1] <= candy_dist[index]:
             candy_dist[index - 1] += candy_dist[index]
-        elif ratings[index] > ratings[index - 1] and candy_dist[index] <= candy_dist[index - 1]:
-            candy_dist[index] += candy_dist[index]
+        if ratings[index] < ratings[index + 1] and candy_dist[index - 1] <= candy_dist[index]:
+            candy_dist[index + 1] += candy_dist[index]
+        elif ratings[index] > ratings[index + 1] and candy_dist[index - 1] >= candy_dist[index]:
+            candy_dist[index] += candy_dist[index + 1]
     return sum(candy_dist)
