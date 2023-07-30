473fb1f9c17364ff9be55246dcbfd8d61071de8c
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-30 11:21:01 +0530

    add compare candy.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- candy.py
+++ candy.py
@@ -2,8 +2,8 @@
 def candy(ratings: list[int]) -> int:
     candy_dist = [1] * (len(ratings))
     for index in range(len(ratings) - 1):
-        if ratings[index] < ratings[index + 1]:
+        if ratings[index] < ratings[index + 1] and candy_dist[index] <= candy_dist[index + 1]:
             candy_dist[index + 1] += 1
-        elif ratings[index] > ratings[index + 1]:
+        elif ratings[index] > ratings[index + 1] and candy_dist[index] >= candy_dist[index + 1]:
             candy_dist[index] += 1
     return sum(candy_dist)
