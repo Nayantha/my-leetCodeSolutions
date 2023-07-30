6d6c3bdd2b656342e7ac923a7f36480aef02dc32
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-07-30 11:08:18 +0530

    import function to test.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_candy.py
+++ tests/test_candy.py
@@ -1,9 +1,11 @@
 import pytest
 
+from candy import candy
+
 
 @pytest.mark.parametrize("ratings, expected", [
     ([1, 0, 2], 5),
     ([1, 2, 2], 4)
 ])
 def test_candy(ratings: list[int], expected: int):
-    assert False
+    candy(ratings)
