18cdba6d8216cb277aa7445b274b0f0191c30ef3
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-01 11:57:24 +0530

    add a solution.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- integer_to_roman.py
+++ integer_to_roman.py
@@ -1,3 +1,26 @@
 # https://leetcode.com/problems/integer-to-roman/?envType=study-plan-v2&envId=top-interview-150
 def int_to_roman(num: int) -> str:
-    ...
+    result_str = ""
+    while num > 0:
+        if num < 1000:
+            num -= 1000
+            result_str += "M"
+        elif num < 500:
+            num -= 500
+            result_str += "D"
+        elif num < 100:
+            num -= 100
+            result_str += "C"
+        elif num < 50:
+            num -= 50
+            result_str += "L"
+        elif num < 10:
+            num -= 10
+            result_str += "X"
+        elif num < 5:
+            num -= 5
+            result_str += "V"
+        else:
+            num -= 1
+            result_str += "I"
+    return result_str
