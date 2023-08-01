8edd0d3757f608631e8406bb8c400f69e96bfb18
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-01 11:49:10 +0530

    add test data.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_integer_to_roman.py
+++ tests/test_integer_to_roman.py
@@ -1,6 +1,10 @@
 import pytest
 
 
-@pytest.mark.parametrize("nums, expected")
+@pytest.mark.parametrize("nums, expected", [
+    (3, "III"),
+    (58, "LVIII"),
+    (1994, "MCMXCIV")
+])
 def test_int_to_roman(nums: int, expected: str):
     assert False
