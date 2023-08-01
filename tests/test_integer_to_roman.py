892ce37432c2cae5b6f53f727b4f2c0bdab08f33
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-01 12:03:43 +0530

    add another test case.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_integer_to_roman.py
+++ tests/test_integer_to_roman.py
@@ -6,7 +6,8 @@ from integer_to_roman import int_to_roman
 @pytest.mark.parametrize("num, expected", [
     (3, "III"),
     (58, "LVIII"),
-    (1994, "MCMXCIV")
+    (1994, "MCMXCIV"),
+    (4, "IV"),
 ])
 def test_int_to_roman(num: int, expected: str):
     int_to_roman(num)
