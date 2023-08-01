1999a4c6f9dfa4af05a494ee6deaf01c95aa5866
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-01 11:53:11 +0530

    fix variable name.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_integer_to_roman.py
+++ tests/test_integer_to_roman.py
@@ -3,10 +3,10 @@ import pytest
 from integer_to_roman import int_to_roman
 
 
-@pytest.mark.parametrize("nums, expected", [
+@pytest.mark.parametrize("num, expected", [
     (3, "III"),
     (58, "LVIII"),
     (1994, "MCMXCIV")
 ])
-def test_int_to_roman(nums: int, expected: str):
-    int_to_roman(nums)
+def test_int_to_roman(num: int, expected: str):
+    int_to_roman(num)
