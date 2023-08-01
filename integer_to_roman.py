90b2bfb30871d419cc8c5b0cb502ec5c5d803b90
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-01 12:45:43 +0530

    add the final solution.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- integer_to_roman.py
+++ integer_to_roman.py
@@ -1,50 +1,23 @@
 # https://leetcode.com/problems/integer-to-roman/?envType=study-plan-v2&envId=top-interview-150
 def int_to_roman(num: int) -> str:
     result_str = ""
-    while num > 0:
-        if num > 1000:
-            num -= 1000
-            result_str += "M"
-        elif num > 500:
-            num -= 500
-            result_str += "D"
-        elif num > 100:
-            num -= 100
-            result_str += "C"
-        elif num > 50:
-            num -= 50
-            result_str += "L"
-        elif num > 10:
-            num -= 10
-            result_str += "X"
-        elif num > 5:
-            num -= 5
-            result_str += "V"
-        else:
-            num -= 1
-            result_str += "I"
-    if "IIII" in result_str:
-        result_str = result_str.replace("IIII", "IV")
-    if "VIV" in result_str:
-        result_str = result_str.replace("VIV", "IX")
-    if "XXXIXI" in result_str:
-        result_str = result_str.replace("XXXIXI", "XL")
-    if "LXXXIXI" in result_str:
-        result_str = result_str.replace("LXXXIXI", "XC")
-    if "LXL" in result_str:
-        result_str = result_str.replace("LXL", "XC")
-    if "LXXXX" in result_str:
-        result_str = result_str.replace("LXXXX", "XC")
-    if "CCCXCIXI" in result_str:
-        result_str = result_str.replace("CCCXCIXI", "CD")
-    if "CCCLXXL" in result_str:
-        result_str = result_str.replace("CCCLXXL", "CD")
-    if "DCCCXCIXI" in result_str:
-        result_str = result_str.replace("DCCCXCIXI", "CM")
-    if "DCCCLXXL" in result_str:
-        result_str = result_str.replace("DCCCLXXL", "CM")
-    if "DCD" in result_str:
-        result_str = result_str.replace("DCD", "CM")
-    if "DCCCC" in result_str:
-        result_str = result_str.replace("DCCCC", "CM")
+    int_to_roman_dict = {
+        1: "I",
+        4: "IV",
+        5: "V",
+        9: "IX",
+        10: "X",
+        40: "XL",
+        50: "L",
+        90: "XC",
+        100: "C",
+        400: "CD",
+        500: "D",
+        900: "CM",
+        1000: "M"
+    }
+    for roman_mirror_int in list(int_to_roman_dict.keys())[::-1]:
+        while num >= roman_mirror_int:
+            result_str += int_to_roman_dict[roman_mirror_int]
+            num -= roman_mirror_int
     return result_str
