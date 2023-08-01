ef110bc1c4ee177ae8479f9da71b2c53c4a12196
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-01 12:15:05 +0530

    Revert "remove the special case checkers."
    This reverts commit 70a4eade62fa4948b682fdad417d1523ad31c530.

--- integer_to_roman.py
+++ integer_to_roman.py
@@ -5,21 +5,39 @@ def int_to_roman(num: int) -> str:
         if num > 1000:
             num -= 1000
             result_str += "M"
+        elif num > 900:
+            num -= 900
+            result_str += "CM"
         elif num > 500:
             num -= 500
             result_str += "D"
+        elif num > 400:
+            num -= 400
+            result_str += "CD"
         elif num > 100:
             num -= 100
             result_str += "C"
+        elif num > 90:
+            num -= 90
+            result_str += "XC"
         elif num > 50:
             num -= 50
             result_str += "L"
+        elif num > 40:
+            num -= 40
+            result_str += "XL"
         elif num > 10:
             num -= 10
             result_str += "X"
+        elif num > 9:
+            num -= 9
+            result_str += "IX"
         elif num > 5:
             num -= 5
             result_str += "V"
+        elif num > 4:
+            num -= 4
+            result_str += "IV"
         else:
             num -= 1
             result_str += "I"
