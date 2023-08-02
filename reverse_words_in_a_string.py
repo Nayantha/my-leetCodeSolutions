05f79a1d9c2426821de48470760a7b46cc51859a
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-02 13:22:29 +0530

    add another possible solution.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- reverse_words_in_a_string.py
+++ reverse_words_in_a_string.py
@@ -1,3 +1,15 @@
 # https://leetcode.com/problems/reverse-words-in-a-string/?envType=study-plan-v2&envId=top-interview-150
 def reverse_words(s: str) -> str:
     return " ".join(s.strip().split()[::-1])
+
+
+def reverse_words_ii(s: str) -> str:
+    res = []
+    word = ""
+    for letter in s:
+        if letter == " ":
+            if not word:
+                res.append(word)
+            word = ""
+        word += letter
+    return " ".join(res[::-1])
