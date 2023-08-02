0e0a2d2197b810dce05707cccd6bb53540725b0f
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-02 13:18:16 +0530

    add a solution.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- reverse_words_in_a_string.py
+++ reverse_words_in_a_string.py
@@ -1,3 +1,3 @@
 # https://leetcode.com/problems/reverse-words-in-a-string/?envType=study-plan-v2&envId=top-interview-150
 def reverse_words(s: str) -> str:
-    ...
+    return " ".join(s.strip().split()[::-1])
