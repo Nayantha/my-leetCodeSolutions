87564abe3cf79215a1ef873bd540b863642603dc
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-02 13:16:21 +0530

    add test data.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_reverse_words_in_a_string.py
+++ tests/test_reverse_words_in_a_string.py
@@ -1,6 +1,10 @@
 import pytest
 
 
-@pytest.mark.parametrize("s, expected")
+@pytest.mark.parametrize("s, expected", [
+    ("the sky is blue", "blue is sky the"),
+    ("  hello world  ", "world hello"),
+    ("a good   example", "example good a")
+])
 def test_reverse_words(s: str, expected: str):
     assert False
