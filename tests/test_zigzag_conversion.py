56973a9a2a400cf0d83df0a8e98d626294c1788e
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-03 16:58:36 +0530

    add test data.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_zigzag_conversion.py
+++ tests/test_zigzag_conversion.py
@@ -3,6 +3,11 @@ import pytest
 from zigzag_conversion import convert
 
 
-@pytest.mark.parametrize("s, num_rows, expected")
+@pytest.mark.parametrize("s, num_rows, expected", [
+    ("PAYPALISHIRING", 1, "PAYPALISHIRING"),
+    ("PAYPALISHIRING", 3, "PAHNAPLSIIGYIR"),
+    ("PAYPALISHIRING", 4, "PINALSIGYAHRPI"),
+    ("A", 1, "A")
+])
 def test_convert(s: str, num_rows: int, expected: str):
     assert convert(s, num_rows) == expected
