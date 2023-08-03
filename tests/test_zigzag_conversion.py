0d23877d3bcdd359f59a496717d3b8b6a6b09851
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-03 16:56:17 +0530

    add test logic and final output parameter.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_zigzag_conversion.py
+++ tests/test_zigzag_conversion.py
@@ -1,6 +1,8 @@
 import pytest
 
+from zigzag_conversion import convert
 
-@pytest.mark.parametrize("s, num_rows")
-def test_convert(s: str, num_rows: int):
-    assert False
+
+@pytest.mark.parametrize("s, num_rows, expected")
+def test_convert(s: str, num_rows: int, expected: str):
+    assert convert(s, num_rows) == expected
