47259993b1be334d0a87d30ec838c3ecc9c68ea9
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-03 17:09:09 +0530

    modify the base case condition.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- zigzag_conversion.py
+++ zigzag_conversion.py
@@ -1,6 +1,6 @@
 # https://leetcode.com/problems/zigzag-conversion/?envType=study-plan-v2&envId=top-interview-150
 def convert(s: str, num_rows: int) -> str:
-    if num_rows:
+    if num_rows == 1:
         return s
     zigzag_str = ""
     for row in range(num_rows):
