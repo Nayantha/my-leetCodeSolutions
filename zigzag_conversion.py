8784ba0d6a1300268954759fa59d2ee7ca2e2deb
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-03 17:00:16 +0530

    add a base case to solution logic.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- zigzag_conversion.py
+++ zigzag_conversion.py
@@ -1,3 +1,4 @@
 # https://leetcode.com/problems/zigzag-conversion/?envType=study-plan-v2&envId=top-interview-150
 def convert(s: str, num_rows: int) -> str:
-    ...
+    if num_rows:
+        return s
