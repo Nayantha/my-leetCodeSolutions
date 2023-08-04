540e12e602603ba8c9cd1e8379d5f294300da4cd
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-04 18:31:23 +0530

    add test data.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_two_sum_ii.py
+++ tests/test_two_sum_ii.py
@@ -1,6 +1,10 @@
 import pytest
 
 
-@pytest.mark.parametrize("numbers, target, expected")
+@pytest.mark.parametrize("numbers, target, expected", [
+    ([2, 7, 11, 15], 9, [1, 2]),
+    ([2, 3, 4], 6, [1, 3]),
+    ([-1, 0], -1, [1, 2])
+])
 def test_two_sum(numbers: list[int], target: int, expected: list[int]):
     assert False
