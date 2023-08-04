c5381af016d67788db3224b6cdad48a5fb6aeb6c
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-04 18:30:00 +0530

    add pytest test framework.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_two_sum_ii.py
+++ tests/test_two_sum_ii.py
@@ -1,2 +1,6 @@
-def test_two_sum():
+import pytest
+
+
+@pytest.mark.parametrize("numbers, target, expected")
+def test_two_sum(numbers: list[int], target: int, expected: list[int]):
     assert False
