a6ef32b00d2c8bea17ddea6c3800a21c3a659c84
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-04 18:27:06 +0530

    clean up the code and fix errors.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- two_sum_ii.py
+++ two_sum_ii.py
@@ -1,2 +1,3 @@
 # https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/?envType=study-plan-v2&envId=top-interview-150
-def twoSum(self, numbers: List[int], target: int) -> List[int]:
+def two_sum(numbers: list[int], target: int) -> list[int]:
+    ...
