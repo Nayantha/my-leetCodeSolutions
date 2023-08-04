96ebf1aa4bf426568b57954c4b2ded73a652ac1e
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-04 18:33:00 +0530

    import testable function and add the test logic.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_two_sum_ii.py
+++ tests/test_two_sum_ii.py
@@ -1,5 +1,7 @@
 import pytest
 
+from two_sum_ii import two_sum
+
 
 @pytest.mark.parametrize("numbers, target, expected", [
     ([2, 7, 11, 15], 9, [1, 2]),
@@ -7,4 +9,4 @@ import pytest
     ([-1, 0], -1, [1, 2])
 ])
 def test_two_sum(numbers: list[int], target: int, expected: list[int]):
-    assert False
+    assert two_sum(numbers, target) == expected
