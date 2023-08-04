363fab6841c832dd498fd0d95566b7301da7ec14
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-04 18:44:28 +0530

    add a solution.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- two_sum_ii.py
+++ two_sum_ii.py
@@ -1,3 +1,6 @@
 # https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/?envType=study-plan-v2&envId=top-interview-150
 def two_sum(numbers: list[int], target: int) -> list[int]:
-    ...
+    for i in range(len(numbers)):
+        rest = target - numbers[i]
+        if rest in numbers[i:]:
+            return [i + 1, numbers.index(rest, i + 1) + 1]
