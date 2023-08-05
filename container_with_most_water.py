f4f574ce1f2daac3a23592547e37f746deb00d0a
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-05 17:39:03 +0530

    change the range of inner loop.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- container_with_most_water.py
+++ container_with_most_water.py
@@ -2,6 +2,6 @@
 def max_area(height: list[int]) -> int:
     result = 0
     for left_pointer in range(len(height)):
-        for right_pointer in range(left_pointer, len(height)):
+        for right_pointer in range(left_pointer + 1, len(height)):
             result = max(result, min(height[left_pointer], height[right_pointer]) * (right_pointer - left_pointer))
     return result
