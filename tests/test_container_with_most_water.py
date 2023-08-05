e7b3e60958a013e1ea486deed8837dc77226868c
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-05 17:22:37 +0530

    add test data.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_container_with_most_water.py
+++ tests/test_container_with_most_water.py
@@ -3,6 +3,9 @@ import pytest
 from container_with_most_water import max_area
 
 
-@pytest.mark.parametrize("heights, expected")
+@pytest.mark.parametrize("heights, expected", [
+    ([1, 8, 6, 2, 5, 4, 8, 3, 7], 49),
+    ([1, 1], 1)
+])
 def test_max_area(heights: list[int], expected: int):
     assert max_area(heights) == expected
