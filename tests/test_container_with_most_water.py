44952c120aeb40e6d95b33338fc975fce4239dec
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-05 17:22:07 +0530

    import function to be tested and add the test logic.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_container_with_most_water.py
+++ tests/test_container_with_most_water.py
@@ -1,6 +1,8 @@
 import pytest
 
+from container_with_most_water import max_area
+
 
 @pytest.mark.parametrize("heights, expected")
 def test_max_area(heights: list[int], expected: int):
-    assert False
+    assert max_area(heights) == expected
