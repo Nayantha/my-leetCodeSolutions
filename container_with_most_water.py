37ba47142c4c3600cc543a1f1acbf2c0ca35fe8e
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-05 17:46:30 +0530

    add the final solution.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- container_with_most_water.py
+++ container_with_most_water.py
@@ -1,7 +1,14 @@
 # https://leetcode.com/problems/container-with-most-water/?envType=study-plan-v2&envId=top-interview-150
 def max_area(height: list[int]) -> int:
-    result = 0
-    for left_pointer in range(len(height)):
-        for right_pointer in range(left_pointer + 1, len(height)):
-            result = max(result, min(height[left_pointer], height[right_pointer]) * (right_pointer - left_pointer))
-    return result
+    areas = []
+    left_pointer = 0
+    right_pointer = len(height) - 1
+    while left_pointer < right_pointer:
+        areas.append(
+            (right_pointer - left_pointer) * min(height[left_pointer], height[right_pointer])
+        )
+        if height[right_pointer] > height[left_pointer]:
+            left_pointer += 1
+        else:
+            right_pointer -= 1
+    return max(areas)
