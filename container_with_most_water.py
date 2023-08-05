17ffcb6a9d534d989748fd185583aad5372b4d7c
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-05 17:54:27 +0530

    add value to max area before the loop.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- container_with_most_water.py
+++ container_with_most_water.py
@@ -1,15 +1,14 @@
 # https://leetcode.com/problems/container-with-most-water/?envType=study-plan-v2&envId=top-interview-150
 def max_area(height: list[int]) -> int:
-    max_area = 0
-    left_pointer = 0
-    right_pointer = len(height) - 1
-    while left_pointer < right_pointer:
-        max_area = max(
-            (right_pointer - left_pointer) * min(height[left_pointer], height[right_pointer]),
-            max_area
-        )
+    left_pointer, right_pointer = 0, len(height) - 1
+    max_area = right_pointer * min(height[left_pointer], height[right_pointer])
+    while left_pointer != right_pointer:
         if height[right_pointer] > height[left_pointer]:
             left_pointer += 1
         else:
             right_pointer -= 1
+        max_area = max(
+            (right_pointer - left_pointer) * min(height[left_pointer], height[right_pointer]),
+            max_area
+        )
     return max_area
