95ca076d0cdfe9da12f04f5ee8badc8ff6925e62
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-05 17:34:43 +0530

    add a solution.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- container_with_most_water.py
+++ container_with_most_water.py
@@ -1,3 +1,7 @@
 # https://leetcode.com/problems/container-with-most-water/?envType=study-plan-v2&envId=top-interview-150
 def max_area(height: list[int]) -> int:
-    ...
+    result = 0
+    for index_i, element_i in enumerate(height):
+        for index_j, element_j in enumerate(height[index_i + 1:]):
+            result = max((min(element_i, element_j) * (index_j + 1)), result)
+    return result
