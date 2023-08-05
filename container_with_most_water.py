81c2114e2e82fcf9f69e5d9cbdd75f20d7a1f13f
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-05 17:18:35 +0530

    clean up the code and fix errors.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- container_with_most_water.py
+++ container_with_most_water.py
@@ -1,2 +1,3 @@
 # https://leetcode.com/problems/container-with-most-water/?envType=study-plan-v2&envId=top-interview-150
-def maxArea(self, height: List[int]) -> int:
+def max_area(self, height: list[int]) -> int:
+    ...
