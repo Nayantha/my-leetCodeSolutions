7ea5e2b43035f35f9da0c44c7dac38ed22e8158d
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-05 17:49:34 +0530

    remove logic of using an array.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- container_with_most_water.py
+++ container_with_most_water.py
@@ -1,14 +1,15 @@
 # https://leetcode.com/problems/container-with-most-water/?envType=study-plan-v2&envId=top-interview-150
 def max_area(height: list[int]) -> int:
-    areas = []
+    max_area = 0
     left_pointer = 0
     right_pointer = len(height) - 1
     while left_pointer < right_pointer:
-        areas.append(
-            (right_pointer - left_pointer) * min(height[left_pointer], height[right_pointer])
+        max_area = max(
+            (right_pointer - left_pointer) * min(height[left_pointer], height[right_pointer]),
+            max_area
         )
         if height[right_pointer] > height[left_pointer]:
             left_pointer += 1
         else:
             right_pointer -= 1
-    return max(areas)
+    return max_area
