e0f081ed3f5afbc8d11dd3abe77f6862939c6b6c
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-06 10:42:31 +0530

    add test data.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_3sum.py
+++ tests/test_3sum.py
@@ -3,6 +3,10 @@ import pytest
 from _3sum import three_sum
 
 
-@pytest.mark.parametrize("nums, expected")
+@pytest.mark.parametrize("nums, expected", [
+    ([-1, 0, 1, 2, -1, -4], [[-1, -1, 2], [-1, 0, 1]]),
+    ([0, 1, 1], []),
+    ([0, 0, 0], [[0, 0, 0]])
+])
 def test_three_sum(nums: list[int], expected: list[list[int]]):
     assert three_sum(nums) == expected
