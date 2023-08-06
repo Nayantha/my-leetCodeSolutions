2dca9f8032907fc0bbe6a2d0ff7c49a7d5e99d6c
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-06 11:29:41 +0530

    add a test case to test the new solution.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_3sum.py
+++ tests/test_3sum.py
@@ -1,6 +1,6 @@
 import pytest
 
-from _3sum import three_sum, three_sum_ii
+from _3sum import three_sum, three_sum_ii, three_sum_iii
 
 
 @pytest.mark.parametrize("nums, expected", [
@@ -12,6 +12,7 @@ def test_three_sum(nums: list[int], expected: list[list[int]]):
     for output in three_sum(nums):
         assert output in expected
 
+
 @pytest.mark.parametrize("nums, expected", [
     ([-1, 0, 1, 2, -1, -4], [[-1, -1, 2], [-1, 0, 1]]),
     ([0, 1, 1], []),
@@ -19,4 +20,14 @@ def test_three_sum(nums: list[int], expected: list[list[int]]):
 ])
 def test_three_sum_ii(nums: list[int], expected: list[list[int]]):
     for output in three_sum_ii(nums):
+        assert output in expected
+
+
+@pytest.mark.parametrize("nums, expected", [
+    ([-1, 0, 1, 2, -1, -4], [[-1, -1, 2], [-1, 0, 1]]),
+    ([0, 1, 1], []),
+    ([0, 0, 0], [[0, 0, 0]])
+])
+def test_three_sum_iii(nums: list[int], expected: list[list[int]]):
+    for output in three_sum_iii(nums):
         assert output in expected
