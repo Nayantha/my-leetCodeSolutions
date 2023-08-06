0dbdadf6a0d803a3aa8b66946c74f33c41191108
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-06 10:40:54 +0530

    add a solution.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- _3sum.py
+++ _3sum.py
@@ -1,3 +1,12 @@
 # https://leetcode.com/problems/3sum/
+from itertools import combinations
+
+
 def three_sum(nums: list[int]) -> list[list[int]]:
-    ...
+    result = list()
+    for triplet in combinations(nums, 3):  # type: list
+        triplet.sort()
+        if sum(triplet) == 0:
+            if triplet not in result:
+                result.append(triplet)
+    return result
