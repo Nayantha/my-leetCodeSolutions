2407bcee84be9539d878bdd2d99351916c6223f2
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-06 10:35:46 +0530

    add test logic.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_3sum.py
+++ tests/test_3sum.py
@@ -1,6 +1,8 @@
 import pytest
 
+from _3sum import three_sum
+
 
 @pytest.mark.parametrize("nums, expected")
 def test_three_sum(nums: list[int], expected: list[list[int]]):
-    assert False
+    assert three_sum(nums) == expected
