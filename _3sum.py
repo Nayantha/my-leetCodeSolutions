dd5c24e6da4fe62e2b8baeb969f8d86163e58828
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-06 12:06:57 +0530

    made changes to the solution logic.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- _3sum.py
+++ _3sum.py
@@ -26,15 +26,21 @@ def three_sum_ii(nums: list[int]) -> list[list[int]]:
 
 def three_sum_iii(nums: list[int]) -> list[list[int]]:
     result = []
-    nums = list(set(sorted(nums)))
-    for i in range(len(nums)):
+    nums.sort()
+    for i, a in enumerate(nums):
+        if i > 0 and a == nums[i - 1]:
+            continue
         l, r = i + 1, len(nums) - 1
         while l < r:
-            triplet = sorted([nums[i], nums[l], nums[r]])
-            if sum(triplet) == 0 and triplet not in result:
-                result.append(triplet)
-            elif sum(triplet) > 0:
+            triplet = sorted([a, nums[l], nums[r]])
+            three_sum = sum(triplet)
+            if three_sum > 0:
                 r -= 1
-            else:
+            elif three_sum < 0:
                 l += 1
+            else:
+                result.append(triplet)
+                l += 1
+                while nums[l] == nums[l - 1] and l < r:
+                    l += 1
     return result
