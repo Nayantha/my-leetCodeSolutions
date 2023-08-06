b3d39e30cc4659366e1de83201ce0716eecd393c
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-06 11:27:09 +0530

    add another solution.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- _3sum.py
+++ _3sum.py
@@ -22,3 +22,14 @@ def three_sum_ii(nums: list[int]) -> list[list[int]]:
                 if sum(triplet) == 0 and triplet not in result:
                     result.append(triplet)
     return result
+
+def three_sum_iii(nums: list[int]) -> list[list[int]]:
+    result = []
+    nums = list(set(sorted(nums)))
+    for i in range(len(nums)):
+        l, r = i + 1, len(nums) - 1
+        while l < r:
+            triplet = sorted(nums[i], nums[l], nums[r])
+            if sum(triplet) == 0 and triplet not in result:
+                result.append(triplet)
+    return result
