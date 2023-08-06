9bc05b916ccd8b3f4f7fe23f661f68d1f564f94d
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-06 11:20:04 +0530

    modify the third for loop iterable.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- _3sum.py
+++ _3sum.py
@@ -14,11 +14,10 @@ def three_sum(nums: list[int]) -> list[list[int]]:
 
 def three_sum_ii(nums: list[int]) -> list[list[int]]:
     result = []
-    nums.sort()
     nums = list(set(nums))
     for i in nums:
         for j in nums[i + 1:]:
-            for k in nums[j + 1:]:
+            for k in nums[i + j + 1:]:
                 triplet = sorted([i, j, k])
                 if sum(triplet) == 0 and triplet not in result:
                     result.append(triplet)
