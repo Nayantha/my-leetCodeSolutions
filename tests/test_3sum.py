2271f3befec8f48a1d0393a94cd2f25fc093656d
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-06 11:01:11 +0530

    extract the new function test to new test function.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_3sum.py
+++ tests/test_3sum.py
@@ -12,5 +12,11 @@ def test_three_sum(nums: list[int], expected: list[list[int]]):
     for output in three_sum(nums):
         assert output in expected
 
+@pytest.mark.parametrize("nums, expected", [
+    ([-1, 0, 1, 2, -1, -4], [[-1, -1, 2], [-1, 0, 1]]),
+    ([0, 1, 1], []),
+    ([0, 0, 0], [[0, 0, 0]])
+])
+def test_three_sum(nums: list[int], expected: list[list[int]]):
     for output in three_sum_ii(nums):
         assert output in expected
