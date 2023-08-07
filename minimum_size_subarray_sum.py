d1b43477661335d2994bec4dc8241475e9555657
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-07 15:14:09 +0530

    add a solution.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- minimum_size_subarray_sum.py
+++ minimum_size_subarray_sum.py
@@ -1,3 +1,19 @@
 # https://leetcode.com/problems/minimum-size-subarray-sum/?envType=study-plan-v2&envId=top-interview-150
 def min_sub_array_len(target: int, nums: list[int]) -> int:
-    ...
+    if target > sum(nums):
+        return 0
+    for num in nums:
+        if target <= num:
+            return 1
+    l, r = 0, 1
+    window_size = 0
+    while l < len(nums) and r < len(nums):
+        if sum(nums[l:r]) < target:
+            r += 1
+        else:
+            if not window_size:
+                window_size = r - l
+            else:
+                window_size = min(window_size, r - l)
+                l += 1
+    return window_size
