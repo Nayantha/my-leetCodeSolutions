dd157286c0c04a49bb61d5513595ea095ee8535b
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-07 14:31:23 +0530

    add the pytest test framework.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_minimum_size_subarray_sum.py
+++ tests/test_minimum_size_subarray_sum.py
@@ -1,2 +1,6 @@
-def test_min_sub_array_len():
+import pytest
+
+
+@pytest.mark.parametrize("target, nums, expected")
+def test_min_sub_array_len(target: int, nums: list[int], expected: int):
     assert False
