b3b904ecf8441cfe90d41fe1f2a3df3d9729af9f
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-07 14:28:45 +0530

    fix errors and reformat according to python recommended format.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- minimum_size_subarray_sum.py
+++ minimum_size_subarray_sum.py
@@ -1,2 +1,3 @@
 # https://leetcode.com/problems/minimum-size-subarray-sum/?envType=study-plan-v2&envId=top-interview-150
-def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+def min_sub_array_len(target: int, nums: list[int]) -> int:
+    ...
