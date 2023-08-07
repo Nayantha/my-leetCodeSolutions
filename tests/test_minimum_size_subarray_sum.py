e4993939bdb5ae02c7160c3fb4b0bfc4b2596c80
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-07 14:32:18 +0530

    add test logic.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_minimum_size_subarray_sum.py
+++ tests/test_minimum_size_subarray_sum.py
@@ -1,6 +1,8 @@
 import pytest
 
+from minimum_size_subarray_sum import min_sub_array_len
+
 
 @pytest.mark.parametrize("target, nums, expected")
 def test_min_sub_array_len(target: int, nums: list[int], expected: int):
-    assert False
+    assert min_sub_array_len(target, nums) == expected
