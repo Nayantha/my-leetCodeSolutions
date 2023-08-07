794b55213a4c842fbad2d1741912441d0edbbee0
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-07 14:36:39 +0530

    add test data.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_minimum_size_subarray_sum.py
+++ tests/test_minimum_size_subarray_sum.py
@@ -3,6 +3,10 @@ import pytest
 from minimum_size_subarray_sum import min_sub_array_len
 
 
-@pytest.mark.parametrize("target, nums, expected")
+@pytest.mark.parametrize("target, nums, expected", [
+    (7, [2, 3, 1, 2, 4, 3], 2),
+    (4, [1, 4, 4], 1),
+    (11, [1, 1, 1, 1, 1, 1, 1, 1], 0)
+])
 def test_min_sub_array_len(target: int, nums: list[int], expected: int):
     assert min_sub_array_len(target, nums) == expected
