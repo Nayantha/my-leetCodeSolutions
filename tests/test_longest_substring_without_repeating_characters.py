aaf785f89d8bad91fef49711fa5390de61e241d1
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-08 18:58:01 +0530

    add test logic for the new solution.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_longest_substring_without_repeating_characters.py
+++ tests/test_longest_substring_without_repeating_characters.py
@@ -1,6 +1,6 @@
 import pytest
 
-from longest_substring_without_repeating_characters import length_of_longest_substring
+from longest_substring_without_repeating_characters import length_of_longest_substring, length_of_longest_substring_ii
 
 
 @pytest.mark.parametrize("s, expected", [
@@ -13,3 +13,4 @@ from longest_substring_without_repeating_characters import length_of_longest_sub
 ])
 def test_length_of_longest_substring(s: str, expected: int):
     assert length_of_longest_substring(s) == expected
+    assert length_of_longest_substring_ii(s) == expected
