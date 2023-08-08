16d4d042ecac26d058136eec5724875c6e5d43d7
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-08 17:36:27 +0530

    add test data.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_longest_substring_without_repeating_characters.py
+++ tests/test_longest_substring_without_repeating_characters.py
@@ -1,6 +1,10 @@
 import pytest
 
 
-@pytest.mark.parametrize("s, expected")
+@pytest.mark.parametrize("s, expected", [
+    ("abcabcbb", 3),
+    ("bbbbb", 1),
+    ("pwwkew", 3)
+])
 def test_length_of_longest_substring(s: str, expected: int):
     assert False
