aa528c1dd19fac2f595fba4abed68391cb0409c1
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-08 17:30:46 +0530

    fix errors and reformat according to python recommended format.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- longest_substring_without_repeating_characters.py
+++ longest_substring_without_repeating_characters.py
@@ -1,2 +1,3 @@
 # https://leetcode.com/problems/longest-substring-without-repeating-characters/?envType=study-plan-v2&envId=top-interview-150
-def lengthOfLongestSubstring(self, s: str) -> int:
+def length_of_longest_substring(s: str) -> int:
+    ...
