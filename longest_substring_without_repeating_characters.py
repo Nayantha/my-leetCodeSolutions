115e7eda0cfc92064b1739f6e8b27283c02c6b4a
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-08 17:56:11 +0530

    add a base case.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- longest_substring_without_repeating_characters.py
+++ longest_substring_without_repeating_characters.py
@@ -1,5 +1,7 @@
 # https://leetcode.com/problems/longest-substring-without-repeating-characters/?envType=study-plan-v2&envId=top-interview-150
 def length_of_longest_substring(s: str) -> int:
+    if len(s) == 1:
+        return 1
     res_str = []
     max_len = 0
     for ele in s:
