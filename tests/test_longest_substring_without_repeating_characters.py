b82a1c65f4e3722aec7ad5a495a788a60ec15304
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-08 17:59:41 +0530

    add two test cases.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_longest_substring_without_repeating_characters.py
+++ tests/test_longest_substring_without_repeating_characters.py
@@ -7,7 +7,8 @@ from longest_substring_without_repeating_characters import length_of_longest_sub
     ("abcabcbb", 3),
     ("bbbbb", 1),
     ("pwwkew", 3),
-    ("au", 2)
+    ("au", 2),
+    (" ", 1)
 ])
 def test_length_of_longest_substring(s: str, expected: int):
     assert length_of_longest_substring(s) == expected
