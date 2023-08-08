ff83638704c1cc1f490da2e52f1b38996e3902bd
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-08 17:53:14 +0530

    add a solution.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- longest_substring_without_repeating_characters.py
+++ longest_substring_without_repeating_characters.py
@@ -1,3 +1,11 @@
 # https://leetcode.com/problems/longest-substring-without-repeating-characters/?envType=study-plan-v2&envId=top-interview-150
 def length_of_longest_substring(s: str) -> int:
-    ...
+    res_str = []
+    max_len = 0
+    for ele in s:
+        if ele not in res_str:  # type: str
+            res_str.append(ele)
+        else:
+            max_len = max(max_len, len(res_str))
+            res_str = [ele]
+    return max_len
