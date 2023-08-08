74544bf42483b272fffcce067bec765eee10ecc7
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-08 18:12:28 +0530

    finalize the solution to accepting level.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- longest_substring_without_repeating_characters.py
+++ longest_substring_without_repeating_characters.py
@@ -3,9 +3,8 @@ def length_of_longest_substring(s: str) -> int:
     res_str = []
     max_len = 0
     for ele in s:
-        if ele not in res_str:  # type: str
-            res_str.append(ele)
-        else:
-            res_str = [ele]
+        if ele in res_str:  # type: str
+            res_str = res_str[res_str.index(ele) + 1:]
+        res_str.append(ele)
         max_len = max(max_len, len(res_str))
     return max_len
