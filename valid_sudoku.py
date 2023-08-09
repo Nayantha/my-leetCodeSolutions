d21bc84d037b35d7026ee75942d8bbdb6cfd1a09
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-09 17:43:51 +0530

    add a solution.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- valid_sudoku.py
+++ valid_sudoku.py
@@ -1,14 +1,21 @@
 # https://leetcode.com/problems/valid-sudoku/?envType=study-plan-v2&envId=top-interview-150
+import collections
+
+
 def is_valid_sudoku(board: list[list[str]]) -> bool:
-    for row in board:
-        for item in row:
-            if row.count(item) > 0 and type(item) == int:
-                return False
+    cols = collections.defaultdict(set)
+    rows = collections.defaultdict(set)
+    squares = collections.defaultdict(set)
 
-    for column_index in range(9):
-        column = [row[column_index] for row in board]
-        for item in column:
-            if column.count(item) > 0 and type(item) == int:
+    for row_index in range(9):
+        for column_index in range(9):
+            item = board[row_index][column_index]
+            if item == ".":
+                continue
+            if item in rows[row_index] or item in cols[column_index] or item in squares[
+                (row_index // 3, column_index // 3)]:
                 return False
-
+            cols[column_index].add(item)
+            rows[row_index].add(item)
+            squares[(row_index // 3, column_index // 3)].add(item)
     return True
