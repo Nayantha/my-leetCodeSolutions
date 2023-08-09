5f244e8e881160065cec56c66ad90be56aa42a1a
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-09 16:23:27 +0530

    add the pytest test framework.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_valid_sudoku.py
+++ tests/test_valid_sudoku.py
@@ -1,2 +1,6 @@
-def test_is_valid_sudoku():
+import pytest
+
+
+@pytest.mark.parametrize("board, expected")
+def test_is_valid_sudoku(board: list[list[int | str]], expected: bool):
     assert False
