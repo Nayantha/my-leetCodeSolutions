151e8d7db3603b06eef16670f87a863dd3226835
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-09 16:58:58 +0530

    add logic to check satisfy the first two conditions.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- valid_sudoku.py
+++ valid_sudoku.py
@@ -1,3 +1,14 @@
 # https://leetcode.com/problems/valid-sudoku/?envType=study-plan-v2&envId=top-interview-150
 def is_valid_sudoku(board: list[list[str]]) -> bool:
-    ...
+    for row in board:
+        for item in row:
+            if row.count(item) > 0 and type(item) == int:
+                return False
+
+    for column_index in range(9):
+        column = [row[column_index] for row in board]
+        for item in column:
+            if column.count(item) > 0 and type(item) == int:
+                return False
+
+    return True
