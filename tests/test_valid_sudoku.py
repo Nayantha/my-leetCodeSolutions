7519df742aafc5f6552a02d407bd88ee3494a523
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-09 16:24:56 +0530

    import the function to test.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_valid_sudoku.py
+++ tests/test_valid_sudoku.py
@@ -1,6 +1,8 @@
 import pytest
 
+from valid_sudoku import is_valid_sudoku
+
 
 @pytest.mark.parametrize("board, expected")
 def test_is_valid_sudoku(board: list[list[int | str]], expected: bool):
-    assert False
+    is_valid_sudoku(board)
