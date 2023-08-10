82fe0cd676da839c91565ca11120175ce63924d9
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-10 10:18:19 +0530

    fix errors and reformat according to python recommended format.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- spiral_matrix.py
+++ spiral_matrix.py
@@ -1,2 +1,3 @@
 # https://leetcode.com/problems/spiral-matrix/?envType=study-plan-v2&envId=top-interview-150
-def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+def spiral_order(matrix: list[list[int]]) -> list[int]:
+    ...
