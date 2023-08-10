a678509efdc9fa7d65dae2b67f237eb781726409
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-10 10:23:52 +0530

    add test logic.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_spiral_matrix.py
+++ tests/test_spiral_matrix.py
@@ -1,9 +1,11 @@
 import pytest
 
+from spiral_matrix import spiral_order
+
 
 @pytest.mark.parametrize("matrix, expected", [
     ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], [1, 2, 3, 6, 9, 8, 7, 4, 5]),
     ([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]], [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]])
 ])
 def test_spiral_order(matrix: list[list[int]], expected: list[int]):
-    assert False
+    assert spiral_order(matrix) == expected
