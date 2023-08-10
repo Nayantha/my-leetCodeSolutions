7d05ee1caf80d6647178bf757d3f4d9e10b263fe
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-10 11:00:18 +0530

    add a dolutio
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_spiral_matrix.py
+++ tests/test_spiral_matrix.py
@@ -5,7 +5,7 @@ from spiral_matrix import spiral_order
 
 @pytest.mark.parametrize("matrix, expected", [
     ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], [1, 2, 3, 6, 9, 8, 7, 4, 5]),
-    ([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]], [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]])
+    ([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]], [1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7])
 ])
 def test_spiral_order(matrix: list[list[int]], expected: list[int]):
     assert spiral_order(matrix) == expected
