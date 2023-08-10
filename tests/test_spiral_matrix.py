e28d3efe104f26e940c248a1a1b0b51b7034deeb
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-10 10:22:52 +0530

    add test data.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_spiral_matrix.py
+++ tests/test_spiral_matrix.py
@@ -1,6 +1,9 @@
 import pytest
 
 
-@pytest.mark.parametrize("matrix, expected")
+@pytest.mark.parametrize("matrix, expected", [
+    ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], [1, 2, 3, 6, 9, 8, 7, 4, 5]),
+    ([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]], [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]])
+])
 def test_spiral_order(matrix: list[list[int]], expected: list[int]):
     assert False
