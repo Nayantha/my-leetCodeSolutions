f7d6cb485c9c653c715c25fa24220e47cb3eff16
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-11 19:21:47 +0530

    add test logic.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_rotate_image.py
+++ tests/test_rotate_image.py
@@ -1,5 +1,7 @@
 import pytest
 
+from rotate_image import rotate
+
 
 @pytest.mark.parametrize("matrix, expected", [
     ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], [[7, 4, 1], [8, 5, 2], [9, 6, 3]]),
@@ -7,4 +9,5 @@ import pytest
      [[15, 13, 2, 5], [14, 3, 4, 1], [12, 6, 8, 9], [16, 7, 10, 11]])
 ])
 def test_rotate(matrix: list[list[int]], expected: list[list[int]]):
-    assert False
+    rotate(matrix)
+    assert matrix == expected
