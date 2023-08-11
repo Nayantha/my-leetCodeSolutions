347b1306db1022410ebfa7edc2c5ad370c621af5
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-11 21:46:15 +0530

    add a solution.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- rotate_image.py
+++ rotate_image.py
@@ -1,7 +1,21 @@
 # https://leetcode.com/problems/rotate-image/?envType=study-plan-v2&envId=top-interview-150
 def rotate(matrix: list[list[int]]) -> None:
-    ...
-
+    l, r = 0, len(matrix) - 1
+    while l < r:
+        for i in range(r - l):
+            top, bottom = l, r
+            # save top left
+            top_left = matrix[top][l + i]
+            # move bottom left to top left
+            matrix[top][l + i] = matrix[bottom - i][l]
+            # move bottom right to bottom left
+            matrix[bottom - i][l] = matrix[bottom][r - i]
+            # move top right to bottom right
+            matrix[bottom][r - i] = matrix[top + i][r]
+            # move top left to right
+            matrix[top + i][r] = top_left
+        l += 1
+        r -= 1
 
 def rotate_ii(matrix: list[list[int]]) -> None:
     left, right = 0, len(matrix) - 1
