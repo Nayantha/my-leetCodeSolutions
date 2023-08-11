32e8a5e1428509c8a6e5eaedaab1e1bc596d7bde
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-11 18:44:36 +0530

    fix errors and reformat according to python recommended format.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- rotate_image.py
+++ rotate_image.py
@@ -1,2 +1,3 @@
 # https://leetcode.com/problems/rotate-image/?envType=study-plan-v2&envId=top-interview-150
-def rotate(self, matrix: List[List[int]]) -> None:
+def rotate(matrix: list[list[int]]) -> None:
+    ...
