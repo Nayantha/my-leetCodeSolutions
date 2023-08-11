e9a9fc750982e3f4bfe1f3acef6c2c0b9e9dff1d
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-11 20:16:11 +0530

    add a solution logic.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- rotate_image.py
+++ rotate_image.py
@@ -1,3 +1,15 @@
 # https://leetcode.com/problems/rotate-image/?envType=study-plan-v2&envId=top-interview-150
 def rotate(matrix: list[list[int]]) -> None:
-    ...
+    left, right = 0, len(matrix) - 1
+    top, bottom = 0, len(matrix[0]) - 1
+    while left < right and top < bottom:
+        for i in (left, right - 1):
+            matrix[top][i], matrix[top][right - i] = matrix[top][right - i], matrix[top][i]
+        for i in (top, bottom - 1):
+            matrix[i][left], matrix[bottom - i][left] = matrix[bottom - i][left], matrix[i][left]
+        for i in (left, right - 1):
+            matrix[bottom][i], matrix[bottom][right - i] = matrix[bottom][right - i], matrix[bottom][i]
+        left += 1
+        top += 1
+        right -= 1
+        bottom -= 1
