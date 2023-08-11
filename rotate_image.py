ef83b7baecc9161040741e6aeeea69041450d426
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-11 21:33:37 +0530

    made the current solution logic a backup.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- rotate_image.py
+++ rotate_image.py
@@ -1,15 +1,17 @@
 # https://leetcode.com/problems/rotate-image/?envType=study-plan-v2&envId=top-interview-150
-def rotate(matrix: list[list[int]]) -> None:
+def rotate_ii(matrix: list[list[int]]) -> None:
     left, right = 0, len(matrix) - 1
     top, bottom = 0, len(matrix[0]) - 1
     while left < right and top < bottom:
-        for i in (left, right - 1):
-            matrix[top][i], matrix[top][right - i] = matrix[top][right - i], matrix[top][i]
-        for i in (top, bottom - 1):
-            matrix[i][left], matrix[bottom - i][left] = matrix[bottom - i][left], matrix[i][left]
-        for i in (left, right - 1):
-            matrix[bottom][i], matrix[bottom][right - i] = matrix[bottom][right - i], matrix[bottom][i]
+        for i in range(left, right):
+            matrix[top][i], matrix[top + i][right] = matrix[top + i][right], matrix[top][i]
+        for i in range(left, right):
+            matrix[top + i][right], matrix[bottom][right - i - left] = matrix[bottom][right - i - left], \
+                matrix[top + i][right]
+        for i in range(left, right):
+            matrix[bottom][right - i - left], matrix[bottom - top - i][left] = matrix[bottom - top - i][left], \
+                matrix[bottom][right - i - left]
         left += 1
-        top += 1
         right -= 1
+        top += 1
         bottom -= 1
