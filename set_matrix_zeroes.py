5217d4be2421840a1467b59a7b6fed99e4f30e3a
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-12 19:29:38 +0530

    fix errors and reformat according to python recommended format.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- set_matrix_zeroes.py
+++ set_matrix_zeroes.py
@@ -1,2 +1,3 @@
 # https://leetcode.com/problems/set-matrix-zeroes/?envType=study-plan-v2&envId=top-interview-150
-def setZeroes(self, matrix: List[List[int]]) -> None:
+def set_zeroes(matrix: list[list[int]]) -> None:
+    ...
