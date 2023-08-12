bdb2b62e911db50c6af72b9694e39815559955f3
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-12 19:38:03 +0530

    add the function to be tested with test logic.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_set_matrix_zeroes.py
+++ tests/test_set_matrix_zeroes.py
@@ -1,6 +1,9 @@
 import pytest
 
+from set_matrix_zeroes import set_zeroes
+
 
 @pytest.mark.parametrize("matrix, expected")
 def test_set_zeroes(matrix: list[list[int]], expected: list[list[int]]):
-    assert False
+    set_zeroes(matrix)
+    assert matrix == expected
