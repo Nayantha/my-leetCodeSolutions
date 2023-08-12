7f0b5d70ee92343abdd776ff93302e7542918f6e
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-12 19:40:49 +0530

    add test data.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_set_matrix_zeroes.py
+++ tests/test_set_matrix_zeroes.py
@@ -3,7 +3,10 @@ import pytest
 from set_matrix_zeroes import set_zeroes
 
 
-@pytest.mark.parametrize("matrix, expected")
+@pytest.mark.parametrize("matrix, expected", [
+    ([[1, 1, 1], [1, 0, 1], [1, 1, 1]], [[1, 0, 1], [0, 0, 0], [1, 0, 1]]),
+    ([[0, 1, 2, 0], [3, 4, 5, 2], [1, 3, 1, 5]], [[0, 0, 0, 0], [0, 4, 5, 0], [0, 3, 1, 0]])
+])
 def test_set_zeroes(matrix: list[list[int]], expected: list[list[int]]):
     set_zeroes(matrix)
     assert matrix == expected
