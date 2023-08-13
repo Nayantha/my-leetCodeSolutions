38d0f2830f0c7ee6b94612b41688e73beba600e3
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-13 21:53:35 +0530

    add a solution.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- game_of_life.py
+++ game_of_life.py
@@ -1,3 +1,22 @@
 # https://leetcode.com/problems/game-of-life/?envType=study-plan-v2&envId=top-interview-150
 def game_of_life(board: list[list[int]]) -> None:
-    ...
+    for row_index in range(len(board)):
+        for col_index in range(len(board[0])):
+            ele = board[row_index][col_index]
+            sum_of_neighbours = sum(
+                # first row / the row above
+                board.__getitem__(row_index - 1).__getitem__(col_index - 1) +
+                board.__getitem__(row_index - 1).__getitem__(col_index) +
+                board.__getitem__(row_index - 1).__getitem__(col_index + 1) +
+                # same row
+                board.__getitem__(row_index).__getitem__(col_index - 1) +
+                board.__getitem__(row_index).__getitem__(col_index + 1) +
+                # row below
+                board.__getitem__(row_index + 1).__getitem__(col_index - 1) +
+                board.__getitem__(row_index + 1).__getitem__(col_index) +
+                board.__getitem__(row_index + 1).__getitem__(col_index + 1)
+            )
+            if ele == 1 and (sum_of_neighbours != 2 or sum_of_neighbours != 3):
+                board[row_index][col_index] = 0
+            if ele == 0 and sum_of_neighbours == 3:
+                board[row_index][col_index] = 1
