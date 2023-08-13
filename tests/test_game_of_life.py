25492aabd762f2fd70ef37d51f84df6556ccc6d2
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-13 21:36:14 +0530

    add test data.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_game_of_life.py
+++ tests/test_game_of_life.py
@@ -3,7 +3,10 @@ import pytest
 from game_of_life import game_of_life
 
 
-@pytest.mark.parametrize("board, expected")
+@pytest.mark.parametrize("board, expected", [
+    ([[0, 1, 0], [0, 0, 1], [1, 1, 1], [0, 0, 0]], [[0, 0, 0], [1, 0, 1], [0, 1, 1], [0, 1, 0]]),
+    ([[1, 1], [1, 0]], [[1, 1], [1, 1]])
+])
 def test_game_of_life(board: list[list[int]], expected: list[list[int]]):
     game_of_life(board)
     assert board == expected
