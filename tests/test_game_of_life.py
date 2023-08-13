a8563b1ce7bd9293c983b9d9a856a8d7fe2f5b83
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-13 21:34:03 +0530

    add test logic.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_game_of_life.py
+++ tests/test_game_of_life.py
@@ -1,6 +1,9 @@
 import pytest
 
+from game_of_life import game_of_life
+
 
 @pytest.mark.parametrize("board, expected")
 def test_game_of_life(board: list[list[int]], expected: list[list[int]]):
-    assert False
+    game_of_life(board)
+    assert board == expected
