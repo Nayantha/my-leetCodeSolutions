613f7c403dba76976cdc9ce907b6f73e08a5a1e6
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-14 11:50:02 +0530

    add test data.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_group_anagrams.py
+++ tests/test_group_anagrams.py
@@ -4,7 +4,9 @@ from group_anagrams import group_anagrams
 
 
 @pytest.mark.parametrize("strs, expected", [
-    (["eat","tea","tan","ate","nat","bat"], )
+    (["eat", "tea", "tan", "ate", "nat", "bat"], [["bat"], ["nat", "tan"], ["ate", "eat", "tea"]]),
+    ([""], [[""]]),
+    (["a"], [["a"]])
 ])
 def test_group_anagrams(strs: list[str], expected: list[list[str]]):
     assert group_anagrams(strs) == expected
