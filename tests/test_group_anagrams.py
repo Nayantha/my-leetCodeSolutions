1e88f6ee8119c058fd8611d33b42f354e398214e
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-14 11:49:06 +0530

    add test logic.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_group_anagrams.py
+++ tests/test_group_anagrams.py
@@ -1,6 +1,10 @@
 import pytest
 
+from group_anagrams import group_anagrams
 
-@pytest.mark.parametrize("strs, expected")
+
+@pytest.mark.parametrize("strs, expected", [
+    (["eat","tea","tan","ate","nat","bat"], )
+])
 def test_group_anagrams(strs: list[str], expected: list[list[str]]):
-    assert False
+    assert group_anagrams(strs) == expected
