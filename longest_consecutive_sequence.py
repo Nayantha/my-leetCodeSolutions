70cf29f7d547a5e2d849cc99f8a2432b82f03535
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-15 16:14:41 +0530

    made necessary changes to get required to be output.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- longest_consecutive_sequence.py
+++ longest_consecutive_sequence.py
@@ -2,7 +2,11 @@
 def longest_consecutive(nums: list[int]) -> int:
     nums.sort()
     last_index_of_consecutive_list = 0
+    longest_consecutive_list_len = 0
     for i in range(1, len(nums)):
-        if nums[i] - nums[last_index_of_consecutive_list] in [0, 1]:
+        if nums[i] - nums[i - 1] == 1:
             last_index_of_consecutive_list += 1
-    return last_index_of_consecutive_list
+        else:
+            last_index_of_consecutive_list = 0
+        longest_consecutive_list_len = max(longest_consecutive_list_len, last_index_of_consecutive_list)
+    return longest_consecutive_list_len + 1
