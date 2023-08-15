63612b12f30e49d4d311f41518bbe6935fcab546
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-15 15:58:14 +0530

    add test logic.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_longest_consecutive_sequence.py
+++ tests/test_longest_consecutive_sequence.py
@@ -1,9 +1,11 @@
 import pytest
 
+from longest_consecutive_sequence import longest_consecutive
+
 
 @pytest.mark.parametrize("nums, expected", [
     ([100, 4, 200, 1, 3, 2], 4),
     ([0, 3, 7, 2, 5, 8, 4, 6, 0, 1], 9)
 ])
 def test_longest_consecutive(nums: list[int], expected: int):
-    assert False
+    assert longest_consecutive(nums) == expected
