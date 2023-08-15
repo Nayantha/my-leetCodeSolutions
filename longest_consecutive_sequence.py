39322e18172a3f8d112540477b662cf3a40e5f4e
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-15 16:03:24 +0530

    add a possible solution.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- longest_consecutive_sequence.py
+++ longest_consecutive_sequence.py
@@ -1,3 +1,8 @@
 # https://leetcode.com/problems/longest-consecutive-sequence/
 def longest_consecutive(nums: list[int]) -> int:
-    ...
+    nums.sort()
+    last_index_of_consecutive_list = 0
+    for i in range(1, len(nums)):
+        if nums[i] - nums[last_index_of_consecutive_list] in [0, 1]:
+            last_index_of_consecutive_list += 1
+    return last_index_of_consecutive_list
