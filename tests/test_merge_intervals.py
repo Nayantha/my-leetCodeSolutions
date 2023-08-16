b56bace114a5aaa832bd5129180fb27456cc7d45
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-16 17:49:41 +0530

    add test data.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_merge_intervals.py
+++ tests/test_merge_intervals.py
@@ -3,6 +3,9 @@ import pytest
 from merge_intervals import merge
 
 
-@pytest.mark.parametrize("intervals, expected")
+@pytest.mark.parametrize("intervals, expected", [
+    ([[1, 3], [2, 6], [8, 10], [15, 18]], [[1, 6], [8, 10], [15, 18]]),
+    ([[1, 4], [4, 5]], [[1, 5]])
+])
 def test_merge(intervals: list[list[int]], expected: list[list[int]]):
     assert merge(intervals) == expected
