3058f8437f3609a8685df1acb799da0e470f08e5
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-16 18:01:04 +0530

    add a new solution.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- merge_intervals.py
+++ merge_intervals.py
@@ -1,3 +1,11 @@
 # https://leetcode.com/problems/merge-intervals/
 def merge(intervals: list[list[int]]) -> list[list[int]]:
-    ...
+    res = [intervals[0]]
+    intervals.sort(key=lambda i: i[0])
+    for start, end in intervals[1:]:
+        last_end = res[-1][1]
+        if start <= last_end:
+            res[-1][1] = max(last_end, end)
+        else:
+            res.append([start, end])
+    return res
