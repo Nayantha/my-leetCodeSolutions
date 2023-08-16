9ebf5932be64077c562abea9a27b5276f33fc2ed
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-16 18:03:22 +0530

    add new test case.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_merge_intervals.py
+++ tests/test_merge_intervals.py
@@ -5,7 +5,8 @@ from merge_intervals import merge
 
 @pytest.mark.parametrize("intervals, expected", [
     ([[1, 3], [2, 6], [8, 10], [15, 18]], [[1, 6], [8, 10], [15, 18]]),
-    ([[1, 4], [4, 5]], [[1, 5]])
+    ([[1, 4], [4, 5]], [[1, 5]]),
+    ([[1, 4], [0, 4]], [[0, 4]])
 ])
 def test_merge(intervals: list[list[int]], expected: list[list[int]]):
     assert merge(intervals) == expected
