d42045db8068e03b2c9ae1a6bc32232aa5d5b559
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-17 19:52:25 +0530

    add the pytest test framework.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_insert_interval.py
+++ tests/test_insert_interval.py
@@ -1,2 +1,6 @@
-def test_insert():
+import pytest
+
+
+@pytest.mark.parametrize("intervals, new_interval, expected")
+def test_insert(intervals: list[list[int]], new_interval: list[int], expected: list[list[int]]):
     assert False
