7f689fe6fe03e5bbe432fb4de1cc3d5d7090b1c2
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-17 20:00:00 +0530

    add test logic.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_insert_interval.py
+++ tests/test_insert_interval.py
@@ -1,9 +1,11 @@
 import pytest
 
+from insert_interval import insert
+
 
 @pytest.mark.parametrize("intervals, new_interval, expected", [
     ([[1, 3], [6, 9]], [2, 5], [[1, 5], [6, 9]]),
     ([[1, 2], [3, 5], [6, 7], [8, 10], [12, 16]], [4, 8], [[1, 2], [3, 10], [12, 16]])
 ])
 def test_insert(intervals: list[list[int]], new_interval: list[int], expected: list[list[int]]):
-    assert False
+    assert insert(intervals, new_interval) == expected
