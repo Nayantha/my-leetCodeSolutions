8920391cad893eb8c0365e29635eeed1acb6b43c
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-17 21:35:41 +0530

    add a solution.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- insert_interval.py
+++ insert_interval.py
@@ -1,6 +1,13 @@
 # https://leetcode.com/problems/insert-interval/
 def insert(intervals: list[list[int]], new_interval: list[int]) -> list[list[int]]:
-    for i in range(len(intervals)):  # type: list[int]
-        if intervals[i][1] <= new_interval[0]:
-            intervals[i][1] = new_interval
-    return intervals
+    res = []
+    for i in range(len(intervals)):
+        if new_interval[1] < intervals[i][0]:
+            res.append(new_interval)
+            return res + intervals[i:]
+        elif new_interval[0] > intervals[i][1]:
+            res.append(intervals[i])
+        else:
+            new_interval = [min(new_interval[0], intervals[i][0]), max(new_interval[1], intervals[i][1])]
+    res.append(new_interval)
+    return res
