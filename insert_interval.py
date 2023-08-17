d75576c7b9d77787050ec5f8bfa267aaa4a63719
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-17 20:01:00 +0530

    add a possible looping logic.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- insert_interval.py
+++ insert_interval.py
@@ -1,3 +1,6 @@
 # https://leetcode.com/problems/insert-interval/
 def insert(intervals: list[list[int]], new_interval: list[int]) -> list[list[int]]:
-    ...
+    for i in range(len(intervals)):  # type: list[int]
+        if intervals[i][1] <= new_interval[0]:
+            intervals[i][1] = new_interval
+    return intervals
