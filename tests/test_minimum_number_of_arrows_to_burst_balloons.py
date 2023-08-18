a8d6795417959baaf44463c03b24618873b34517
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-18 16:22:18 +0530

    add a new test data.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_minimum_number_of_arrows_to_burst_balloons.py
+++ tests/test_minimum_number_of_arrows_to_burst_balloons.py
@@ -3,7 +3,8 @@ import pytest
 
 @pytest.mark.parametrize("points, expected", [
     ([[10, 16], [2, 8], [1, 6], [7, 12]], 2),
-    ([[1, 2], [3, 4], [5, 6], [7, 8]], 4)
+    ([[1, 2], [3, 4], [5, 6], [7, 8]], 4),
+    ([[1, 2], [2, 3], [3, 4], [4, 5]], 2)
 ])
 def test_find_min_arrow_shots(points: list[list[int]], expected: int):
     assert False
