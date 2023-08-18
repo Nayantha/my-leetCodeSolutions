b8722a928dec49ed91e38cd48b580fe8fac7eccd
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-18 17:00:37 +0530

    add function docstring.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_minimum_number_of_arrows_to_burst_balloons.py
+++ tests/test_minimum_number_of_arrows_to_burst_balloons.py
@@ -10,4 +10,9 @@ from minimum_number_of_arrows_to_burst_balloons import find_min_arrow_shots
     ([[3, 9], [7, 12], [3, 8], [6, 8], [9, 10], [2, 9], [0, 9], [3, 9], [0, 6], [2, 8]], 2)
 ])
 def test_find_min_arrow_shots(points: list[list[int]], expected: int):
+    """
+    test find_min_arrow_shots in minimum_number_of_arrows_to_burst_balloons modules
+    :param points: data that send as argument to the testing function
+    :param expected: the expected result mapped to each point argument, used to validate the function output
+    """
     assert find_min_arrow_shots(points) == expected
