0c9506ab98f048fd337dfe4cc38a24d61d17615e
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-18 15:13:05 +0530

    add the pytest test framework.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_minimum_number_of_arrows_to_burst_balloons.py
+++ tests/test_minimum_number_of_arrows_to_burst_balloons.py
@@ -1,2 +1,6 @@
-def test_find_min_arrow_shots():
+import pytest
+
+
+@pytest.mark.parametrize("points, expected")
+def test_find_min_arrow_shots(points: list[list[int]], expected: int):
     assert False
