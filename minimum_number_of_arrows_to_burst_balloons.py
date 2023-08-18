06a3d3d193d0232a0664e39d561b9401aed6195f
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-18 16:53:20 +0530

    incorporate limit to logic.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- minimum_number_of_arrows_to_burst_balloons.py
+++ minimum_number_of_arrows_to_burst_balloons.py
@@ -17,7 +17,7 @@ def find_min_arrow_shots_with_limit(points: list[list[int]], limit: int = 2) -> 
     res = [points[0] + [1]]
     for start, end in points[1:]:
         last_end = res[-1][1]
-        if start <= last_end and res[-1][2] == 1:
+        if start <= last_end and res[-1][2] == limit - 1:
             res[-1][1] = min(last_end, end)
             res[-1][0] = max(start, res[-1][0])
             res[-1][2] += 1
