7c61ce0f9f3c240a286dde03b7007dc7b5f95811
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-18 16:37:26 +0530

    add a solution.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- minimum_number_of_arrows_to_burst_balloons.py
+++ minimum_number_of_arrows_to_burst_balloons.py
@@ -1,2 +1,11 @@
 def find_min_arrow_shots(points: list[list[int]]) -> int:
-    ...
+    points.sort(key=lambda x: x[0])
+    res = [points[0] + [1]]
+    for start, end in points[1:]:
+        last_end = res[-1][1]
+        if start <= last_end and res[-1][2] == 1:
+            res[-1][1] = max(last_end, end)
+            res[-1][2] += 1
+        else:
+            res.append([start, end, 1])
+    return len(res)
