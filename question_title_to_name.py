8fc42a3c61ea65197220807b1e71b3fee4e5bcc8
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-18 15:06:26 +0530

    uncomment create test file with the solution file.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- question_title_to_name.py
+++ question_title_to_name.py
@@ -4,8 +4,8 @@ def main():
     print(file_name)
     with open(f"{file_name}.py", "w") as new_solution_file:
         ...
-    # with open(f"./tests/test_{file_name}.py", "w") as new_test_solution_file:
-    #     ...
+    with open(f"./tests/test_{file_name}.py", "w") as new_test_solution_file:
+        ...
     return file_name
 
 
