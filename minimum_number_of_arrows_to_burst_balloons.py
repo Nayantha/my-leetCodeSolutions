91fe7657eeabbe3d67d0f70324b7ab5498b97575
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-18 16:55:25 +0530

    remove limit related variables and logic.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- minimum_number_of_arrows_to_burst_balloons.py
+++ minimum_number_of_arrows_to_burst_balloons.py
@@ -1,14 +1,13 @@
 def find_min_arrow_shots(points: list[list[int]]) -> int:
     points.sort(key=lambda x: x[1])
-    res = [points[0] + [1]]
+    res = [points[0]]
     for start, end in points[1:]:
         last_end = res[-1][1]
-        if start <= last_end and res[-1][2] == 1:
+        if start <= last_end:
             res[-1][1] = min(last_end, end)
             res[-1][0] = max(start, res[-1][0])
-            # res[-1][2] += 1
         else:
-            res.append([start, end, 1])
+            res.append([start, end])
     return len(res)
 
 
