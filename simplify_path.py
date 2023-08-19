e2e6af0edec6d39fe860dec72fd1cb176bcc26b4
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-19 16:05:38 +0530

    add a solution.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- simplify_path.py
+++ simplify_path.py
@@ -1,3 +1,3 @@
 # https://leetcode.com/problems/simplify-path/
 def simplify_path(path: str) -> str:
-    ...
+    return path.replace("//", "/").removesuffix("/").replace("..", "").replace("/.", "/")
