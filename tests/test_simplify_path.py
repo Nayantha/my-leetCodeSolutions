84f00859aa499d437736b17f91567175d4dfa341
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-19 15:57:01 +0530

    add test logic.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_simplify_path.py
+++ tests/test_simplify_path.py
@@ -1,6 +1,8 @@
 import pytest
 
+from simplify_path import simplify_path
+
 
 @pytest.mark.parametrize("path, expected")
 def test_simplify_path(path: str, expected: str):
-    assert False
+    assert simplify_path(path) == expected
