7d7c23e3dfc00061dfb4151944124113eebed851
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-19 15:58:40 +0530

    add test data.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_simplify_path.py
+++ tests/test_simplify_path.py
@@ -3,6 +3,10 @@ import pytest
 from simplify_path import simplify_path
 
 
-@pytest.mark.parametrize("path, expected")
+@pytest.mark.parametrize("path, expected", [
+    ("/home/", "/home"),
+    ("/../", "/"),
+    ("/home//foo/", "/home/foo")
+])
 def test_simplify_path(path: str, expected: str):
     assert simplify_path(path) == expected
