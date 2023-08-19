c4f60a877762c20f5644aec25012fd4eaf1a12e0
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-19 16:37:38 +0530

    add a new solution.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- simplify_path.py
+++ simplify_path.py
@@ -11,3 +11,19 @@ def simplify_path(path: str) -> str:
         else:
             path_stack.append(folder_name)
     return "/" + "/".join(path_stack)
+
+
+def simplify_path_ii(path: str) -> str:
+    stack = []
+    cur = ""
+    for c in path + "/":
+        if c == "/":
+            if cur == "..":
+                if stack:
+                    stack.pop()
+                elif cur != "" and cur != ".":
+                    stack.append(cur)
+                cur = ""
+        else:
+            cur += c
+    return "/" + "/".join(stack)
