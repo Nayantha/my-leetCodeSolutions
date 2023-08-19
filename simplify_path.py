d46c0100886159a7e12dd1f123d5e7ef03ede199
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-19 16:28:15 +0530

    finish an accepted solution.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- simplify_path.py
+++ simplify_path.py
@@ -1,3 +1,13 @@
 # https://leetcode.com/problems/simplify-path/
 def simplify_path(path: str) -> str:
-    return path.replace("//", "/").removesuffix("/").replace("..", "").replace("/.", "/")
+    path_stack = []
+    folder_names = [folder_name for folder_name in path.removesuffix("/").replace("//", "/").split("/") if folder_name]
+    for folder_name in folder_names:
+        if folder_name == "..":
+            if len(path_stack):
+                path_stack.pop()
+        elif folder_name == ".":
+            continue
+        else:
+            path_stack.append(folder_name)
+    return "/" + "/".join(path_stack)
