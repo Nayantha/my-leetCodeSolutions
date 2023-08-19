a8e1302725e84e6d08aab187abeadd9c22717aa2
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-19 16:09:00 +0530

    add a new test case.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_simplify_path.py
+++ tests/test_simplify_path.py
@@ -6,7 +6,8 @@ from simplify_path import simplify_path
 @pytest.mark.parametrize("path, expected", [
     ("/home/", "/home"),
     ("/../", "/"),
-    ("/home//foo/", "/home/foo")
+    ("/home//foo/", "/home/foo"),
+    ("/a/./b/../../c/", "/c")
 ])
 def test_simplify_path(path: str, expected: str):
     assert simplify_path(path) == expected
