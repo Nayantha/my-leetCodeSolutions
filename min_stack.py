3c392cb6c94368f8921075bd136fc83b1ebea158
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-20 08:32:30 +0530

    copy skeleton code from the site.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- min_stack.py
+++ min_stack.py
@@ -1,1 +1,12 @@
 # https://leetcode.com/problems/min-stack/
+class MinStack:
+
+    def __init__(self):
+
+    def push(self, val: int) -> None:
+
+    def pop(self) -> None:
+
+    def top(self) -> int:
+
+    def getMin(self) -> int:
