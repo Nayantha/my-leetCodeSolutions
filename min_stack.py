9fb834189b69437f2355e8021c0b423223b3c39c
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-20 10:09:35 +0530

    modify the logic in push method.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- min_stack.py
+++ min_stack.py
@@ -8,7 +8,7 @@ class MinStack:
     def push(self, val: int) -> None:
         if not self.min_val or self.min_val > val:
             self.min_val = val
-        self.stack[-1] = val
+        self.stack.append(val)
 
     def pop(self) -> None:
         self.stack.pop()
