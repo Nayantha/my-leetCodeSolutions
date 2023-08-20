2119647e911408d8df1b18e63e88ab5cfe3481f1
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-20 10:42:27 +0530

    implement a list to keep track of the min value.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- min_stack.py
+++ min_stack.py
@@ -1,21 +1,21 @@
 # https://leetcode.com/problems/min-stack/
 class MinStack:
-    min_val: int | None = None
 
     def __init__(self):
         self.stack = []
+        self.min_value_stack = []
 
     def push(self, val: int) -> None:
-        if not self.min_val or self.min_val > val:
-            self.min_val = val
         self.stack.append(val)
+        val = min(val, self.min_value_stack[-1] if self.min_value_stack else val)
+        self.min_value_stack.append(val)
 
     def pop(self) -> None:
         self.stack.pop()
-        self.min_val = min(self.stack)
+        self.min_value_stack.pop()
 
     def top(self) -> int:
         return self.stack[-1]
 
     def get_min(self) -> int:
-        return self.min_val
+        return self.min_value_stack[-1]
