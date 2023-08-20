321a96fc5a69fc5f85cb6acdabe3e107e9b494b7
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-20 10:22:27 +0530

    add test logic.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_min_stack.py
+++ tests/test_min_stack.py
@@ -1,6 +1,13 @@
-import pytest
+from min_stack import MinStack
 
 
-@pytest.mark.parametrize("")
 class TestMinStack:
-    pass
+    def test_min_stack_01(self):
+        min_stack = MinStack()
+        min_stack.push(-2)
+        min_stack.push(0)
+        min_stack.push(-3)
+        assert min_stack.get_min() == -3
+        min_stack.pop()
+        assert min_stack.top() == 0
+        assert min_stack.get_min() == -2
