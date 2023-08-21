9396ffd0a35c8558ee6967f3afcedb04c18fe596
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-21 13:36:57 +0530

    add test data.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_evaluate_reverse_polish_notation.py
+++ tests/test_evaluate_reverse_polish_notation.py
@@ -3,6 +3,9 @@ import pytest
 from evaluate_reverse_polish_notation import eval_rpn
 
 
-@pytest.mark.parametrize("tokens, expected")
+@pytest.mark.parametrize("tokens, expected", [
+    (["2", "1", "+", "3", "*"], 9),
+    (["10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"], 22)
+])
 def test_eval_rpn(tokens: list[str], expected: int):
     assert eval_rpn(tokens) == expected
