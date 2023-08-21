6aedb64793db36923e69cd59ed3ff20c1c3e692b
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-21 13:32:19 +0530

    add test logic.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_evaluate_reverse_polish_notation.py
+++ tests/test_evaluate_reverse_polish_notation.py
@@ -1,6 +1,8 @@
 import pytest
 
+from evaluate_reverse_polish_notation import eval_rpn
+
 
 @pytest.mark.parametrize("tokens, expected")
 def test_eval_rpn(tokens: list[str], expected: int):
-    assert False
+    assert eval_rpn(tokens) == expected
