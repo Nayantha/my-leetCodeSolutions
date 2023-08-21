2dc801c7335a74a67dd6a17f160d4ab250cad32a
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-21 14:01:17 +0530

    optimize the solution.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- evaluate_reverse_polish_notation.py
+++ evaluate_reverse_polish_notation.py
@@ -2,17 +2,16 @@
 def eval_rpn(tokens: list[str]) -> int:
     number_stack: list[int] = []
     for token in tokens:
-        if token.lstrip("-").isnumeric():
-            number_stack.append(int(token))
-            continue
-        num1 = number_stack.pop()
-        num2 = number_stack.pop()
         if token == "+":
-            number_stack.append(num2 + num1)
+            number_stack.append(number_stack.pop() + number_stack.pop())
         elif token == "-":
+            num1, num2 = number_stack.pop(), number_stack.pop()
             number_stack.append(num2 - num1)
         elif token == "*":
-            number_stack.append(num1 * num2)
+            number_stack.append(number_stack.pop() * number_stack.pop())
         elif token == "/":
+            num1, num2 = number_stack.pop(), number_stack.pop()
             number_stack.append(int(num2 / num1))
+        else:
+            number_stack.append(int(token))
     return number_stack.pop()
