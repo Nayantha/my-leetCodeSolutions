3a67a9337327b079ec0ba46a18b18dde72907168
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-21 13:35:51 +0530

    implement stack logic to build up the solution.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- evaluate_reverse_polish_notation.py
+++ evaluate_reverse_polish_notation.py
@@ -1,3 +1,7 @@
 # https://leetcode.com/problems/evaluate-reverse-polish-notation/
 def eval_rpn(tokens: list[str]) -> int:
-    ...
+    number_stack: list[int] = []
+    for token in tokens:
+        if token.isnumeric():
+            number_stack.append(int(token))
+    return number_stack.pop()
