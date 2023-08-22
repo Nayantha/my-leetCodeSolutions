98f45238211b547a67380c74eb575917af4cc5e9
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-22 21:09:04 +0530

    add a solution logic.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- add_two_numbers.py
+++ add_two_numbers.py
@@ -9,4 +9,11 @@ class ListNode:
 
 
 def add_two_numbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-    ...
+    remainder = 0
+    while l1 and l2:
+        l1 = l1.val + l2.val
+        l1 = l1.next
+        l2 = l2.next
+    if l2 and not l1:
+        l1 = l2
+    return l1
