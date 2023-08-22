f65188288a184df29bb4ab53b383675be62c64e3
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-22 21:49:56 +0530

    add another solution logic.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- add_two_numbers.py
+++ add_two_numbers.py
@@ -11,12 +11,19 @@ class ListNode:
 def add_two_numbers(l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
     result_list = l1
     remainder = 0
-    while l1 and l2:
-        addition = l1.val + l2.val + remainder
-        l1 = ListNode(addition % 10, l1.next)
-        remainder = int(addition / 10)
+    l1_str = ""
+    while l1:
+        l1_str += str(l1.val)
         l1 = l1.next
+    l2_str = ""
+    while l2:
+        l2_str += str(l2.val)
         l2 = l2.next
-    if l2 and not l1:
-        l1 = l2
-    return result_list
+    add = str(int(l1_str[::-1]) + int(l2_str[::-1]))[::-1]
+    result_node_list = [ListNode(int(x)) for x in add]
+    result = result_node_list[0]
+    cur = result
+    for i in result_node_list[1:]:
+        cur.next = i
+        cur = cur.next
+    return result
