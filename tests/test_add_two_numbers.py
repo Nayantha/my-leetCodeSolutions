969ee644c21d5a7d0f5004329a85f20d67e1e66a
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-22 21:18:31 +0530

    add test data.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_add_two_numbers.py
+++ tests/test_add_two_numbers.py
@@ -1,2 +1,20 @@
+from add_two_numbers import ListNode, add_two_numbers
+
+
 def test_add_two_numbers():
-    assert False
+    l1_val_list = [ListNode(2), ListNode(4), ListNode(3)]
+    l1_val_list[0].next = l1_val_list[1]
+    l1_val_list[1].next = l1_val_list[2]
+    l1 = l1_val_list[0]
+
+    l2_val_list = [ListNode(5), ListNode(6), ListNode(4)]
+    l2_val_list[0].next = l2_val_list[1]
+    l2_val_list[1].next = l2_val_list[2]
+    l2 = l2_val_list[0]
+
+    expected_val_list = [ListNode(7), ListNode(0), ListNode(8)]
+    expected_val_list[0].next = expected_val_list[1]
+    expected_val_list[1].next = expected_val_list[2]
+    expected = expected_val_list[0]
+
+    output = add_two_numbers(l1, l2)
