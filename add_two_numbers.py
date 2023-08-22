991944b593d00a4040d10c285a62b3d5e4d8afac
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-22 20:28:26 +0530

    add list node class.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- add_two_numbers.py
+++ add_two_numbers.py
@@ -1,2 +1,9 @@
 # https://leetcode.com/problems/add-two-numbers/
-def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+def add_two_numbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+    ...
