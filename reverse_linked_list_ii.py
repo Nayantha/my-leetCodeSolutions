fe0ac0721caf8687c041d2a2e2e6bba154be8478
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-23 10:47:02 +0530

    add solution logic.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- reverse_linked_list_ii.py
+++ reverse_linked_list_ii.py
@@ -5,4 +5,17 @@ from add_two_numbers import ListNode
 
 
 def reverse_between(head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
-    ...
+    prev, current = None, head
+    current_position = 1
+    temp_head = head
+    while temp_head:
+        next_node = temp_head.next
+        if left <= current_position <= right:
+            temp_head.next = prev
+        elif current_position > right:
+            break
+        current_position += 1
+        prev = current
+        current = temp_head.next
+        temp_head = next_node
+    return head
