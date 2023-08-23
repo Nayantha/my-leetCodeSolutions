29d318308a4024dbc6691d1d1d6975e3fff208d5
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-23 07:44:22 +0530

    add more linked list way.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- add_two_numbers.py
+++ add_two_numbers.py
@@ -25,3 +25,19 @@ def add_two_numbers(l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[
         cur.next = i
         cur = cur.next
     return result
+
+
+def add_two_numbers_ii(l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+    dummy = ListNode()
+    cur = dummy
+    carry = 0
+    while l1 or l2 or carry:
+        num1 = l1.val if l1 else 0
+        num2 = l2.val if l2 else 0
+        addition = num1 + num2 + carry
+        carry = addition // 10
+        cur.next = ListNode(val=addition % 10)
+        cur = cur.next
+        l1 = l1.next if l1 else None
+        l2 = l2.next if l2 else None
+    return dummy.next
