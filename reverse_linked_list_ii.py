14f2617f9742e1ad99fe61dd92c7865ed0683faf
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-23 10:36:30 +0530

    fix errors and reformat according to python recommended format.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- reverse_linked_list_ii.py
+++ reverse_linked_list_ii.py
@@ -1,3 +1,8 @@
 # https://leetcode.com/problems/reverse-linked-list-ii/
-class Solution:
-    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+from typing import Optional
+
+from add_two_numbers import ListNode
+
+
+def reverse_between(head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+    ...
