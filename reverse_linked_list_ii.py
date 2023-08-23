0d03e1e61e839c46b69e09f32c55ddaa307f74cb
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-23 11:04:42 +0530

    add the final solution.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- reverse_linked_list_ii.py
+++ reverse_linked_list_ii.py
@@ -5,17 +5,16 @@ from add_two_numbers import ListNode
 
 
 def reverse_between(head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
-    prev, current = None, head
-    current_position = 1
-    temp_head = head
-    while temp_head:
-        next_node = temp_head.next
-        if left <= current_position <= right:
-            temp_head.next = prev
-        elif current_position > right:
-            break
-        current_position += 1
-        prev = current
-        current = temp_head.next
-        temp_head = next_node
-    return head
+    dummy_node = ListNode(0, head)
+    left_previous_pointer, curr = dummy_node, head
+    for i in range(left - 1):
+        left_previous_pointer, curr = curr, curr.next
+    prev = None
+    for i in range(right - left + 1):
+        next_node = curr.next
+        curr.next = prev
+        prev, curr = curr, next_node
+    # now curr is the node at position (right + 1), prev is pointing at the node at position right
+    left_previous_pointer.next.next = curr
+    left_previous_pointer.next = prev
+    return dummy_node.next
