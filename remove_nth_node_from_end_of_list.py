9f3577bb16a6a280152510b58e7513382861291d
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-24 12:53:41 +0530

    add solution logic.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- remove_nth_node_from_end_of_list.py
+++ remove_nth_node_from_end_of_list.py
@@ -1,8 +1,18 @@
 # https://leetcode.com/problems/remove-nth-node-from-end-of-list/
 from typing import Optional
 
-from linked_list_cycle import ListNode
+from add_two_numbers import ListNode
 
 
 def remove_nth_from_end(head: Optional[ListNode], n: int) -> Optional[ListNode]:
-    ...
+    dummy = head
+    total_nodes = 0
+    while dummy:
+        total_nodes += 1
+        dummy = dummy.next
+    index_of_node_to_remove = total_nodes - n
+    dummy = head
+    for i in range(index_of_node_to_remove - 1):
+        dummy = dummy.next
+    dummy.next = dummy.next.next
+    return head
