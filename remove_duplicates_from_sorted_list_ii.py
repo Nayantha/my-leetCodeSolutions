903b51a4f77422549b1cd83fc431fbb5b92e9f2e
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-25 16:23:01 +0530

    add a solution logic.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- remove_duplicates_from_sorted_list_ii.py
+++ remove_duplicates_from_sorted_list_ii.py
@@ -5,4 +5,13 @@ from list_node import ListNode
 
 
 def delete_duplicates(head: Optional[ListNode]) -> Optional[ListNode]:
-    ...
+    dummy = ListNode(next_node=head)
+    previous_node, current_node = dummy, head
+    while current_node:
+        if current_node.val == current_node.next.next.val:
+            while current_node.val == current_node.next.next.val:
+                current_node = current_node.next
+            previous_node.next = current_node
+        previous_node = current_node
+        current_node = current_node.next
+    return dummy.next
