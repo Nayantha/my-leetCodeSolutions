ec69cd466331c65dd86e3211bdef3be96337e2f1
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-25 15:02:38 +0530

    extract the list node class.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- remove_duplicates_from_sorted_list_ii.py
+++ remove_duplicates_from_sorted_list_ii.py
@@ -1,12 +1,5 @@
 # https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii
 from typing import Optional
 
-
-class ListNode:
-    def __init__(self, val=0, next_node=None):
-        self.val = val
-        self.next = next_node
-
-
 def delete_duplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
     ...
