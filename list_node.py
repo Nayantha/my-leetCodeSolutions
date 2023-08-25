ada6e00aa8c49a01338d901a6501c6446eeaeb8e
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-25 15:07:50 +0530

    add docstring for class.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- list_node.py
+++ list_node.py
@@ -1,4 +1,8 @@
 class ListNode:
+    """
+    ListNode keep track of the value and what node comes next.
+    """
+
     def __init__(self, val=0, next_node=None):
         self.val = val
         self.next = next_node
