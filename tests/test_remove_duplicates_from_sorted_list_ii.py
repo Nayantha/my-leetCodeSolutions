37463e84a9c5466109cfc67dff20fd94c76d1388
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-25 16:57:36 +0530

    add test logic incorporated with the function to be tested.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_remove_duplicates_from_sorted_list_ii.py
+++ tests/test_remove_duplicates_from_sorted_list_ii.py
@@ -1,8 +1,7 @@
-from re
-
 import pytest
 
 from list_node import ListNode
+from remove_duplicates_from_sorted_list_ii import delete_duplicates
 
 
 @pytest.mark.parametrize("input_list, expected_list", [
@@ -10,10 +9,10 @@ from list_node import ListNode
     ([1, 1, 1, 2, 3], [2, 3])
 ])
 def test_delete_duplicates(input_list: list[int], expected_list: list[int]):
-    input_list_head = ListNode(val=input_list[0])
     expected_list_head = ListNode(val=expected_list[0])
-    assert input_list_head == expected_list_head
-    while input_list_head and expected_list_head:
-        assert input_list_head.val == expected_list_head.val
-        input_list_head = input_list_head.next
+    output_list_head = delete_duplicates(ListNode(val=input_list[0]))
+    assert output_list_head == expected_list_head
+    while output_list_head and expected_list_head:
+        assert output_list_head.val == expected_list_head.val
+        output_list_head = output_list_head.next
         expected_list_head.next = expected_list_head.next
