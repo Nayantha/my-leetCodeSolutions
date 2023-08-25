a0b2ac3d9fe81d72cff3e43fb912b89521fd9926
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-25 15:01:38 +0530

    fix errors and reformat according to python recommended format.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- remove_duplicates_from_sorted_list_ii.py
+++ remove_duplicates_from_sorted_list_ii.py
@@ -1,8 +1,12 @@
 # https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii
+from typing import Optional
+
+
 class ListNode:
     def __init__(self, val=0, next_node=None):
         self.val = val
         self.next = next_node
 
 
-def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+def delete_duplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    ...
