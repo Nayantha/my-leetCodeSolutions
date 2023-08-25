87773e48e4bef9a21dbb15391db1debba78c236c
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-25 15:00:23 +0530

    add ListNode class.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- remove_duplicates_from_sorted_list_ii.py
+++ remove_duplicates_from_sorted_list_ii.py
@@ -1,1 +1,6 @@
 # https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii
+class ListNode:
+    def __init__(self, val=0, next_node=None):
+        self.val = val
+        self.next = next_node
+     
