547e1d6a33836f2c0592512d6203bcae4830ba28
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-25 16:51:07 +0530

    add pytest framework.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_remove_duplicates_from_sorted_list_ii.py
+++ tests/test_remove_duplicates_from_sorted_list_ii.py
@@ -1,6 +1,9 @@
+import pytest
+
 from list_node import ListNode
 
 
-def test_delete_duplicates():
+@pytest.mark.parametrize("input_list, expected_list")
+def test_delete_duplicates(input_list: list[int], expected_list: list[int]):
     list_values = [1, 2, 3, 3, 4, 4, 5]
     head = ListNode(val=list_values[0])
