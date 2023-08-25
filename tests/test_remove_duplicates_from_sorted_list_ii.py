641babede96fe531058f7f8a6d461fa80030f11d
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-25 16:52:29 +0530

    add test case data.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_remove_duplicates_from_sorted_list_ii.py
+++ tests/test_remove_duplicates_from_sorted_list_ii.py
@@ -3,7 +3,10 @@ import pytest
 from list_node import ListNode
 
 
-@pytest.mark.parametrize("input_list, expected_list")
+@pytest.mark.parametrize("input_list, expected_list", [
+    ([1, 2, 3, 3, 4, 4, 5], [1, 2, 5]),
+    ([1, 1, 1, 2, 3], [2, 3])
+])
 def test_delete_duplicates(input_list: list[int], expected_list: list[int]):
     list_values = [1, 2, 3, 3, 4, 4, 5]
     head = ListNode(val=list_values[0])
