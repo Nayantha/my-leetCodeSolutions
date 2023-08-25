4d16647498994db4a1faaa0ce9f0104705040901
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-25 15:11:31 +0530

    import list node class.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- remove_duplicates_from_sorted_list_ii.py
+++ remove_duplicates_from_sorted_list_ii.py
@@ -1,5 +1,8 @@
 # https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii
 from typing import Optional
 
+from list_node import ListNode
+
+
 def delete_duplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
     ...
