3e21b4630d38440f6a3607039a184b3309e85233
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-26 09:53:00 +0530

    add test data.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_rotate_list.py
+++ tests/test_rotate_list.py
@@ -1,6 +1,9 @@
 import pytest
 
 
-@pytest.mark.parametrize("initial_list, number_of_rotations, rotated_list")
+@pytest.mark.parametrize("initial_list, number_of_rotations, rotated_list", [
+    ([1, 2, 3, 4, 5], 2, [4, 5, 1, 2, 3]),
+    ([0, 1, 2], 4, [2, 0, 1])
+])
 def test_rotate_right(initial_list: list[int], number_of_rotations: int, rotated_list: list[int]):
     assert False
