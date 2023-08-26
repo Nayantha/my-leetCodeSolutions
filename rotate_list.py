26026a76fb1fef8df7f05de438dd0dc7ad682842
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-26 11:37:58 +0530

    add a new solution logic.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- rotate_list.py
+++ rotate_list.py
@@ -12,14 +12,15 @@ def rotate_right(head: Optional[ListNode], k: int) -> Optional[ListNode]:
             head = head.next
         return length
 
-    dummy = ListNode(next_node=head)
-    for i in range(k):
-        previous_node = dummy
-        current_node = head
-        while current_node.next:
-            previous_node = current_node
-            current_node = current_node.next
-        current_node.next = dummy.next
-        dummy.next = current_node
-        previous_node.next = None
-    return dummy.next
+    total_num_of_nodes = find_length_of_list(head)
+    total_num_of_jumps = (total_num_of_nodes - k - 1) % total_num_of_nodes
+    current_node = head
+    for i in range(total_num_of_jumps):
+        current_node = current_node.next
+    new_head = current_node.next
+    current_node.next = None
+    current_node = new_head
+    while current_node.next:
+        current_node = current_node.next
+    current_node.next = head
+    return new_head
