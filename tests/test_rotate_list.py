55db514c97a414d2f9df3cbd61c5ebc446c8c4ff
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-26 09:48:53 +0530

    add pytest framework coding.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_rotate_list.py
+++ tests/test_rotate_list.py
@@ -1,2 +1,6 @@
-def test_rotate_right():
+import pytest
+
+
+@pytest.mark.parametrize("initial_list, rotated_list")
+def test_rotate_right(initial_list: list[int], rotated_list: list[int]):
     assert False
