a7ae1849f8c66a686c551d40f4aba695b4bb63ea
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-26 11:51:55 +0530

    add less space complex solution.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- rotate_list.py
+++ rotate_list.py
@@ -7,21 +7,17 @@ from data_structures.list_node import ListNode
 def rotate_right(head: Optional[ListNode], k: int) -> Optional[ListNode]:
     if not head:
         return head
-    total_num_of_nodes, tail = 1, head
-    while tail.next:
-        total_num_of_nodes += 1
-        tail = tail.next
-
-    k = k % total_num_of_nodes
-    if k == 0:
-        return head
-
-    current_node = head
-    for i in range(total_num_of_nodes - k - 1):
-        current_node = current_node.next
-
-    new_head = current_node.next
-    current_node.next = None
-
-    tail.next = head
+    length = 1
+    curr = head
+    while curr.next:
+        length += 1
+        curr = curr.next
+    # set the next pointer of original list's tail to original list's head
+    curr.next = head
+    k = length - k % length
+    while k > 0:
+        k -= 1
+        curr = curr.next
+    new_head = curr.next
+    curr.next = None
     return new_head
