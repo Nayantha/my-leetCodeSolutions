07b6b9b72c5bb00ba26216b5e232d6946c2412b5
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-26 10:53:03 +0530

    add a possible solution.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- rotate_list.py
+++ rotate_list.py
@@ -8,9 +8,11 @@ def rotate_right(head: Optional[ListNode], k: int) -> Optional[ListNode]:
     dummy = ListNode(next_node=head)
     for i in range(k):
         previous_node = dummy
-        head = dummy.next
-        while head.next:
-            head = head.next
+        current_node = head
+        while current_node.next:
+            previous_node = current_node
+            current_node = current_node.next
         previous_node.next = None
-        head.next = dummy.next
+        current_node.next = dummy.next
+        dummy.next = current_node
     return dummy.next
