afcf9e56045b40089de7b0e926b45e9a6c90ff12
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-26 10:41:05 +0530

    implement array_to_list function.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_rotate_list.py
+++ tests/test_rotate_list.py
@@ -1,6 +1,6 @@
 import pytest
 
-from data_structures.list_node import ListNode
+from data_structures.singly_linked_list import array_to_list
 from rotate_list import rotate_right
 
 
@@ -9,8 +9,8 @@ from rotate_list import rotate_right
     ([0, 1, 2], 4, [2, 0, 1])
 ])
 def test_rotate_right(initial_list: list[int], number_of_rotations: int, rotated_list: list[int]):
-    output_list_head = rotate_right(ListNode(val=initial_list[0]), number_of_rotations)
-    rotated_list_head = ListNode(val=rotated_list[0])
+    output_list_head = rotate_right(array_to_list(initial_list), number_of_rotations)
+    rotated_list_head = array_to_list(rotated_list)
     while rotated_list_head and output_list_head:
         assert rotated_list_head.val == output_list_head.val
         rotated_list_head = rotated_list_head.next
