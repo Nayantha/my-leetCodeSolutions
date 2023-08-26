641d158b85280745fde9d20834da7e8bac4290a1
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-26 09:51:31 +0530

    add missing parameter.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_rotate_list.py
+++ tests/test_rotate_list.py
@@ -1,6 +1,6 @@
 import pytest
 
 
-@pytest.mark.parametrize("initial_list, rotated_list")
-def test_rotate_right(initial_list: list[int], rotated_list: list[int]):
+@pytest.mark.parametrize("initial_list, number_of_rotations, rotated_list")
+def test_rotate_right(initial_list: list[int], number_of_rotations: int, rotated_list: list[int]):
     assert False
