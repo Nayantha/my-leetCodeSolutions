ea2deb21ac8696bb1912a5448f2da96e965e3c40
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-26 09:58:17 +0530

    add test logic.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_rotate_list.py
+++ tests/test_rotate_list.py
@@ -1,9 +1,17 @@
 import pytest
 
+from list_node import ListNode
+from rotate_list import rotate_right
+
 
 @pytest.mark.parametrize("initial_list, number_of_rotations, rotated_list", [
     ([1, 2, 3, 4, 5], 2, [4, 5, 1, 2, 3]),
     ([0, 1, 2], 4, [2, 0, 1])
 ])
 def test_rotate_right(initial_list: list[int], number_of_rotations: int, rotated_list: list[int]):
-    assert False
+    output_list_head = rotate_right(ListNode(initial_list[0]), number_of_rotations)
+    rotated_list_head = ListNode(rotated_list[0])
+    while rotated_list_head and output_list_head:
+        assert rotated_list_head.val == output_list_head.val
+        rotated_list_head = rotated_list_head.next
+        output_list_head = output_list_head.next
