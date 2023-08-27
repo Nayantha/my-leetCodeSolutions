389d720371d9d6c633e157f7675dc6a26a59e643
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-27 10:36:29 +0530

    add test logic.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_partition_list.py
+++ tests/test_partition_list.py
@@ -1,6 +1,7 @@
 import pytest
 
 from data_structures.singly_linked_list import array_to_list
+from partition_list import partition
 
 
 @pytest.mark.parametrize("input_list_values,partition_value,partitioned_list_values", [
@@ -10,4 +11,8 @@ from data_structures.singly_linked_list import array_to_list
 def test_partition(input_list_values: list[int], partition_value: int, partitioned_list_values: list[int]):
     input_list_head = array_to_list(input_list_values)
     partitioned_list_head = array_to_list(partitioned_list_values)
-    
+    output_list_head = partition(input_list_head, partition_value)
+    while partitioned_list_head and output_list_head:
+        assert output_list_head.val == partitioned_list_head.val
+        output_list_head = output_list_head.next
+        partitioned_list_head = partitioned_list_head.next
