aa667e7b4649c354d9a5b2fc234ab22076cc142d
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-27 10:32:50 +0530

    add a missing parameter.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_partition_list.py
+++ tests/test_partition_list.py
@@ -1,6 +1,6 @@
 import pytest
 
 
-@pytest.mark.parametrize("input_list_values,partitioned_list_values")
-def test_partition(input_list_values: list[int], partitioned_list_values: list[int]):
+@pytest.mark.parametrize("input_list_values,partition_value,partitioned_list_values")
+def test_partition(input_list_values: list[int], partition_value: int, partitioned_list_values: list[int]):
     assert False
