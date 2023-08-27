ebe47219a45b68720e0b9696342d79f4ae530ca7
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-27 10:31:31 +0530

    add pytest framework.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_partition_list.py
+++ tests/test_partition_list.py
@@ -1,2 +1,6 @@
-def test_partition():
+import pytest
+
+
+@pytest.mark.parametrize("input_list_values,partitioned_list_values")
+def test_partition(input_list_values: list[int], partitioned_list_values: list[int]):
     assert False
