d82e5cd5f111c591920048409dc91a39b389e802
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-27 10:34:02 +0530

    add test data.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_partition_list.py
+++ tests/test_partition_list.py
@@ -1,6 +1,9 @@
 import pytest
 
 
-@pytest.mark.parametrize("input_list_values,partition_value,partitioned_list_values")
+@pytest.mark.parametrize("input_list_values,partition_value,partitioned_list_values", [
+    ([1, 4, 3, 2, 5, 2], 3, [1, 2, 2, 4, 3, 5]),
+    ([2, 1], 2, [1, 2]),
+])
 def test_partition(input_list_values: list[int], partition_value: int, partitioned_list_values: list[int]):
     assert False
