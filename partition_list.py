9e951614d3bb6d0d2fbeff225bbca7fcd2d873e7
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-27 10:06:28 +0530

    add a solution logic.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- partition_list.py
+++ partition_list.py
@@ -5,4 +5,16 @@ from data_structures.list_node import ListNode
 
 
 def partition(head: Optional[ListNode], x: int) -> Optional[ListNode]:
-    ...
+    left, right = ListNode(), ListNode()
+    left_tail, right_tail = left, right
+    while head:
+        if head.val < x:
+            left_tail.next = head
+            left_tail = left_tail.next
+        else:
+            right_tail = head
+            right_tail = right_tail.next
+        head = head.next
+    left_tail.next = right.next
+    right_tail.next = None
+    return left.next
