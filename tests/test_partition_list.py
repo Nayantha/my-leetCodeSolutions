8c13cfba9a89c94b90dc839cb0cbae612ac3e0c1
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-27 10:35:24 +0530

    add array to list methods.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_partition_list.py
+++ tests/test_partition_list.py
@@ -1,9 +1,13 @@
 import pytest
 
+from data_structures.singly_linked_list import array_to_list
+
 
 @pytest.mark.parametrize("input_list_values,partition_value,partitioned_list_values", [
     ([1, 4, 3, 2, 5, 2], 3, [1, 2, 2, 4, 3, 5]),
     ([2, 1], 2, [1, 2]),
 ])
 def test_partition(input_list_values: list[int], partition_value: int, partitioned_list_values: list[int]):
-    assert False
+    input_list_head = array_to_list(input_list_values)
+    partitioned_list_head = array_to_list(partitioned_list_values)
+    
