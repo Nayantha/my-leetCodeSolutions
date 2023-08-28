0e80a7a93b5e373d1bf04f19a12c8b62a4d2bf6d
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-28 14:27:02 +0530

    add class variables.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- lru_cache.py
+++ lru_cache.py
@@ -1,8 +1,9 @@
 # https://leetcode.com/problems/lru-cache/
 class LRUCache:
+    map = {}
 
     def __init__(self, capacity: int):
-        ...
+        self.capacity = capacity
 
     def get(self, key: int) -> int:
         ...
