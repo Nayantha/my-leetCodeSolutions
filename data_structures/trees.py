fef3dc2b0dab92dc7052122a3559dcc1bb242bec
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-29 17:33:22 +0530

    add a function to collect values of each node in pre order.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- data_structures/trees.py
+++ data_structures/trees.py
@@ -3,3 +3,11 @@ from data_structures.tree_node import TreeNode
 
 def generate_list_according_to_pre_order(root: TreeNode):
     ...
+
+
+def traverse_in_binary_tree(root: TreeNode, pre_order_value_list: list[int | None]):
+    if root:
+        pre_order_value_list.append(root.val)
+        traverse_in_binary_tree(root.left, pre_order_value_list)
+        traverse_in_binary_tree(root.right, pre_order_value_list)
+    pre_order_value_list.append(None)
