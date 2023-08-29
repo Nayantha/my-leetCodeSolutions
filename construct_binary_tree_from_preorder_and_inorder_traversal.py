d1b1aede1ed24871de4ed8ffddb7752a3aefa8eb
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-29 18:12:23 +0530

    implement the create node function.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- construct_binary_tree_from_preorder_and_inorder_traversal.py
+++ construct_binary_tree_from_preorder_and_inorder_traversal.py
@@ -7,7 +7,7 @@ from data_structures.tree_node import TreeNode
 def build_tree(preorder: list[int], inorder: list[int]) -> Optional[TreeNode]:
     root: Optional[TreeNode] = None
     for i in preorder:
-        new_node = TreeNode(val=i, left=inorder[:inorder.index(i)], right=inorder[inorder.index(i) + 1:])
+        new_node = create_new_node(i, inorder)
         if not root:
             root = new_node
     return root
