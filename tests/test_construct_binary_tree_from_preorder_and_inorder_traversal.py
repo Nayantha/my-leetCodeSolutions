41c5c88de295b8f9cc66f0e3199bd31f1aae7657
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-29 17:55:53 +0530

    add test logic.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_construct_binary_tree_from_preorder_and_inorder_traversal.py
+++ tests/test_construct_binary_tree_from_preorder_and_inorder_traversal.py
@@ -2,6 +2,7 @@ import pytest
 
 from construct_binary_tree_from_preorder_and_inorder_traversal import build_tree
 from data_structures.tree_node import TreeNode
+from data_structures.trees import generate_list_according_to_pre_order
 
 
 @pytest.mark.parametrize("pre_order_list_values, in_order_list_values, expected_tree_node_values_in_pre_order", [
@@ -11,3 +12,8 @@ from data_structures.tree_node import TreeNode
 def test_build_tree(pre_order_list_values: list[int], in_order_list_values: list[int],
                     expected_tree_node_values_in_pre_order: list[int]):
     tree_root: TreeNode = build_tree(preorder=pre_order_list_values, inorder=in_order_list_values)
+    pre_order_list_values_of_tree = generate_list_according_to_pre_order(tree_root)
+    for value in pre_order_list_values:
+        assert value in pre_order_list_values_of_tree
+    pre_order_list_values_of_tree = [value for value in pre_order_list_values_of_tree if value]
+    assert pre_order_list_values_of_tree == pre_order_list_values
