0ecedc5c583dc8d6c6bc730b24c0581fde173226
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-29 17:18:42 +0530

    add pytest framework.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_construct_binary_tree_from_preorder_and_inorder_traversal.py
+++ tests/test_construct_binary_tree_from_preorder_and_inorder_traversal.py
@@ -1,2 +1,7 @@
-def test_build_tree():
+import pytest
+
+
+@pytest.mark.parametrize("pre_order_list_values, in_order_list_values, expected_tree_node_values_in_pre_order")
+def test_build_tree(pre_order_list_values: list[int], in_order_list_values: list[int],
+                    expected_tree_node_values_in_pre_order: list[int]):
     assert False
