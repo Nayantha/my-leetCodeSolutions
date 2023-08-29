62e6c733e1550ed0a91a5e7e2b5ffc704dd9aa62
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-29 18:02:27 +0530

    add basic solution logic.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- construct_binary_tree_from_preorder_and_inorder_traversal.py
+++ construct_binary_tree_from_preorder_and_inorder_traversal.py
@@ -5,4 +5,9 @@ from data_structures.tree_node import TreeNode
 
 
 def build_tree(preorder: list[int], inorder: list[int]) -> Optional[TreeNode]:
-    ...
+    root: Optional[TreeNode] = None
+    for i in preorder:
+        new_node = TreeNode(val=i, left=inorder[:inorder.index(i)], right=inorder[inorder.index(i) + 1:])
+        if not root:
+            root = new_node
+    return root
