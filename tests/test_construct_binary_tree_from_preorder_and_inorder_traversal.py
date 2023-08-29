459c9d18bde1b94351d94109663e4305633f3dc2
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-29 17:21:56 +0530

    import the function to be tested.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_construct_binary_tree_from_preorder_and_inorder_traversal.py
+++ tests/test_construct_binary_tree_from_preorder_and_inorder_traversal.py
@@ -1,5 +1,8 @@
 import pytest
 
+from construct_binary_tree_from_preorder_and_inorder_traversal import build_tree
+from data_structures.tree_node import TreeNode
+
 
 @pytest.mark.parametrize("pre_order_list_values, in_order_list_values, expected_tree_node_values_in_pre_order", [
     ([3, 9, 20, 15, 7], [9, 3, 15, 20, 7], [3, 9, 20, None, None, 15, 7]),
@@ -7,4 +10,4 @@ import pytest
 ])
 def test_build_tree(pre_order_list_values: list[int], in_order_list_values: list[int],
                     expected_tree_node_values_in_pre_order: list[int]):
-    assert False
+    tree_root: TreeNode = build_tree(preorder=pre_order_list_values, inorder=in_order_list_values)
