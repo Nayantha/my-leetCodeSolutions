91deccfb136b6d0e2154aaeb4bbd71b5d030d4ff
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-29 17:03:38 +0530

    add docstring to initializer.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- data_structures/tree_node.py
+++ data_structures/tree_node.py
@@ -1,5 +1,11 @@
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
+        """
+
+        :param val:
+        :param left:
+        :param right:
+        """
         self.val = val
         self.left = left
         self.right = right
