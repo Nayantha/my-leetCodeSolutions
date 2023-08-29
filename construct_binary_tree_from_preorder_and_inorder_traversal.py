e5fd3fc5ee22595ea034a937e65a5c2e741ac7fd
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-29 17:15:17 +0530

    fix errors and format according to pep 8 recommendations.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- construct_binary_tree_from_preorder_and_inorder_traversal.py
+++ construct_binary_tree_from_preorder_and_inorder_traversal.py
@@ -1,2 +1,8 @@
 # https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
-def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+from typing import Optional
+
+from data_structures.tree_node import TreeNode
+
+
+def build_tree(preorder: list[int], inorder: list[int]) -> Optional[TreeNode]:
+    ...
