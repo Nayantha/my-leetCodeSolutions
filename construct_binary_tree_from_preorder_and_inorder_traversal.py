6b781e974b1592cfb7190c00f0999a012d8832ef
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-29 18:11:23 +0530

    create a logic to construct tree node with recursion.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- construct_binary_tree_from_preorder_and_inorder_traversal.py
+++ construct_binary_tree_from_preorder_and_inorder_traversal.py
@@ -11,3 +11,21 @@ def build_tree(preorder: list[int], inorder: list[int]) -> Optional[TreeNode]:
         if not root:
             root = new_node
     return root
+
+
+def create_new_node(i: int, inorder: list[int]) -> Optional[TreeNode]:
+    if i not in inorder:
+        return None
+    new_node: Optional[TreeNode] = TreeNode(val=i, left=inorder[:inorder.index(i)],
+                                            right=inorder[inorder.index(i) + 1:])
+    left_tree_value_list = inorder[:inorder.index(i)]
+    if not left_tree_value_list:
+        return None
+    left_tree_value = left_tree_value_list[len(left_tree_value_list) // 2]
+    new_node.left = create_new_node(left_tree_value, left_tree_value_list)
+    right_tree_value_list = inorder[inorder.index(i) + 1:]
+    if not right_tree_value_list:
+        return None
+    right_tree_value = right_tree_value_list[len(right_tree_value_list) // 2]
+    new_node.right = create_new_node(right_tree_value, right_tree_value_list)
+    return new_node
