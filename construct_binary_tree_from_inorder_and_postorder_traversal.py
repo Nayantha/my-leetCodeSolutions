c7ba094f9ead97191079fd89e8e9d6a50a213a9b
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-30 11:30:00 +0530

    remove the last used element.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- construct_binary_tree_from_inorder_and_postorder_traversal.py
+++ construct_binary_tree_from_inorder_and_postorder_traversal.py
@@ -7,7 +7,7 @@ from data_structures.tree_node import TreeNode
 def build_ree(inorder: list[int], postorder: list[int]) -> Optional[TreeNode]:
     if not postorder or not inorder:
         return None
-    root = TreeNode(postorder[-1])
-    mid = inorder.index(postorder[-1])
+    mid = inorder.index(postorder.pop())
+    root = TreeNode(inorder[mid])
     root.left = build_ree(inorder[:mid], postorder)
     root.right = build_ree(inorder[mid + 1:], postorder)
