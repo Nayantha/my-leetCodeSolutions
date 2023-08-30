76c7c35dd02716c58476100b6ce3e87dbc829be5
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-30 12:21:00 +0530

    add a new solution logic.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- construct_binary_tree_from_preorder_and_inorder_traversal.py
+++ construct_binary_tree_from_preorder_and_inorder_traversal.py
@@ -7,9 +7,15 @@ from data_structures.tree_node import TreeNode
 def build_tree(preorder: list[int], inorder: list[int]) -> Optional[TreeNode]:
     if not preorder or not inorder:
         return None
-    root = TreeNode(preorder[0])
-    mid = inorder.index(preorder[0])
-    root.left = build_tree(preorder[1:mid + 1], inorder[:mid])
-    root.right = build_tree(preorder[mid + 1:], inorder[mid + 1:])
-    return root
+    values_to_index_map_of_inorder_list = {value: index for index, value in enumerate(inorder)}
 
+    def helper_func(left, right):
+        if left > right:
+            return None
+        root = TreeNode(preorder.pop())
+        index_of_root_val_in_inorder_list = values_to_index_map_of_inorder_list[root.val]
+        root.right = helper_func(index_of_root_val_in_inorder_list + 1, right)
+        root.left = helper_func(left, index_of_root_val_in_inorder_list - 1)
+        return root
+
+    return helper_func(0, len(inorder) - 1)
