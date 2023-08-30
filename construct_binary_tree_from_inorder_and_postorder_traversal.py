24d6a7b03c058cb93b3e4ffda7839016c0b6580b
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-30 12:18:23 +0530

    add a body to the helper function.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- construct_binary_tree_from_inorder_and_postorder_traversal.py
+++ construct_binary_tree_from_inorder_and_postorder_traversal.py
@@ -10,6 +10,12 @@ def build_tree(inorder: list[int], postorder: list[int]) -> Optional[TreeNode]:
     values_to_index_map_of_inorder_list = {value: index for index, value in enumerate(inorder)}
 
     def helper_func(left, right):
-        ...
+        if left > right:
+            return None
+        root = TreeNode(postorder.pop())
+        index_of_root_val_in_inorder_list = values_to_index_map_of_inorder_list[root.val]
+        root.right = helper_func(index_of_root_val_in_inorder_list + 1, right)
+        root.left = helper_func(left, index_of_root_val_in_inorder_list - 1)
+        return root
 
     return helper_func(0, len(inorder) - 1)
