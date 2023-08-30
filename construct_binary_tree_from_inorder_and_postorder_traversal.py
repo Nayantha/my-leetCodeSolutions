5fca3df598a737833ef3000113ff3c9c61867d31
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-08-30 11:25:52 +0530

    add a base case.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- construct_binary_tree_from_inorder_and_postorder_traversal.py
+++ construct_binary_tree_from_inorder_and_postorder_traversal.py
@@ -5,4 +5,5 @@ from data_structures.tree_node import TreeNode
 
 
 def build_ree(inorder: list[int], postorder: list[int]) -> Optional[TreeNode]:
-    ...
+    if not postorder or not inorder:
+        return None
