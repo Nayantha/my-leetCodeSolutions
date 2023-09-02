370efdca6055ec9c5c31c29aff1c26feda0fe5d2
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-02 21:30:54 +0530

    fix errors and format according to recommendations.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- sum_root_to_leaf_numbers.py
+++ sum_root_to_leaf_numbers.py
@@ -1,2 +1,8 @@
 # https://leetcode.com/problems/sum-root-to-leaf-numbers/
-def sumNumbers(self, root: Optional[TreeNode]) -> int:
+from typing import Optional
+
+from data_structures.tree_node import TreeNode
+
+
+def sum_numbers(root: Optional[TreeNode]) -> int:
+    ...
