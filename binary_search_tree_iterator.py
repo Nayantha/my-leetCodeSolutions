ae12631f6850f0b28656528ff710a19d2d498844
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-03 22:41:39 +0530

    format the code.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- binary_search_tree_iterator.py
+++ binary_search_tree_iterator.py
@@ -1,6 +1,9 @@
 # https://leetcode.com/problems/binary-search-tree-iterator/
 from typing import Optional
+
 from data_structures.tree_node import TreeNode
+
+
 class BSTIterator:
 
     def __init__(self, root: Optional[TreeNode]):
