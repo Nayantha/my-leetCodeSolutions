7d351069372b75e5ccf593076b7e68e286b1ac7a
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-03 22:24:29 +0530

    copy skeleton code from the site.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- binary_search_tree_iterator.py
+++ binary_search_tree_iterator.py
@@ -1,1 +1,8 @@
 # https://leetcode.com/problems/binary-search-tree-iterator/
+class BSTIterator:
+
+    def __init__(self, root: Optional[TreeNode]):
+
+    def next(self) -> int:
+
+    def hasNext(self) -> bool:
