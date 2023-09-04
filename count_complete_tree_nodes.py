eca3abecd50abdd35887b539577ad7cc9e7d3444
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-04 13:11:30 +0530

    remove previous logic.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- count_complete_tree_nodes.py
+++ count_complete_tree_nodes.py
@@ -7,13 +7,4 @@ from data_structures.tree_node import TreeNode
 def count_nodes(root: Optional[TreeNode]) -> int:
     if not root:
         return 0
-
-    def inorder_traversal(node, count):
-        if node:
-            count += 1
-            inorder_traversal(node.left, count)
-            inorder_traversal(node.right, count)
-
-    res = 0
-    inorder_traversal(root, res)
-    return res
+    
