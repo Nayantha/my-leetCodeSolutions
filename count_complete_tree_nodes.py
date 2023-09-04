afc5b5797bf49d7fa3fb6e4edf31318720890401
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-04 13:03:13 +0530

    add a base case.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- count_complete_tree_nodes.py
+++ count_complete_tree_nodes.py
@@ -5,6 +5,9 @@ from data_structures.tree_node import TreeNode
 
 
 def count_nodes(root: Optional[TreeNode]) -> int:
+    if not root:
+        return 0
+
     def inorder_traversal(node, count):
         if node:
             count += 1
