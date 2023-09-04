1810863d3fd518ebd2c875480c9102ea758b75a9
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-04 12:56:20 +0530

    add helper function to count.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- count_complete_tree_nodes.py
+++ count_complete_tree_nodes.py
@@ -5,4 +5,8 @@ from data_structures.tree_node import TreeNode
 
 
 def count_nodes(root: Optional[TreeNode]) -> int:
-    ...
+    def inorder_traversal(node, count):
+        if node:
+            count += 1
+            inorder_traversal(node.left, count)
+            inorder_traversal(node.right, count)
