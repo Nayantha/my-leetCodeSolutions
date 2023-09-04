1d8fd4aa42084d95c73a55fddf9f01e21f8d9169
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-04 13:38:45 +0530

    add a new logic.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- lowest_common_ancestor_of_a_binary_tree.py
+++ lowest_common_ancestor_of_a_binary_tree.py
@@ -5,3 +5,11 @@ from data_structures.tree_node import TreeNode
 def lowest_common_ancestor(root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
     if not root:
         return TreeNode()
+    if root == p or root == q:
+        return root
+    l = lowest_common_ancestor(root.left, p, q)
+    r = lowest_common_ancestor(root.right, p, q)
+    if l and r:
+        return root
+    else:
+        return l or r
