eda08b21f7f8361787aceb7f38bd3680685f3e20
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-04 13:17:33 +0530

    complete the rest of the logic.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- count_complete_tree_nodes.py
+++ count_complete_tree_nodes.py
@@ -7,4 +7,14 @@ from data_structures.tree_node import TreeNode
 def count_nodes(root: Optional[TreeNode]) -> int:
     if not root:
         return 0
-    
+    left_tree, right_tree = root, root
+    left_height, right_height = 0, 0
+    while left_tree:
+        left_height += 1
+        left_tree = left_tree.left
+    while right_tree:
+        right_height += 1
+        right_tree = right_tree.right
+    if left_height == right_height:
+        return pow(2, right_height)
+    return 1 + count_nodes(root.left) + count_nodes(root.right)
