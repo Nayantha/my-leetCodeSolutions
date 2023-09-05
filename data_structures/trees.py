a0b1a0eb230bcd7fc86e75b3126d4a2c26c75d27
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-05 19:07:23 +0530

    add a method to convert a list to a binary tree.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- data_structures/trees.py
+++ data_structures/trees.py
@@ -13,3 +13,13 @@ def traverse_in_binary_tree(root: TreeNode, pre_order_value_list: list[int | Non
         traverse_in_binary_tree(root.left, pre_order_value_list)
         traverse_in_binary_tree(root.right, pre_order_value_list)
     pre_order_value_list.append(None)
+
+
+def list_to_tree(value_list: list[int]):
+    if not value_list:
+        return None
+    mid = len(value_list)
+    root = TreeNode(val=value_list[mid])
+    root.left = list_to_tree(value_list[:mid])
+    root.right = list_to_tree(value_list[mid + 1:])
+    return root
