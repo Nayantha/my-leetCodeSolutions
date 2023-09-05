49fd059d95322ee8b9163b233977ceb8fd1d8902
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-05 19:08:35 +0530

    add the parameters.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_binary_tree_right_side_view.py
+++ tests/test_binary_tree_right_side_view.py
@@ -1,6 +1,6 @@
 import pytest
 
 
-@pytest.mark.parametrize()
-def test_right_side_view():
+@pytest.mark.parametrize("value_list, right_node_value_list")
+def test_right_side_view(value_list: list[int], right_node_value_list: list[int]):
     assert False
