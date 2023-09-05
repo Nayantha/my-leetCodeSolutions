1c6a14c4cd37891d0335d67970a353494ba01a72
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-05 19:30:42 +0530

    add logic based on queue.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- binary_tree_right_side_view.py
+++ binary_tree_right_side_view.py
@@ -1,4 +1,5 @@
 # https://leetcode.com/problems/binary-tree-right-side-view/
+from collections import deque
 from typing import Optional
 
 from data_structures.tree_node import TreeNode
@@ -6,4 +7,17 @@ from data_structures.tree_node import TreeNode
 
 def right_side_view(root: Optional[TreeNode]) -> list[int]:
     right_node_values: list[int] = []
+    queue = deque([root])
+    while queue:
+        right_side = None
+        queue_length = len(queue)
+
+        for i in range(queue_length):
+            node = queue.popleft()
+            if node:
+                right_side = node
+                queue.append(node.left)
+                queue.append(node.right)
+        if right_side:
+            right_node_values.append(right_side.val)
     return right_node_values
