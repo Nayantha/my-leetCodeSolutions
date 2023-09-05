3af3571521e97a6264b76d997ddd750312ff4c91
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-05 18:15:18 +0530

    fix errors and format according to recommendations.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- binary_tree_right_side_view.py
+++ binary_tree_right_side_view.py
@@ -1,2 +1,8 @@
 # https://leetcode.com/problems/binary-tree-right-side-view/
-def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+from typing import Optional
+
+from data_structures.tree_node import TreeNode
+
+
+def right_side_view(self, root: Optional[TreeNode]) -> list[int]:
+    ...
