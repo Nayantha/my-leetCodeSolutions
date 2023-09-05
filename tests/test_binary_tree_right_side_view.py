fcf94430a240f650277f6d2f90fa48c6cb30b2ef
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-05 19:12:46 +0530

    add test data.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_binary_tree_right_side_view.py
+++ tests/test_binary_tree_right_side_view.py
@@ -1,6 +1,10 @@
 import pytest
 
 
-@pytest.mark.parametrize("value_list, right_node_value_list")
+@pytest.mark.parametrize("value_list, right_node_value_list", [
+    ([1, 2, 3, None, 5, None, 4], [1, 3, 4]),
+    ([1, None, 3], [1, 3]),
+    ([], [])
+])
 def test_right_side_view(value_list: list[int], right_node_value_list: list[int]):
-    assert False
+    ...
