ef07530deced7bfcc955e3af80d15d7fba44d343
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-07 15:41:35 +0530

    add queue based solution logic.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- binary_tree_level_order_traversal.py
+++ binary_tree_level_order_traversal.py
@@ -1,4 +1,5 @@
 # https://leetcode.com/problems/binary-tree-level-order-traversal/
+from collections import deque
 from typing import Optional
 
 from data_structures.tree_node import TreeNode
@@ -6,4 +7,9 @@ from data_structures.tree_node import TreeNode
 
 def level_order(self, root: Optional[TreeNode]) -> list[list[int]]:
     res: list[list[int]] = []
+    node_queue = deque([root])
+    while node_queue:
+        node = node_queue.popleft()
+        if node:
+            res.append([node.left, node.right])
     return res
