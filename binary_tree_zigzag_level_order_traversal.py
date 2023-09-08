5dc7e964e0d5c24d8126082303f583631ea07f66
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-08 22:11:21 +0530

    add the same logic as the binary tree level traversal.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- binary_tree_zigzag_level_order_traversal.py
+++ binary_tree_zigzag_level_order_traversal.py
@@ -1,4 +1,5 @@
 # https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/
+from collections import deque
 from typing import Optional
 
 from data_structures.tree_node import TreeNode
@@ -6,4 +7,15 @@ from data_structures.tree_node import TreeNode
 
 def zigzag_level_order(root: Optional[TreeNode]) -> list[list[int]]:
     res: list[list[int]] = []
+    tree_nodes = deque([root])
+    while tree_nodes:
+        level_nodes = []
+        for i in range(len(tree_nodes)):
+            node = tree_nodes.popleft()
+            if node:
+                level_nodes.append(node.val)
+                tree_nodes.append(node.left)
+                tree_nodes.append(node.right)
+        if level_nodes:
+            res.append(level_nodes)
     return res
