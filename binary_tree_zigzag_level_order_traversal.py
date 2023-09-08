e1e819907cc72b57641f43ea629e463ff9f917ce
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-08 22:11:57 +0530

    reverse the level node list.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- binary_tree_zigzag_level_order_traversal.py
+++ binary_tree_zigzag_level_order_traversal.py
@@ -17,5 +17,5 @@ def zigzag_level_order(root: Optional[TreeNode]) -> list[list[int]]:
                 tree_nodes.append(node.left)
                 tree_nodes.append(node.right)
         if level_nodes:
-            res.append(level_nodes)
+            res.append(level_nodes[::-1])
     return res
