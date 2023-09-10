fcb83d7ea4aacca5026b269a6b3d1860d989665d
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-10 11:56:31 +0530

    add recursion logic.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- validate_binary_search_tree.py
+++ validate_binary_search_tree.py
@@ -8,3 +8,6 @@ def is_valid_bst(root: Optional[TreeNode]) -> bool:
     def is_valid(node: Optional[TreeNode], left_boundary: int, right_boundary: int):
         if not node:
             return True
+        if not (left_boundary < node.val < right_boundary):
+            return False
+        return is_valid(node.left, left_boundary, node.val) and is_valid(node.right, node.val, right_boundary)
