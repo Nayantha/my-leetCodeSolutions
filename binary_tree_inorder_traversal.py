0db3cfb985c25d5a13e7b4583d9d0d93088f7c04
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-10 11:27:59 +0530

    import tree node from data structure.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- binary_tree_inorder_traversal.py
+++ binary_tree_inorder_traversal.py
@@ -1,13 +1,8 @@
 # https://leetcode.com/problems/binary-tree-inorder-traversal/
 # Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
-
 from typing import Optional, List
+
+from data_structures.tree_node import TreeNode
 
 
 class Solution:
--- maximum_depth_of_binary_tree.py
+++ maximum_depth_of_binary_tree.py
@@ -1,12 +1,7 @@
 # https://leetcode.com/problems/maximum-depth-of-binary-tree/?envType=study-plan-v2&envId=top-interview-150
 from typing import Optional
 
-
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+from data_structures.tree_node import TreeNode
 
 
 def max_depth(root: Optional[TreeNode]) -> int:
