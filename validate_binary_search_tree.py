0638fb9ed06bb4dd54f04b5b624bc19c27dd4634
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-10 12:13:07 +0530

    add bases for a new solution logic.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- validate_binary_search_tree.py
+++ validate_binary_search_tree.py
@@ -5,15 +5,4 @@ from data_structures.tree_node import TreeNode
 
 
 def is_valid_bst(root: Optional[TreeNode]) -> bool:
-    prev = float('-inf')
-
-    def inorder(node):
-        nonlocal prev
-        if not node:
-            return True
-        if not (inorder(node.left) and prev < node.val):
-            return False
-        prev = node.val
-        return inorder(node.right)
-
-    return inorder(root)
+    node_values = []
