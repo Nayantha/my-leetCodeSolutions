b17d9c7bcdd24e7b59d946fa1e7fc5dc892ff39c
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-10 11:53:42 +0530

    add a base case.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- validate_binary_search_tree.py
+++ validate_binary_search_tree.py
@@ -6,4 +6,5 @@ from data_structures.tree_node import TreeNode
 
 def is_valid_bst(root: Optional[TreeNode]) -> bool:
     def is_valid(node: Optional[TreeNode], left_boundary: int, right_boundary: int):
-        ...
+        if not node:
+            return True
