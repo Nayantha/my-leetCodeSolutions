017bd1539a96e9f3351bf845aec9ccd21868aa96
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-10 12:15:55 +0530

    complete the in order function.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- validate_binary_search_tree.py
+++ validate_binary_search_tree.py
@@ -7,5 +7,9 @@ from data_structures.tree_node import TreeNode
 def is_valid_bst(root: Optional[TreeNode]) -> bool:
     node_values = []
 
-    def inorder_traverse():
-        ...
+    def inorder_traverse(node: Optional[TreeNode]):
+        if node:
+            return
+        inorder_traverse(node.left)
+        node_values.append(node.val)
+        inorder_traverse(node.right)
