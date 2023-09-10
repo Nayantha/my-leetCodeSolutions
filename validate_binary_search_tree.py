ec51cc5335ffc07d6fba6f8a20a16cae45c4e639
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-10 11:35:52 +0530

    fix the comparing error.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- validate_binary_search_tree.py
+++ validate_binary_search_tree.py
@@ -6,7 +6,7 @@ from data_structures.tree_node import TreeNode
 
 def is_valid_bst(root: Optional[TreeNode]) -> bool:
     if root.left and root.right:
-        return root.left.val < root.right and is_valid_bst(root.left) and is_valid_bst(root.right)
+        return root.left.val < root.right.val and is_valid_bst(root.left) and is_valid_bst(root.right)
     elif not root.left and not root.right:
         return True
     elif root.left or root.right:
