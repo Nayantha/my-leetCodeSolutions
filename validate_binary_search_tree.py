df2e2ac780900a65dc4b45b9e32adcb397bef659
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-10 11:35:05 +0530

    add a control flow basing next nodes.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- validate_binary_search_tree.py
+++ validate_binary_search_tree.py
@@ -5,4 +5,9 @@ from data_structures.tree_node import TreeNode
 
 
 def is_valid_bst(root: Optional[TreeNode]) -> bool:
-    return root.left.val < root.right and is_valid_bst(root.left) and is_valid_bst(root.right)
+    if root.left and root.right:
+        return root.left.val < root.right and is_valid_bst(root.left) and is_valid_bst(root.right)
+    elif not root.left and not root.right:
+        return True
+    elif root.left or root.right:
+        return False
