a3ccadb26504170131936f9d03a44eb1e6d8fc78
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-11 15:34:43 +0530

    add test cases.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_number_of_islands.py
+++ tests/test_number_of_islands.py
@@ -1,6 +1,23 @@
 import pytest
 
 
-@pytest.mark.parametrize("grid, expected_island_amount")
+@pytest.mark.parametrize("grid, expected_island_amount", [
+    (
+            [
+                ["1", "1", "1", "1", "0"],
+                ["1", "1", "0", "1", "0"],
+                ["1", "1", "0", "0", "0"],
+                ["0", "0", "0", "0", "0"]
+            ], 1
+    ),
+    (
+            [
+                ["1", "1", "0", "0", "0"],
+                ["1", "1", "0", "0", "0"],
+                ["0", "0", "1", "0", "0"],
+                ["0", "0", "0", "1", "1"]
+            ], 3
+    )
+])
 def test_num_islands(grid: list[list[str]], expected_island_amount: int):
     assert False
