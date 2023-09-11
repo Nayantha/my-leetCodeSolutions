c42320f740badd42f402d410554485d2a8c7945b
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-11 15:45:05 +0530

    add the helper function.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- number_of_islands.py
+++ number_of_islands.py
@@ -1,6 +1,24 @@
 # https://leetcode.com/problems/number-of-islands/
+from collections import deque
+
+
 def num_islands(grid: list[list[str]]) -> int:
     rows, columns = len(grid), len(grid[0])
     number_of_islands = 0
-    visited_indexes = set()
+    visited_indexes = set()  # (row, column)
+
+    def bfs(r: int, c: int):
+        queue = deque()
+        visited_indexes.add((r, c))
+        queue.append((r, c))
+        while queue:
+            row, col = queue.pop()
+            directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+            for dr, dc in directions:
+                r, c = row + dr, dc + col
+                visited_indexes.add((r, c))
+                if grid[r][c] == "1" and (r, c) not in visited_indexes and r < rows and c < columns:
+                    queue.append((r, c))
+                    visited_indexes.add((r, c))
+
     return number_of_islands
