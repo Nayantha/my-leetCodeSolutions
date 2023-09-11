d2834e5663b792a7f5d4aee52545aff3f831fe43
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-11 15:38:07 +0530

    add the basic variables the solution logic will be based.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- number_of_islands.py
+++ number_of_islands.py
@@ -1,3 +1,6 @@
 # https://leetcode.com/problems/number-of-islands/
 def num_islands(grid: list[list[str]]) -> int:
-    ...
+    rows, columns = len(grid), len(grid[0])
+    number_of_islands = 0
+    visited_indexes = set()
+    return number_of_islands
