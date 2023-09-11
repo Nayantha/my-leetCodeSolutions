81fc792072b0a095c8a78545e1f3f84d468c3cee
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-11 15:33:19 +0530

    add pytest framework.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_number_of_islands.py
+++ tests/test_number_of_islands.py
@@ -1,2 +1,6 @@
-def test_num_islands():
+import pytest
+
+
+@pytest.mark.parametrize("grid, expected_island_amount")
+def test_num_islands(grid: list[list[str]], expected_island_amount: int):
     assert False
