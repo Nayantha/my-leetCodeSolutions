d131d25a69fefe5499adff06b3d90d179a608a35
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-12 18:41:12 +0530

    add loop to iterate through the borderline and add not surrounded indexes.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- surrounded_regions.py
+++ surrounded_regions.py
@@ -3,7 +3,11 @@ def solve(board: list[list[str]]) -> None:
     un_surrounded_areas = []
     rows = len(board)
     columns = len(board[0])
-    for row_index in range(0, rows - 1, rows - 2):
-        for column_index in range(0, columns - 1, columns - 2):
+    for row_index in range(0, rows, rows - 1):
+        for column_index in range(0, columns):
+            if board[row_index][column_index] == "O":
+                un_surrounded_areas.append((row_index, column_index))
+    for row_index in range(1, rows - 1):
+        for column_index in range(0, columns, columns - 1):
             if board[row_index][column_index] == "O":
                 un_surrounded_areas.append((row_index, column_index))
