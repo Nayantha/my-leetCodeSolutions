9252fb928c59b85a7de0a5aace6a2f73876bc915
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-12 18:31:06 +0530

    add logic to store all Os in borderline.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- surrounded_regions.py
+++ surrounded_regions.py
@@ -1,3 +1,9 @@
 # https://leetcode.com/problems/surrounded-regions/
 def solve(board: list[list[str]]) -> None:
-    ...
+    un_surrounded_areas = []
+    rows = len(board)
+    columns = len(board[0])
+    for row_index in range(0, rows - 1, rows - 2):
+        for column_index in range(0, columns - 1, columns - 2):
+            if board[row_index][column_index] == "O":
+                un_surrounded_areas.append((row_index, column_index))
