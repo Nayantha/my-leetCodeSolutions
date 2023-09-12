cc0e0a4b5d1872f5ebb0f793e0b734708a8b1ee9
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-12 17:33:19 +0530

    remove comparing neighboring cells.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- surrounded_regions.py
+++ surrounded_regions.py
@@ -6,7 +6,5 @@ def solve(board: list[list[str]]) -> None:
     board_copy = board.copy()
     for row_index in range(rows):
         for column_index in range(columns):
-            if board_copy[row_index][column_index] == "O" and board_copy[row_index + 1][column_index] == "O" and \
-                    board_copy[row_index - 1][column_index] == "O" and board_copy[row_index][
-                column_index + 1] == "O" and board_copy[row_index][column_index - 1] == "O":
+            if board_copy[row_index][column_index] == "O":
                 board[row_index][column_index] = "X"
