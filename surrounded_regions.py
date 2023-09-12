9fc98d3179b5208df360aaef0baa77750049b7ba
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-12 19:01:20 +0530

    add logic to get board details.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- surrounded_regions.py
+++ surrounded_regions.py
@@ -1,27 +1,7 @@
 # https://leetcode.com/problems/surrounded-regions/
 def solve(board: list[list[str]]) -> None:
-    un_surrounded_areas = []
-    rows = len(board)
+    row = len(board)
     columns = len(board[0])
-    if rows == columns == 1:
-        return
-    for row_index in range(0, rows, rows - 1):
-        for column_index in range(0, columns):
-            if board[row_index][column_index] == "O":
-                un_surrounded_areas.append((row_index, column_index))
-    for row_index in range(1, rows - 1):
-        for column_index in range(0, columns, columns - 1):
-            if board[row_index][column_index] == "O":
-                un_surrounded_areas.append((row_index, column_index))
-    for row_index in range(1, rows - 1):
-        for column_index in range(1, columns - 1):
-            if board[row_index][column_index] == "O" and (row_index, column_index) not in un_surrounded_areas:
-                if not (
-                        (row_index + 1, column_index) in un_surrounded_areas or
-                        (row_index - 1, column_index) in un_surrounded_areas or
-                        (row_index + 1, column_index) in un_surrounded_areas or
-                        (row_index, column_index + 1) in un_surrounded_areas
-                ):
-                    board[row_index][column_index] = "X"
-                else:
-                    un_surrounded_areas.append((row_index, column_index))
+    # get un-surrounded regions
+    # get surrounded regions
+    # rename un-surrounded regions
