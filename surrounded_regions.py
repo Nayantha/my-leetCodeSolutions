97eda6a344a4d549655d7d6c5fa0cb7da711c7cf
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-12 19:05:27 +0530

    add the helper function logic.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- surrounded_regions.py
+++ surrounded_regions.py
@@ -6,6 +6,11 @@ def solve(board: list[list[str]]) -> None:
     def capture(row: int, column: int):
         if row not in range(rows) or column not in range(columns) or board[row][column] != "O":
             return
+        board[row][column] = "T"
+        capture(row - 1, column)
+        capture(row + 1, column)
+        capture(row, column - 1)
+        capture(row, column + 1)
     # get un-surrounded regions
     # get surrounded regions
     # rename un-surrounded regions
