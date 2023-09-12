769c25e43ac8e7e8ed96d49557e3d87f2705cc23
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-12 18:52:10 +0530

    add a base case.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- surrounded_regions.py
+++ surrounded_regions.py
@@ -3,6 +3,8 @@ def solve(board: list[list[str]]) -> None:
     un_surrounded_areas = []
     rows = len(board)
     columns = len(board[0])
+    if rows == columns == 1:
+        return
     for row_index in range(0, rows, rows - 1):
         for column_index in range(0, columns):
             if board[row_index][column_index] == "O":
