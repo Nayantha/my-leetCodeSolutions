12b103538a1f2bc7926d3fc2ecb898875893c114
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-12 17:27:03 +0530

    add a basic solution logic.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- surrounded_regions.py
+++ surrounded_regions.py
@@ -1,3 +1,10 @@
 # https://leetcode.com/problems/surrounded-regions/
 def solve(board: list[list[str]]) -> None:
-    ...
+    # board_copy = copy.deepcopy(board)
+    board_copy = board.copy()
+    for row_index in range(len(board)):
+        for column_index in range(len(board[0])):
+            if board_copy[row_index][column_index] == "O" and board_copy[row_index + 1][column_index] == "O" and \
+                    board_copy[row_index - 1][column_index] == "O" and board_copy[row_index][
+                column_index + 1] == "O" and board_copy[row_index][column_index - 1] == "O":
+                board[row_index][column_index] = "X"
