680f9ee648e3803c0d6a2ccbc65a58dae8be38c9
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-12 17:47:49 +0530

    add test data.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_surrounded_regions.py
+++ tests/test_surrounded_regions.py
@@ -1,6 +1,10 @@
 import pytest
 
 
-@pytest.mark.parametrize("board, expected")
+@pytest.mark.parametrize("board, expected", [
+    ([["X", "X", "X", "X"], ["X", "O", "O", "X"], ["X", "X", "O", "X"], ["X", "O", "X", "X"]],
+     [["X", "X", "X", "X"], ["X", "X", "X", "X"], ["X", "X", "X", "X"], ["X", "O", "X", "X"]]),
+    ([["X"]], [["X"]])
+])
 def test_solve(board: list[list[str]], expected: list[list[str]]):
     assert False
