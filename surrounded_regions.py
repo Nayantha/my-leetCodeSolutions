fa28365efd1962ea2dc11da9d3a53e042e468c83
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-12 17:30:47 +0530

    extract number of rows and columns.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- surrounded_regions.py
+++ surrounded_regions.py
@@ -1,9 +1,11 @@
 # https://leetcode.com/problems/surrounded-regions/
 def solve(board: list[list[str]]) -> None:
     # board_copy = copy.deepcopy(board)
+    rows = len(board)
+    columns = len(board[0])
     board_copy = board.copy()
-    for row_index in range(len(board)):
-        for column_index in range(len(board[0])):
+    for row_index in range(rows):
+        for column_index in range(columns):
             if board_copy[row_index][column_index] == "O" and board_copy[row_index + 1][column_index] == "O" and \
                     board_copy[row_index - 1][column_index] == "O" and board_copy[row_index][
                 column_index + 1] == "O" and board_copy[row_index][column_index - 1] == "O":
