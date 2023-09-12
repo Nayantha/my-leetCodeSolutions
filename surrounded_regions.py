9d686b221e4e9eb183af5faac5b65b607d975ca3
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-12 19:03:53 +0530

    add base case for the helper function.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- surrounded_regions.py
+++ surrounded_regions.py
@@ -1,7 +1,11 @@
 # https://leetcode.com/problems/surrounded-regions/
 def solve(board: list[list[str]]) -> None:
-    row = len(board)
+    rows = len(board)
     columns = len(board[0])
+
+    def capture(row: int, column: int):
+        if row not in range(rows) or column not in range(columns) or board[row][column] != "O":
+            return
     # get un-surrounded regions
     # get surrounded regions
     # rename un-surrounded regions
