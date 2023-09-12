8cce3ca294f3a0dd8e6baa1db2dbe2443b20ffe9
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-12 17:49:11 +0530

    remove checking the edge elements.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- surrounded_regions.py
+++ surrounded_regions.py
@@ -4,8 +4,8 @@ def solve(board: list[list[str]]) -> None:
     rows = len(board)
     columns = len(board[0])
     board_copy = board.copy()
-    for row_index in range(rows):
-        for column_index in range(columns):
+    for row_index in range(1, rows - 1):
+        for column_index in range(1, columns - 1):
             if board_copy[row_index][column_index] == "O":
                 directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
                 for dr, dc in directions:
