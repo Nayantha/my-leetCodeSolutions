2ca7c8e776f49f805c27f31a6225f1eed99aa7ad
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-12 18:04:03 +0530

    add a logic that counts x and o amount and decide to flip.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- surrounded_regions.py
+++ surrounded_regions.py
@@ -7,4 +7,19 @@ def solve(board: list[list[str]]) -> None:
     for row_index in range(1, rows - 1):
         for column_index in range(1, columns - 1):
             if board_copy[row_index][column_index] == "O":
-                board[row_index][column_index] = "X"
+                directions = [
+                    [-1, -1], [-1, 0], [-1, 1],
+                    [0, -1], [0, 1],
+                    [1, -1], [1, 0], [1, 1]
+                ]
+                sum_of_x = 0
+                sum_of_o = 0
+                for dr, dc in directions:
+                    row, column = row_index + dr, column_index + dc
+                    if row in range(rows) and column in range(columns):
+                        if board_copy[row][column] == "X":
+                            sum_of_x += 1
+                        elif board_copy[row][column] == "O":
+                            sum_of_o += 1
+                if sum_of_x > sum_of_o:
+                    board[row_index][column_index] = "X"
