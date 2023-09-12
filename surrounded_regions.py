b85acf2b0d71ac48cc84dcd7a29dbe784d8bb0a2
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-12 19:08:20 +0530

    add catching border elements.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- surrounded_regions.py
+++ surrounded_regions.py
@@ -11,6 +11,12 @@ def solve(board: list[list[str]]) -> None:
         capture(row + 1, column)
         capture(row, column - 1)
         capture(row, column + 1)
+
     # get un-surrounded regions
+    for row_index in range(rows):
+        for column_index in range(columns):
+            if board[row_index][column_index] == "O" and (
+                    row_index in [0, rows - 1] or column_index in [0, columns - 1]):
+                capture(row_index, column_index)
     # get surrounded regions
     # rename un-surrounded regions
