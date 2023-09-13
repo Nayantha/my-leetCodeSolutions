91a0cbac1bf84128692383e17c2aebe43282b8e6
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-13 08:50:03 +0530

    add queue based solution logic.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- clone_graph.py
+++ clone_graph.py
@@ -1,4 +1,5 @@
 # https://leetcode.com/problems/clone-graph/description/
+from collections import deque
 from typing import Optional
 
 
@@ -13,3 +14,10 @@ def clone_graph(node: Optional['Node']) -> Optional['Node']:
         return None
     if not node.neighbors:
         return Node(node.val)
+    old_to_new_node_map = {}
+    new_node = Node(node.val)
+    old_to_new_node_map[node] = new_node
+    old_node_queue = deque([node])
+    while old_node_queue:
+        next_node = old_node_queue.popleft()
+    return new_node
