724ba15d1b07624596179597ef2c9ed3ca709d80
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-13 08:47:00 +0530

    add two base cases.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- clone_graph.py
+++ clone_graph.py
@@ -9,14 +9,7 @@ class Node:
 
 
 def clone_graph(node: Optional['Node']) -> Optional['Node']:
-    old_node_to_new_node_map = dict()
-
-    def clone_node(old_node: Node):
-        if old_node in old_node_to_new_node_map:
-            return old_node_to_new_node_map[old_node]
-        new_node = Node(old_node.val)
-        old_node_to_new_node_map[old_node] = new_node
-        for neighbour_node in old_node.neighbors:
-            new_node.neighbors.appenddfs(clone_node(neighbour_node))
-        return new_node
-    return clone_node(node) if node else None
+    if not node:
+        return None
+    if not node.neighbors:
+        return Node(node.val)
