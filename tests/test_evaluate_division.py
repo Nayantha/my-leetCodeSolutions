423d532d2cccb9b669e7d1c03b53d112f44972ee
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-14 09:21:12 +0530

    add pytest framework.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_evaluate_division.py
+++ tests/test_evaluate_division.py
@@ -1,2 +1,6 @@
-def test_calc_equation():
+import pytest
+
+
+@pytest.mark.parametrize("equations, values, queries")
+def test_calc_equation(equations: list[list[str]], values: list[float], queries: list[list[str]]):
     assert False
