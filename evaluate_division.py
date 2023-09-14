5b1e4dd20454174d7195b1ba59fe171c8989ea47
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-14 09:31:40 +0530

    map number to divider.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- evaluate_division.py
+++ evaluate_division.py
@@ -1,3 +1,10 @@
 # https://leetcode.com/problems/evaluate-division
+from collections import defaultdict
+
+
 def calc_equation(equations: list[list[str]], values: list[float], queries: list[list[str]]) -> list[float]:
-    ...
+    numerator_to_denominator_map = defaultdict(list)
+    for i, equation in enumerate(equations):
+        a, b = equation
+        numerator_to_denominator_map[a].append(b, values[i])
+        numerator_to_denominator_map[b].append(a, 1 / values[i])
