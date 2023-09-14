07543881e8aca6b4e2374e332a9791014b64e2f6
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-14 09:23:27 +0530

    add test data.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_evaluate_division.py
+++ tests/test_evaluate_division.py
@@ -1,6 +1,10 @@
 import pytest
 
 
-@pytest.mark.parametrize("equations, values, queries")
+@pytest.mark.parametrize("equations, values, queries", [
+    ([["a", "b"], ["b", "c"]], [2.0, 3.0], [["a", "c"], ["b", "a"], ["a", "e"], ["a", "a"], ["x", "x"]]),
+    ([["a", "b"], ["b", "c"], ["bc", "cd"]], [1.5, 2.5, 5.0], [["a", "c"], ["c", "b"], ["bc", "cd"], ["cd", "bc"]]),
+    ([["a", "b"]], [0.5], [["a", "b"], ["b", "a"], ["a", "c"], ["x", "y"]])
+])
 def test_calc_equation(equations: list[list[str]], values: list[float], queries: list[list[str]]):
     assert False
