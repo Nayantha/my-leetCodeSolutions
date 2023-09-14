8e5228bb5843d932ce8a14c9ccaedf39cc3e3576
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-14 09:38:04 +0530

    change appending to a list to adding to dict.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- evaluate_division.py
+++ evaluate_division.py
@@ -3,8 +3,8 @@ from collections import defaultdict
 
 
 def calc_equation(equations: list[list[str]], values: list[float], queries: list[list[str]]) -> list[float]:
-    numerator_to_denominator_map = defaultdict(list)
+    numerator_to_denominator_map = defaultdict(dict)
     for i, equation in enumerate(equations):
         a, b = equation
-        numerator_to_denominator_map[a].append(b, values[i])
-        numerator_to_denominator_map[b].append(a, 1 / values[i])
+        numerator_to_denominator_map[a][b] = values[i]
+        numerator_to_denominator_map[b][a] = 1 / values[i]
