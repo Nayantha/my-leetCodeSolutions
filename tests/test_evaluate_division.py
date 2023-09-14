cccd8f31b83c735a143835dbc8639fe271710248
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-14 09:24:42 +0530

    add test logic.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_evaluate_division.py
+++ tests/test_evaluate_division.py
@@ -1,5 +1,7 @@
 import pytest
 
+from evaluate_division import calc_equation
+
 
 @pytest.mark.parametrize("equations, values, queries", [
     ([["a", "b"], ["b", "c"]], [2.0, 3.0], [["a", "c"], ["b", "a"], ["a", "e"], ["a", "a"], ["x", "x"]]),
@@ -7,4 +9,4 @@ import pytest
     ([["a", "b"]], [0.5], [["a", "b"], ["b", "a"], ["a", "c"], ["x", "y"]])
 ])
 def test_calc_equation(equations: list[list[str]], values: list[float], queries: list[list[str]]):
-    assert False
+    assert calc_equation(equations, values, queries) == expedted
