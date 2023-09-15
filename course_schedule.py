e9c7fb466e96c0f80044840d99a85e754325a9ea
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-15 20:06:58 +0530

    add course and prerequisite courses map.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- course_schedule.py
+++ course_schedule.py
@@ -1,3 +1,5 @@
 # https://leetcode.com/problems/course-schedule/description/
 def can_finish(numCourses: int, prerequisites: list[list[int]]) -> bool:
-    ...
+    prerequisites_course_map: dict[int: list[int]] = {i: [] for i in range(numCourses)}
+    for course, prerequisite in prerequisites:
+        prerequisites_course_map[course].append(prerequisite)
