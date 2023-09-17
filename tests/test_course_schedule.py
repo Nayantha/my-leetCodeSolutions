8a67f762a46969d71b5b96d6db40289eedecfd09
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-17 11:26:33 +0530

    add pytest framework.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_course_schedule.py
+++ tests/test_course_schedule.py
@@ -1,2 +1,6 @@
-def test_can_finish():
+import pytest
+
+
+@pytest.mark.parametrize("num_courses, prerequisites, expected")
+def test_can_finish(num_courses: int, prerequisites: list[list[int]], expected: bool):
     assert False
