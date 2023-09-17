907b02c21cbbe393a1d584d26cb4802ba761b901
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-17 12:32:40 +0530

    add pytest framework.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_course_schedule_ii.py
+++ tests/test_course_schedule_ii.py
@@ -1,2 +1,6 @@
-def test_find_order():
+import pytest
+
+
+@pytest.mark.parametrize("num_courses, prerequisites, expected")
+def test_find_order(num_courses: int, prerequisites: list[list[int]], expected: list[int]):
     assert False
