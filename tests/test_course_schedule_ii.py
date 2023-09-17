54f6d44fe2b2e880a8e2ac0a74c9284d4c4993d7
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-17 13:32:01 +0530

    fix mistake in test data.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_course_schedule_ii.py
+++ tests/test_course_schedule_ii.py
@@ -7,7 +7,7 @@ from course_schedule_ii import find_order
     (2, [[1, 0]], [0, 1]),
     (4, [[1, 0], [2, 0], [3, 1], [3, 2]], [0, 2, 1, 3]),
     (1, [], 0),
-    (6, [[0, 1], [2, 2], [1, 3], [3, 2], [4, 0], [5, 0]], [2, 3, 1, 0, 4, 5]),
+    (6, [[0, 1], [0, 2], [1, 3], [3, 2], [4, 0], [5, 0]], [2, 3, 1, 0, 4, 5]),
 ])
 def test_find_order(num_courses: int, prerequisites: list[list[int]], expected: list[int]):
     assert find_order(num_courses, prerequisites) == expected
