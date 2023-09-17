91e7fac0e7c84ce4a37348de594ca3ddb1456039
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-17 13:37:04 +0530

    add the variables to store the progress.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- course_schedule_ii.py
+++ course_schedule_ii.py
@@ -3,4 +3,6 @@ def find_order(num_courses: int, prerequisites: list[list[int]]) -> list[int]:
     course_to_prerequisites_map: dict[int: list[int]] = {i: [] for i in range(num_courses)}
     for course, prerequisite in prerequisites:
         course_to_prerequisites_map[course].append(prerequisite)
+    completed_courses, completed_courses_in_cycle = set(), set()
+    course_complete_order = []
     return []
