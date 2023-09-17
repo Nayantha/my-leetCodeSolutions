9a4b0719053c031ae7be3e28e4dd7fdbb81337d3
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-17 13:42:30 +0530

    completed the helper function.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- course_schedule_ii.py
+++ course_schedule_ii.py
@@ -6,7 +6,18 @@ def find_order(num_courses: int, prerequisites: list[list[int]]) -> list[int]:
     completed_courses, completed_courses_in_cycle = set(), set()
     course_complete_order = []
 
-    def dfs(course_number: int):
-        ...
+    def dfs(course_number: int) -> bool:
+        if course_number in completed_courses_in_cycle:
+            return False
+        if course_number in completed_courses:
+            return True
+        completed_courses_in_cycle.add(course_number)
+        for prerequisite_course_for_course_number in course_to_prerequisites_map[course_number]:
+            if not dfs(prerequisite_course_for_course_number):
+                return False
+        completed_courses_in_cycle.remove(course_number)
+        completed_courses.add(course_number)
+        course_complete_order.append(course_number)
+        return True
 
     return []
