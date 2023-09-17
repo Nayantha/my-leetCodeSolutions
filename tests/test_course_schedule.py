bf57f9f9daca8deef3502f95332d8005c0635b7d
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-17 11:30:03 +0530

    add test logic.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_course_schedule.py
+++ tests/test_course_schedule.py
@@ -1,9 +1,11 @@
 import pytest
 
+from course_schedule import can_finish
+
 
 @pytest.mark.parametrize("num_courses, prerequisites, expected", [
     (2, [[1, 0]], True),
     (2, [[1, 0], [0, 1]], False)
 ])
 def test_can_finish(num_courses: int, prerequisites: list[list[int]], expected: bool):
-    assert False
+    assert can_finish(num_courses, prerequisites)
