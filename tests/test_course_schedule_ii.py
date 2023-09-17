16e291558a790012d4b2bb16adc00ca2ae80efd4
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-17 12:34:25 +0530

    add test data.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_course_schedule_ii.py
+++ tests/test_course_schedule_ii.py
@@ -1,6 +1,10 @@
 import pytest
 
 
-@pytest.mark.parametrize("num_courses, prerequisites, expected")
+@pytest.mark.parametrize("num_courses, prerequisites, expected", [
+    (2, [[1, 0]], [0, 1]),
+    (4, [[1, 0], [2, 0], [3, 1], [3, 2]], [0, 2, 1, 3]),
+    (1, [], 0)
+])
 def test_find_order(num_courses: int, prerequisites: list[list[int]], expected: list[int]):
     assert False
