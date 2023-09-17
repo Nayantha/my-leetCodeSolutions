a50dcc09e5cb7ba07c9907eabf7be4d713cf116c
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-17 13:59:37 +0530

    add a faster solution from the site.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- course_schedule_ii.py
+++ course_schedule_ii.py
@@ -1,23 +1,23 @@
 # https://leetcode.com/problems/course-schedule-ii/description/
+from collections import deque
+
+
 def find_order(num_courses: int, prerequisites: list[list[int]]) -> list[int]:
-    course_to_prerequisites_map: dict[int: list[int]] = {i: [] for i in range(num_courses)}
+    degrees = [0] * num_courses
+    adj = [[] for _ in range(num_courses)]
     for course, prerequisite in prerequisites:
-        course_to_prerequisites_map[course].append(prerequisite)
-    completed_courses, completed_courses_in_cycle = set(), set()
-    course_complete_order = []
-
-    def dfs(course_number: int) -> bool:
-        if course_number in completed_courses_in_cycle:
-            return False
-        if course_number in completed_courses:
-            return True
-        completed_courses_in_cycle.add(course_number)
-        for prerequisite_course_for_course_number in course_to_prerequisites_map[course_number]:
-            if not dfs(prerequisite_course_for_course_number):
-                return False
-        completed_courses_in_cycle.remove(course_number)
-        completed_courses.add(course_number)
-        course_complete_order.append(course_number)
-        return True
-
-    return course_complete_order if all(dfs(course) for course in range(num_courses)) else []
+        adj[prerequisite].append(course)
+        degrees[course] += 1
+    q = deque()
+    for i in range(num_courses):
+        if degrees[i] == 0:
+            q.append(i)
+    res = []
+    while q:
+        course = q.popleft()
+        res.append(course)
+        for neighbour in adj[course]:  # type: int
+            degrees[neighbour] -= 1
+            if degrees[neighbour] == 0:
+                q.append(neighbour)
+    return res if len(res) == num_courses else []
