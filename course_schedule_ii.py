0c4434b60b92d82552bc14b0ea99888fa1fa9250
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-17 12:31:02 +0530

    fill the map.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- course_schedule_ii.py
+++ course_schedule_ii.py
@@ -1,4 +1,6 @@
 # https://leetcode.com/problems/course-schedule-ii/description/
 def find_order(num_courses: int, prerequisites: list[list[int]]) -> list[int]:
     course_to_prerequisites_map: dict[int: list[int]] = {i: [] for i in range(num_courses)}
+    for course, prerequisite in prerequisites:
+        course_to_prerequisites_map[course].append(prerequisite)
     return []
