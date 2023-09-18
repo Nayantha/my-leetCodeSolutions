4ced6951ed96d6bc299f313fcd8fdf4b80d07c0a
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-18 16:54:33 +0530

    add test data.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_snakes_and_ladders.py
+++ tests/test_snakes_and_ladders.py
@@ -1,6 +1,19 @@
 import pytest
 
 
-@pytest.mark.parametrize("board, min_turns_to_top")
+@pytest.mark.parametrize("board, min_turns_to_top", [
+    ([
+         [-1, -1, -1, -1, -1, -1],
+         [-1, -1, -1, -1, -1, -1],
+         [-1, -1, -1, -1, -1, -1],
+         [-1, 35, -1, -1, 13, -1],
+         [-1, -1, -1, -1, -1, -1],
+         [-1, 15, -1, -1, -1, -1]
+     ], 4),
+    ([
+         [-1, -1],
+         [-1, 3]
+     ], 1)
+])
 def test_snakes_and_ladders(board: list[list[int]], min_turns_to_top: int):
     assert False
