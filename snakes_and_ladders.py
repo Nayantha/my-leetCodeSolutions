de59723eb39c3a3e31727ef316f6424dae206ed4
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-18 17:26:21 +0530

    add a second helper function to do dfs.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- snakes_and_ladders.py
+++ snakes_and_ladders.py
@@ -1,6 +1,14 @@
 # https://leetcode.com/problems/snakes-and-ladders/description/
+from collections import deque
+
+
 def snakes_and_ladders(board: list[list[int]]) -> int:
     board_length = len(board)
 
     def int_to_position(number: int):
         return [number // board_length, number % board_length]
+
+    queue = deque([1, 0])  # [number, no of moves to get there]
+    visited_numbers = set()
+    while queue:
+        number, moves_done = queue.popleft()
