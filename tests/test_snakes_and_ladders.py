dede3e33722c823d54655c237d3f6d40bb02fa46
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-18 17:52:07 +0530

    add another test case.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_snakes_and_ladders.py
+++ tests/test_snakes_and_ladders.py
@@ -15,7 +15,16 @@ from snakes_and_ladders import snakes_and_ladders
     ([
          [-1, -1],
          [-1, 3]
-     ], 1)
+     ], 1),
+    (
+            [
+                [-1, 1, 2, -1],
+                [2, 13, 15, -1],
+                [-1, 10, -1, -1],
+                [-1, 6, 2, 8]
+            ],
+            2
+    )
 ])
 def test_snakes_and_ladders(board: list[list[int]], min_turns_to_top: int):
     assert snakes_and_ladders(board) == min_turns_to_top
