4f52044d470b57cd0910d62aba8477ac1c8895e6
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-18 16:52:51 +0530

    add pytest framework.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_snakes_and_ladders.py
+++ tests/test_snakes_and_ladders.py
@@ -1,2 +1,6 @@
-def test_snakes_and_ladders():
+import pytest
+
+
+@pytest.mark.parametrize("board, min_turns_to_top")
+def test_snakes_and_ladders(board: list[list[int]], min_turns_to_top: int):
     assert False
