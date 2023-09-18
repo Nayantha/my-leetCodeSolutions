4b4d1f51716fc881cc9adcbb5baf0e653b27f283
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-18 17:04:28 +0530

    add logic to flip the forwarding through the column according to the row index.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- snakes_and_ladders.py
+++ snakes_and_ladders.py
@@ -1,6 +1,12 @@
 # https://leetcode.com/problems/snakes-and-ladders/description/
 def snakes_and_ladders(board: list[list[int]]) -> int:
     rows, columns = len(board), len(board[0])
+    go_forward_in_columns = True
     for row in range(rows - 1, -1, -1):
-        for column in range(columns):
-            print(f"board[r={row}][c={column}]={board[row][column]}")
+        if go_forward_in_columns:
+            for column in range(columns):
+                print(f"board[r={row}][c={column}]={board[row][column]}")
+        else:
+            for column in range(columns - 1, -1, -1):
+                print(f"board[r={row}][c={column}]={board[row][column]}")
+        go_forward_in_columns = not go_forward_in_columns
