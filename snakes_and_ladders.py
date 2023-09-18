79b0891429c210bee090c118a62cd7163e005633
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-18 17:23:49 +0530

    add a helper function to convert number to index on board.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- snakes_and_ladders.py
+++ snakes_and_ladders.py
@@ -1,12 +1,6 @@
 # https://leetcode.com/problems/snakes-and-ladders/description/
 def snakes_and_ladders(board: list[list[int]]) -> int:
-    rows, columns = len(board), len(board[0])
-    go_forward_in_columns = True
-    for row in range(rows - 1, -1, -1):
-        if go_forward_in_columns:
-            for column in range(columns):
-                print(f"board[r={row}][c={column}]={board[row][column]}")
-        else:
-            for column in range(columns - 1, -1, -1):
-                print(f"board[r={row}][c={column}]={board[row][column]}")
-        go_forward_in_columns = not go_forward_in_columns
+    board_length = len(board)
+
+    def int_to_position(number: int):
+        return [number // board_length, number % board_length]
