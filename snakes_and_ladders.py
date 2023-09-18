b3badea8e158f1c13b8ff2715f595ab5114dbc08
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-18 16:59:08 +0530

    add a loop to travel from left bottom to top.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- snakes_and_ladders.py
+++ snakes_and_ladders.py
@@ -1,3 +1,6 @@
 # https://leetcode.com/problems/snakes-and-ladders/description/
 def snakes_and_ladders(board: list[list[int]]) -> int:
-    ...
+    rows, columns = len(board), len(board[0])
+    for row in range(rows - 1, -1, -1):
+        for column in range(columns):
+            print(f"board[r={row}][c={column}]={board[row][column]}")
