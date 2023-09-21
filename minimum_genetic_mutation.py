d863d5c8d17be3149f79acb275f1f2130f96a0c1
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-21 06:53:45 +0530

    mutate logic to return -1 when the mutation is 0.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- minimum_genetic_mutation.py
+++ minimum_genetic_mutation.py
@@ -1,10 +1,9 @@
 # https://leetcode.com/problems/minimum-genetic-mutation/
 def min_mutation(startGene: str, endGene: str, bank: list[str]) -> int:
-    total_mutations = -1
+    total_mutations = 0
     if endGene not in bank:
-        return total_mutations
-
+        return -1
     for i in range(len(startGene)):
         if startGene[i] != endGene[i]:
             total_mutations += 1
-    return total_mutations
+    return total_mutations if total_mutations else -1
