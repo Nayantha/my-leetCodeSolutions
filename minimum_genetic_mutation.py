9fd5db9e9f2ae134c92479839ecaa29eb6940603
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-21 07:06:03 +0530

    add logic to count bases of DNA in each sequence and compare them at the end.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- minimum_genetic_mutation.py
+++ minimum_genetic_mutation.py
@@ -3,7 +3,12 @@ def min_mutation(startGene: str, endGene: str, bank: list[str]) -> int:
     total_mutations = 0
     if endGene not in bank:
         return -1
+    bases_in_dna_count = {"A": 0, "C": 0, "G": 0, "T": 0}
+    start_gene_base_count = bases_in_dna_count.copy()
+    end_gene_base_count = bases_in_dna_count.copy()
     for i in range(len(startGene)):
+        start_gene_base_count[startGene[i]] += 1
+        end_gene_base_count[endGene[i]] += 1
         if startGene[i] != endGene[i]:
             total_mutations += 1
-    return total_mutations if total_mutations else -1
+    return total_mutations if total_mutations and start_gene_base_count != end_gene_base_count else -1
