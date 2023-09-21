274700ff24f4cc3fb5bcd6952956e7ba07d3107d
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-21 06:48:07 +0530

    add test data.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_minimum_genetic_mutation.py
+++ tests/test_minimum_genetic_mutation.py
@@ -1,6 +1,9 @@
 import pytest
 
 
-@pytest.mark.parametrize("start_gene, end_gene, bank, min_mutations")
+@pytest.mark.parametrize("start_gene, end_gene, bank, min_mutations", [
+    ("AACCGGTT", "AACCGGTA", ["AACCGGTA"], 1),
+    ("AACCGGTT", "AAACGGTA", ["AACCGGTA", "AACCGCTA", "AAACGGTA"], 2)
+])
 def test_min_mutation(start_gene: str, end_gene: str, bank: list[str], min_mutations: int):
     assert False
