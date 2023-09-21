c7fff6332de1638fdb78aa971e8a7d871d0be453
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-21 07:49:42 +0530

    add possible bases list.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- minimum_genetic_mutation.py
+++ minimum_genetic_mutation.py
@@ -1,6 +1,7 @@
 # https://leetcode.com/problems/minimum-genetic-mutation/
 def min_mutation(startGene: str, endGene: str, bank: list[str]) -> int:
     changed_indexes = []
+    possible_bases = ['A', 'C', 'G', 'T']
     for i in range(len(startGene)):
         if startGene[i] != endGene[i]:
             changed_indexes.append(i)
