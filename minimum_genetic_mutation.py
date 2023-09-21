a6d6a40c0d4abb700e1cfe051588b99890be6b1c
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-21 08:36:18 +0530

    add logic to loop over the queue.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- minimum_genetic_mutation.py
+++ minimum_genetic_mutation.py
@@ -9,15 +9,16 @@ def min_mutation(startGene: str, endGene: str, bank: list[str]) -> int:
     visited_genes.add(startGene)
     total_mutations = 0
     while queue:
-        gene = queue.popleft()
-        if gene == endGene:
-            return total_mutations
-        for i in range(len(startGene)):
-            current_possible_bases = [base for base in possible_bases if base != gene[i]]
-            for base in current_possible_bases:
-                mutated_gene = f"{gene[:i]}{base}{gene[i + 1:]}"
-                if mutated_gene in bank and mutated_gene not in visited_genes:
-                    visited_genes.add(mutated_gene)
-                    queue.append(mutated_gene)
+        for _ in range(len(queue)):
+            gene = queue.popleft()
+            if gene == endGene:
+                return total_mutations
+            for i in range(len(startGene)):
+                current_possible_bases = [base for base in possible_bases if base != gene[i]]
+                for base in current_possible_bases:
+                    mutated_gene = f"{gene[:i]}{base}{gene[i + 1:]}"
+                    if mutated_gene in bank and mutated_gene not in visited_genes:
+                        visited_genes.add(mutated_gene)
+                        queue.append(mutated_gene)
         total_mutations += 1
     return -1
