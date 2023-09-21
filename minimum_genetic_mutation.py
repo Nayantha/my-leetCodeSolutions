7a7501df98b81c14f6e724084d6ad74c2d7002b6
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-21 06:44:36 +0530

    add a base case.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- minimum_genetic_mutation.py
+++ minimum_genetic_mutation.py
@@ -1,3 +1,4 @@
 # https://leetcode.com/problems/minimum-genetic-mutation/
 def min_mutation(startGene: str, endGene: str, bank: list[str]) -> int:
-    ...
+    if endGene not in bank:
+        return -1
