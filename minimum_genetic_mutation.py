5ea74a40fc7c75c52375ac897e449e5cb71333a9
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-21 06:51:15 +0530

    add the basic solution logic.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- minimum_genetic_mutation.py
+++ minimum_genetic_mutation.py
@@ -1,4 +1,10 @@
 # https://leetcode.com/problems/minimum-genetic-mutation/
 def min_mutation(startGene: str, endGene: str, bank: list[str]) -> int:
+    total_mutations = -1
     if endGene not in bank:
-        return -1
+        return total_mutations
+
+    for i in range(len(startGene)):
+        if startGene[i] != endGene[i]:
+            total_mutations += 1
+    return total_mutations
