805b2201bf5160f1cec240c9c422a8bc189a669b
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-21 06:46:52 +0530

    add pytest framework.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_minimum_genetic_mutation.py
+++ tests/test_minimum_genetic_mutation.py
@@ -1,2 +1,6 @@
-def test_min_mutation():
+import pytest
+
+
+@pytest.mark.parametrize("start_gene, end_gene, bank, min_mutations")
+def test_min_mutation(start_gene: str, end_gene: str, bank: list[str], min_mutations: int):
     assert False
