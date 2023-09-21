966570e29db88efb706ea3b9208b6c906e279d82
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-21 08:23:48 +0530

    add another test case.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_minimum_genetic_mutation.py
+++ tests/test_minimum_genetic_mutation.py
@@ -6,7 +6,8 @@ from minimum_genetic_mutation import min_mutation
 @pytest.mark.parametrize("start_gene, end_gene, bank, min_mutations", [
     ("AACCGGTT", "AACCGGTA", ["AACCGGTA"], 1),
     ("AACCGGTT", "AAACGGTA", ["AACCGGTA", "AACCGCTA", "AAACGGTA"], 2),
-    ("AACCGGTT", "AAACGGTA", ["AACCGATT", "AACCGATA", "AAACGATA", "AAACGGTA"], 4)
+    ("AACCGGTT", "AAACGGTA", ["AACCGATT", "AACCGATA", "AAACGATA", "AAACGGTA"], 4),
+    ("AACCGGTT", "AACCGCTA", ["AACCGGTA", "AACCGCTA", "AAACGGTA"], 2)
 ])
 def test_min_mutation(start_gene: str, end_gene: str, bank: list[str], min_mutations: int):
     assert min_mutation(start_gene, end_gene, bank) == min_mutations
