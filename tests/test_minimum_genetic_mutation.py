0ae98765fdbea56b25d62f2f11724fea79d69153
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-21 06:49:11 +0530

    add test logic.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_minimum_genetic_mutation.py
+++ tests/test_minimum_genetic_mutation.py
@@ -1,9 +1,11 @@
 import pytest
 
+from minimum_genetic_mutation import min_mutation
+
 
 @pytest.mark.parametrize("start_gene, end_gene, bank, min_mutations", [
     ("AACCGGTT", "AACCGGTA", ["AACCGGTA"], 1),
     ("AACCGGTT", "AAACGGTA", ["AACCGGTA", "AACCGCTA", "AAACGGTA"], 2)
 ])
 def test_min_mutation(start_gene: str, end_gene: str, bank: list[str], min_mutations: int):
-    assert False
+    assert min_mutation(start_gene, end_gene, bank) == min_mutations
