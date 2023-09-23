52dad59cd1a1ed380ea2e7562aece3fa4b4ab44e
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-23 20:23:02 +0530

    add a test case.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

---
+++ tests/test_implement_trie.py
@@ -0,0 +1,11 @@
+from implement_trie import Trie
+
+
+def test_trie():
+    trie = Trie()
+    trie.insert("apple")
+    assert trie.search("apple") == True
+    assert trie.search("app") == False
+    assert trie.startsWith("app") == True
+    trie.insert("app")
+    assert trie.search("app") == True
