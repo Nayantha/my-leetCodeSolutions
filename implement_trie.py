abf49951e61e976c534625ab08f7ad1cef75a0f4
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-23 19:24:36 +0530

    copy skeleton code from the site.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- implement_trie.py
+++ implement_trie.py
@@ -1,1 +1,10 @@
 # https://leetcode.com/problems/implement-trie-prefix-tree/description/
+class Trie:
+
+    def __init__(self):
+
+    def insert(self, word: str) -> None:
+
+    def search(self, word: str) -> bool:
+
+    def startsWith(self, prefix: str) -> bool:
