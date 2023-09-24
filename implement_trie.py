3064c64bdd05e75070b01972a9586801e3d0e289
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-09-24 08:41:45 +0530

    add the changing of the current node logic.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- implement_trie.py
+++ implement_trie.py
@@ -15,12 +15,14 @@ class Trie:
         for letter in word:
             if letter not in current_node.letter_collections:
                 current_node.letter_collections[letter] = TrieNode()
+            current_node = current_node.letter_collections[letter]
 
     def search(self, word: str) -> bool:
         current_node: TrieNode = self.root_node
         for letter in word:
             if letter not in current_node.letter_collections:
                 return False
+            current_node = current_node.letter_collections[letter]
         return current_node.is_last_letter
 
     def startsWith(self, prefix: str) -> bool:
