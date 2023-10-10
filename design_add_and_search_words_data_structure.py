0f86f133d50d53b05379d61bcfcc841c255ea327
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-10-10 21:41:23 +0530

    add a root node.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- design_add_and_search_words_data_structure.py
+++ design_add_and_search_words_data_structure.py
@@ -1,8 +1,9 @@
 # https://leetcode.com/problems/design-add-and-search-words-data-structure/
+from implement_trie import TrieNode
 class WordDictionary:
 
     def __init__(self):
-        ...
+        self.root = TrieNode()
 
     def add_word(self, word: str) -> None:
         ...
