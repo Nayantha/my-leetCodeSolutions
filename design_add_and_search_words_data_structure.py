e833eefd5cc1c8698394494790bc2f1785cb0cca
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-10-10 21:44:03 +0530

    add add_node method.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- design_add_and_search_words_data_structure.py
+++ design_add_and_search_words_data_structure.py
@@ -6,7 +6,12 @@ class WordDictionary:
         self.root = TrieNode()
 
     def add_word(self, word: str) -> None:
-        ...
+        current_node = self.root
+        for letter in word:
+            if letter not in current_node.letter_collections:
+                current_node.letter_collections[letter] = TrieNode()
+            current_node = current_node[letter]
+        current_node.is_last_letter = True
 
     def search(self, word: str) -> bool:
         ...
