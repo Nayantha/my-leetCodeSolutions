ead27c3a6668aadf473b463f433c85eb2d6b08c0
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-10-10 11:46:14 +0530

    copy skeleton code from the site.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- design_add_and_search_words_data_structure.py
+++ design_add_and_search_words_data_structure.py
@@ -1,1 +1,8 @@
 # https://leetcode.com/problems/design-add-and-search-words-data-structure/
+class WordDictionary:
+
+    def __init__(self):
+
+    def addWord(self, word: str) -> None:
+
+    def search(self, word: str) -> bool:
