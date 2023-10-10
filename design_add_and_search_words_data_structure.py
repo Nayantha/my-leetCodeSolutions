a96667c0cac61c03e199be32de440c38bb9573d7
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-10-10 21:50:56 +0530

    add search method.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- design_add_and_search_words_data_structure.py
+++ design_add_and_search_words_data_structure.py
@@ -14,4 +14,19 @@ class WordDictionary:
         current_node.is_last_letter = True
 
     def search(self, word: str) -> bool:
-        ...
+        def dfs(j: int, root: TrieNode):
+            current_node = root
+            for i in range(j, len(word)):
+                letter = word[i]
+                if letter == ".":
+                    for letter_collection in current_node.letter_collections.values():
+                        if dfs(i + 1, letter_collection):
+                            return True
+                    return False
+                else:
+                    if letter not in current_node.letter_collections:
+                        return False
+                    current_node = current_node.letter_collections[letter]
+                return current_node.is_last_letter
+
+        return dfs(0, self.root)
