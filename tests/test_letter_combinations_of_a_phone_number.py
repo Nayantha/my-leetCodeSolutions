59317c076f8cb67971cfeb5e3dce894724152e84
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-10-11 17:06:53 +0530

    import pytest framework.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_letter_combinations_of_a_phone_number.py
+++ tests/test_letter_combinations_of_a_phone_number.py
@@ -1,2 +1,10 @@
-def test_letter_combinations():
+import pytest
+
+
+@pytest.mark.parametrize("digits, combinations", (
+        ["23", ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]],
+        ["", []],
+        ['2', ["a", "b", "c"]]
+))
+def test_letter_combinations(digits: str, combinations: list[str]):
     assert False
