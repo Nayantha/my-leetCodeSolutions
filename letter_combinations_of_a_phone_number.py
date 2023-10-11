f7ec95a45d135ab279d197a790f2c851255aae1b
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-10-11 17:03:37 +0530

    add the digit to letter map.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- letter_combinations_of_a_phone_number.py
+++ letter_combinations_of_a_phone_number.py
@@ -1,3 +1,12 @@
 # https://leetcode.com/problems/letter-combinations-of-a-phone-number/description/
 def letter_combinations(digits: str) -> list[str]:
-    ...
+    digit_to_letter_dict = {
+        2: ['a', 'b', 'c'],
+        3: ['d', 'e', 'f'],
+        4: ['g', 'h', 'i'],
+        5: ['j', 'k', 'l'],
+        6: ['m', 'n', 'o'],
+        7: ['p', 'q', 'r', 's'],
+        8: ['t', 'u', 'v'],
+        9: ['w', 'x', 'y', 'z']
+    }
