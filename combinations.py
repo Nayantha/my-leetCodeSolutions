7e2553f0b5a69b82f4654869fafcd9016f561c66
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-10-12 17:07:50 +0530

    add a helper function.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- combinations.py
+++ combinations.py
@@ -1,3 +1,8 @@
 # https://leetcode.com/problems/combinations/
 def combine(n: int, k: int) -> list[list[int]]:
-    ...
+    total_combinations = []
+
+    def backtrack():
+        ...
+
+    return total_combinations
