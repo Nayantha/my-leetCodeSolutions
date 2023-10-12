7cea19dd22ba336c05cebac8234f38dd2e3fab84
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-10-12 16:57:19 +0530

    add test logic.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_combinations.py
+++ tests/test_combinations.py
@@ -1,6 +1,8 @@
 import pytest
 
+from combinations import combine
+
 
 @pytest.mark.parametrize("n, k, combinations")
 def test_combine(n: int, k: int, combinations: list[list[int]]):
-    assert False
+    assert combine(n, k) == combinations
