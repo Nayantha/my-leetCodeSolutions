2638d7d01e68f58bd470a12c34d5e435cc76f517
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-10-12 17:11:57 +0530

    complete the helper function.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- combinations.py
+++ combinations.py
@@ -2,7 +2,14 @@
 def combine(n: int, k: int) -> list[list[int]]:
     total_combinations = []
 
-    def backtrack():
-        ...
+    def backtrack(start: int, current_combination: list[int]):
+        if len(current_combination) == k:
+            total_combinations.append(current_combination.copy())
+            return
+        for i in range(start, n + 1):
+            current_combination.append(i)
+            backtrack(i + 1, current_combination)
+            current_combination.pop()
 
+    backtrack(1, [])
     return total_combinations
