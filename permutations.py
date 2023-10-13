92fae6139ad773e468cf3f840df235799fc84f98
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-10-13 18:03:06 +0530

    add 2 base cases.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- permutations.py
+++ permutations.py
@@ -1,3 +1,6 @@
 # https://leetcode.com/problems/permutations/description/
 def permute(nums: list[int]) -> list[list[int]]:
-    ...
+    if not nums:
+        return [[]]
+    if len(nums) == 1:
+        return [nums.copy()]
