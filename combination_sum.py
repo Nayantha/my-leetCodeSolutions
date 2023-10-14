0c5b27fde46629448a612cec360e3a2f09358a76
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-10-14 16:19:39 +0530

    add a solution logic.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- combination_sum.py
+++ combination_sum.py
@@ -1,3 +1,17 @@
 # https://leetcode.com/problems/combination-sum/description/
 def combination_sum(candidates: list[int], target: int) -> list[list[int]]:
-    ...
+    res = []
+
+    def dfs(i: int, cur: list[int], total: int):
+        if total == target:
+            res.append(cur.copy())
+            return
+        if i >= len(candidates) or target < total:
+            return
+        cur.append(candidates[i])
+        dfs(i, cur, total + candidates[i])
+        cur.pop()
+        dfs(i + 1, cur, total)
+
+    dfs(0, [], 0)
+    return res
