fb9ee8d07e9bb943d8dfe7c378d40eb657934429
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-10-14 16:04:52 +0530

    fix errors and format according to recommendations.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- combination_sum.py
+++ combination_sum.py
@@ -1,2 +1,3 @@
 # https://leetcode.com/problems/combination-sum/description/
-def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+def combination_sum(candidates: list[int], target: int) -> list[list[int]]:
+    ...
