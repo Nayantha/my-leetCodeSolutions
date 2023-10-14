748e25bca9df0d02df6729fc5abcc72c9700c4f0
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-10-14 16:09:25 +0530

    import pytest framework.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_combination_sum.py
+++ tests/test_combination_sum.py
@@ -1,2 +1,6 @@
-def test_combination_sum():
+import pytest
+
+
+@pytest.mark.parametrize("num_list, target, expected_combination")
+def test_combination_sum(num_list: list[int], target: int, expected_combination: list[list[int]]):
     assert False
