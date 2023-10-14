b8d37d064448a16f17c752631734cbaf8e8b4ba7
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2023-10-14 16:12:23 +0530

    add test logic.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_combination_sum.py
+++ tests/test_combination_sum.py
@@ -1,9 +1,11 @@
 import pytest
 
+from combination_sum import combination_sum
+
 
 @pytest.mark.parametrize("num_list, target, expected_combination", [
     ([2, 3, 6, 7], 7, [[2, 2, 3], [7]]),
     ([2, 3, 5], 8, [[2, 2, 2, 2], [2, 3, 3], [3, 5]])
 ])
 def test_combination_sum(num_list: list[int], target: int, expected_combination: list[list[int]]):
-    assert False
+    assert combination_sum(num_list, target) == expected_combination
