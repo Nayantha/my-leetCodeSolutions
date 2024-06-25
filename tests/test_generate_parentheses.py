42c5fdf9e14126b0250218e04af970b42fc879a6
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-06-25 18:38:45 +0530

    add test data.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_generate_parentheses.py
+++ tests/test_generate_parentheses.py
@@ -1,2 +1,10 @@
-def test_generate_parentheses():
+import pytest
+
+
+@pytest.mark.parametrize("n, expected", [
+    (3, ["((()))", "(()())", "(())()", "()(())", "()()()"]),
+    (0, []),
+    (1, ["()"])
+])
+def test_generate_parentheses(n: int, expected: list[str]):
     assert False
