7d21f36656b7db62c503ba9aa08bd5630b7196a4
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-07-20 19:24:04 +0530

    copy skeleton code from the site.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- trapping_rain_water.py
+++ trapping_rain_water.py
@@ -1,1 +1,6 @@
 # https://leetcode.com/problems/trapping-rain-water/description/?envType=study-plan-v2&envId=top-interview-150
+from typing import List
+
+
+def trap(height: List[int]) -> int:
+    ...
