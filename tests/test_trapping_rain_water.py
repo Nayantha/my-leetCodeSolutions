2e87e259a9f20ba5343bbbd5b8c995bbb301356f
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-07-20 19:31:07 +0530

    add test data.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- tests/test_trapping_rain_water.py
+++ tests/test_trapping_rain_water.py
@@ -1,2 +1,9 @@
-def test_trap():
+import pytest
+
+
+@pytest.mark.parametrize("height, expected", [
+    ([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1], 6),
+    ([4, 2, 0, 3, 2, 5], 9)
+])
+def test_trap(height: list[int], expected: int):
     assert False
