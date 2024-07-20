f11c94dde3bc4f7df6a59e5028ae35a552af9be0
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2024-07-20 19:56:07 +0530

    add solution logic.
    Signed-off-by: Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>

--- trapping_rain_water.py
+++ trapping_rain_water.py
@@ -3,4 +3,17 @@ from typing import List
 
 
 def trap(height: List[int]) -> int:
-    ...
+    l, r = 0, len(height) - 1
+    left_max, right_max = height[l], height[r]
+    total_height = 0
+    while l < r:
+        if left_max < right_max:
+            l += 1
+            left_max = max(left_max, height[l])
+            total_height += left_max - height[l]
+        else:
+            r -= 1
+            right_max = max(right_max, height[r])
+            total_height += right_max - height[r]
+
+    return total_height
